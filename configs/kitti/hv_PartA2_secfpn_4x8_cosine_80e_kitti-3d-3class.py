21a8c818ba91866d2536e516667833d2ce865c0c
wuyuefeng <wuyuefeng@sensetime.com>
2020-05-02 22:09:25 +0800

    Feature parta2 head

--- configs/kitti/hv_PartA2_secfpn_4x8_cosine_80e_kitti-3d-3class.py
+++ configs/kitti/hv_PartA2_secfpn_4x8_cosine_80e_kitti-3d-3class.py
@@ -3,20 +3,16 @@ voxel_size = [0.05, 0.05, 0.1]
 point_cloud_range = [0, -40, -3, 70.4, 40, 1]  # velodyne coordinates, x, y, z
 
 model = dict(
-    type='DynamicVoxelNet',
+    type='PartA2',
     voxel_layer=dict(
-        max_num_points=-1,  # max_points_per_voxel
+        max_num_points=5,  # max_points_per_voxel
         point_cloud_range=point_cloud_range,
         voxel_size=voxel_size,
-        max_voxels=(-1, -1),  # (training, testing) max_coxels
+        max_voxels=(16000, 40000)  # (training, testing) max_coxels
     ),
-    voxel_encoder=dict(
-        type='DynamicVFEV3',
-        num_input_features=4,
-        voxel_size=voxel_size,
-        point_cloud_range=point_cloud_range),
+    voxel_encoder=dict(type='VoxelFeatureExtractorV3'),
     middle_encoder=dict(
-        type='SparseEncoder',
+        type='SparseUNet',
         in_channels=4,
         output_shape=[41, 1600, 1408],
         pre_act=False,
@@ -26,16 +22,14 @@ model = dict(
         in_channels=256,
         layer_nums=[5, 5],
         layer_strides=[1, 2],
-        num_filters=[128, 256],
-    ),
+        num_filters=[128, 256]),
     neck=dict(
         type='SECONDFPN',
         in_channels=[128, 256],
         upsample_strides=[1, 2],
-        num_upsample_filters=[256, 256],
-    ),
-    bbox_head=dict(
-        type='SECONDHead',
+        num_upsample_filters=[256, 256]),
+    rpn_head=dict(
+        type='PartA2RPNHead',
         class_name=['Pedestrian', 'Cyclist', 'Car'],
         in_channels=512,
         feat_channels=512,
@@ -43,11 +37,9 @@ model = dict(
         encode_bg_as_zeros=True,
         anchor_generator=dict(
             type='Anchor3DRangeGenerator',
-            ranges=[
-                [0, -40.0, -0.6, 70.4, 40.0, -0.6],
-                [0, -40.0, -0.6, 70.4, 40.0, -0.6],
-                [0, -40.0, -1.78, 70.4, 40.0, -1.78],
-            ],
+            ranges=[[0, -40.0, -0.6, 70.4, 40.0, -0.6],
+                    [0, -40.0, -0.6, 70.4, 40.0, -0.6],
+                    [0, -40.0, -1.78, 70.4, 40.0, -1.78]],
             strides=[2],
             sizes=[[0.6, 0.8, 1.73], [0.6, 1.76, 1.73], [1.6, 3.9, 1.56]],
             rotations=[0, 1.57],
@@ -55,7 +47,7 @@ model = dict(
         diff_rad_by_sin=True,
         assigner_per_size=True,
         assign_per_class=True,
-        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder', ),
+        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder'),
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
@@ -65,46 +57,51 @@ model = dict(
         loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=2.0),
         loss_dir=dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.2),
-    ),
-)
+    ))
 # model training and testing settings
 train_cfg = dict(
-    assigner=[
-        dict(  # for Pedestrian
-            type='MaxIoUAssigner',
-            iou_calculator=dict(type='BboxOverlapsNearest3D'),
-            pos_iou_thr=0.35,
-            neg_iou_thr=0.2,
-            min_pos_iou=0.2,
-            ignore_iof_thr=-1),
-        dict(  # for Cyclist
-            type='MaxIoUAssigner',
-            iou_calculator=dict(type='BboxOverlapsNearest3D'),
-            pos_iou_thr=0.35,
-            neg_iou_thr=0.2,
-            min_pos_iou=0.2,
-            ignore_iof_thr=-1),
-        dict(  # for Car
-            type='MaxIoUAssigner',
-            iou_calculator=dict(type='BboxOverlapsNearest3D'),
-            pos_iou_thr=0.6,
-            neg_iou_thr=0.45,
-            min_pos_iou=0.45,
-            ignore_iof_thr=-1),
-    ],
-    allowed_border=0,
-    pos_weight=-1,
-    debug=False)
-test_cfg = dict(
-    use_rotate_nms=True,
-    nms_across_levels=False,
-    nms_thr=0.01,
-    score_thr=0.3,
-    min_bbox_size=0,
-    post_center_limit_range=[0, -40, -3, 70.4, 40, 0.0],
-    # soft-nms is also supported for rcnn testing
-    # e.g., nms=dict(type='soft_nms', iou_thr=0.5, min_score=0.05)
+    rpn=dict(
+        assigner=[
+            dict(  # for Pedestrian
+                type='MaxIoUAssigner',
+                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.35,
+                min_pos_iou=0.35,
+                ignore_iof_thr=-1),
+            dict(  # for Cyclist
+                type='MaxIoUAssigner',
+                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.35,
+                min_pos_iou=0.35,
+                ignore_iof_thr=-1),
+            dict(  # for Car
+                type='MaxIoUAssigner',
+                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                pos_iou_thr=0.6,
+                neg_iou_thr=0.45,
+                min_pos_iou=0.45,
+                ignore_iof_thr=-1),
+        ],
+        allowed_border=0,
+        pos_weight=-1,
+        debug=False),
+    rpn_proposal=dict(
+        nms_pre=9000,
+        nms_post=512,
+        nms_thr=0.8,
+        score_thr=0,
+        use_rotate_nms=False),
 )
+test_cfg = dict(
+    rpn=dict(
+        nms_pre=1024,
+        max_per_img=100,
+        use_rotate_nms=True,
+        nms_across_levels=False,
+        nms_thr=0.7,
+        score_thr=0))
 
 # dataset settings
 dataset_type = 'KittiDataset'
@@ -113,11 +110,7 @@ class_names = ['Pedestrian', 'Cyclist', 'Car']
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 input_modality = dict(
-    use_lidar=True,
-    use_depth=False,
-    use_lidar_intensity=True,
-    use_camera=True,
-)
+    use_lidar=True, use_depth=False, use_lidar_intensity=True, use_camera=True)
 db_sampler = dict(
     root_path=data_root,
     info_path=data_root + 'kitti_dbinfos_train.pkl',
@@ -126,17 +119,8 @@ db_sampler = dict(
     object_rot_range=[0.0, 0.0],
     prepare=dict(
         filter_by_difficulty=[-1],
-        filter_by_min_points=dict(
-            Car=5,
-            Pedestrian=10,
-            Cyclist=10,
-        ),
-    ),
-    sample_groups=dict(
-        Car=12,
-        Pedestrian=6,
-        Cyclist=6,
-    ),
+        filter_by_min_points=dict(Car=5, Pedestrian=10, Cyclist=10)),
+    sample_groups=dict(Car=12, Pedestrian=6, Cyclist=6),
 )
 train_pipeline = [
     dict(type='ObjectSample', db_sampler=db_sampler),
@@ -212,8 +196,7 @@ lr_config = dict(
     warmup_iters=1000,
     warmup_ratio=1.0 / 10,
     target_lr=1e-5,
-    as_ratio=True,
-)
+    as_ratio=True)
 momentum_config = None
 checkpoint_config = dict(interval=1)
 # yapf:disable
@@ -228,7 +211,7 @@ log_config = dict(
 total_epochs = 80
 dist_params = dict(backend='nccl', port=29502)
 log_level = 'INFO'
-work_dir = './work_dirs/sec_secfpn_80e'
+work_dir = './work_dirs/parta2_secfpn_80e'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
--- mmdet3d/models/detectors/__init__.py
+++ mmdet3d/models/detectors/__init__.py
@@ -3,10 +3,11 @@ from .mvx_faster_rcnn import (DynamicMVXFasterRCNN, DynamicMVXFasterRCNNV2,
                               DynamicMVXFasterRCNNV3)
 from .mvx_single_stage import MVXSingleStageDetector
 from .mvx_two_stage import MVXTwoStageDetector
+from .parta2 import PartA2
 from .voxelnet import DynamicVoxelNet, VoxelNet
 
 __all__ = [
     'BaseDetector', 'VoxelNet', 'DynamicVoxelNet', 'MVXSingleStageDetector',
     'MVXTwoStageDetector', 'DynamicMVXFasterRCNN', 'DynamicMVXFasterRCNNV2',
-    'DynamicMVXFasterRCNNV3'
+    'DynamicMVXFasterRCNNV3', 'PartA2'
 ]
