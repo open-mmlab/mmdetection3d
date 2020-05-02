2bbaabfe8ff965124869f7e21199640d378028db
liyinhao <SENSETIME\liyinhao@cn0214005036u.domain.sensetime.com>
2020-05-02 21:40:16 +0800

    change mean_color to color_mean

--- mmdet3d/datasets/pipelines/indoor_loading.py
+++ mmdet3d/datasets/pipelines/indoor_loading.py
@@ -15,13 +15,13 @@ class LoadPointsFromFile(object):
         name (str): scannet or sunrgbd.
         use_color (bool): Whether to use color.
         use_height (bool): Whether to use height.
-        mean_color (List[float]): Mean color of the point cloud.
+        color_mean (List[float]): Mean color of the point cloud.
     """
 
-    def __init__(self, use_color, use_height, mean_color):
+    def __init__(self, use_color, use_height, color_mean):
         self.use_color = use_color
         self.use_height = use_height
-        self.mean_color = mean_color
+        self.color_mean = color_mean
 
     def __call__(self, results):
         data_path = results.get('data_path', None)
@@ -44,7 +44,7 @@ class LoadPointsFromFile(object):
                 pcl_color = point_cloud[:, 3:6]
             point_cloud = point_cloud[:, 0:6]
             point_cloud[:, 3:] = (point_cloud[:, 3:] -
-                                  np.array(self.mean_color)) / 256.0
+                                  np.array(self.color_mean)) / 256.0
 
         if self.use_height:
             floor_height = np.percentile(point_cloud[:, 2], 0.99)
@@ -60,7 +60,7 @@ class LoadPointsFromFile(object):
         repr_str = self.__class__.__name__
         repr_str += '(use_height={})'.format(self.use_height)
         repr_str += '(use_color={}'.format(self.use_color)
-        repr_str += '(mean_color={})'.format(self.mean_color)
+        repr_str += '(mean_color={})'.format(self.color_mean)
         return repr_str
 
     def _get_lidar(self, scan_name, data_path):
