ba888088fb65bc5cd38fa7573e68039c919ec174
zhangwenwei <wayne.zw@outlook.com>
2020-05-05 15:19:45 +0800

    Fix training runtime bug

--- mmdet3d/apis/train.py
+++ mmdet3d/apis/train.py
@@ -2,11 +2,11 @@ import torch
 from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import DistSamplerSeedHook, Runner
 
+from mmdet3d.utils import get_root_logger
 from mmdet.apis.train import parse_losses
 from mmdet.core import (DistEvalHook, DistOptimizerHook, EvalHook,
                         Fp16OptimizerHook, build_optimizer)
 from mmdet.datasets import build_dataloader, build_dataset
-from mmdet.utils import get_root_logger
 
 
 def batch_processor(model, data, train_mode):
