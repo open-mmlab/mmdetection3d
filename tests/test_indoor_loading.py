51fa14c298810aa3ce752da86f981fe74d1c7589
liyinhao <SENSETIME\liyinhao@cn0214005036u.domain.sensetime.com>
2020-05-08 13:59:45 +0800

    change name, add new color module

--- tests/test_indoor_loading.py
+++ tests/test_indoor_loading.py
@@ -1,6 +1,7 @@
 import os.path as osp
 
 import mmcv
+import numpy as np
 
 from mmdet3d.datasets.pipelines.indoor_loading import (LoadAnnotations3D,
                                                        LoadPointsFromFile)
@@ -13,7 +14,6 @@ def test_load_points_from_file():
     data_path = './tests/data/sunrgbd/sunrgbd_trainval'
     sunrgbd_info = sunrgbd_info[0]
     scan_name = sunrgbd_info['point_cloud']['lidar_idx']
-    sunrgbd_results['info'] = sunrgbd_info
     sunrgbd_results['pts_filename'] = osp.join(data_path, 'lidar',
                                                '%06d.npy' % scan_name)
     sunrgbd_results = sunrgbd_load_points_from_file(sunrgbd_results)
@@ -27,7 +27,7 @@ def test_load_points_from_file():
     scannet_results['data_path'] = data_path
     scannet_info = scannet_info[0]
     scan_name = scannet_info['point_cloud']['lidar_idx']
-    scannet_results['info'] = scannet_info
+
     scannet_results['pts_filename'] = osp.join(data_path,
                                                scan_name + '_vert.npy')
     scannet_results = scannet_load_data(scannet_results)
@@ -36,10 +36,20 @@ def test_load_points_from_file():
 
 
 def test_load_annotations3D():
-    sunrgbd_info = mmcv.load('./tests/data/sunrgbd/sunrgbd_infos.pkl')
+    sunrgbd_info = mmcv.load('./tests/data/sunrgbd/sunrgbd_infos.pkl')[0]
     sunrgbd_load_annotations3D = LoadAnnotations3D()
     sunrgbd_results = dict()
-    sunrgbd_results['info'] = sunrgbd_info[0]
+    if sunrgbd_info['annos']['gt_num'] != 0:
+        sunrgbd_gt_bboxes_3d = sunrgbd_info['annos']['gt_boxes_upright_depth']
+        sunrgbd_gt_labels = sunrgbd_info['annos']['class'].reshape(-1, 1)
+        sunrgbd_gt_bboxes_3d_mask = np.ones_like(sunrgbd_gt_labels)
+    else:
+        sunrgbd_gt_bboxes_3d = np.zeros((1, 6), dtype=np.float32)
+        sunrgbd_gt_labels = np.zeros((1, 1))
+        sunrgbd_gt_bboxes_3d_mask = np.zeros((1, 1))
+    sunrgbd_results['gt_bboxes_3d'] = sunrgbd_gt_bboxes_3d
+    sunrgbd_results['gt_labels'] = sunrgbd_gt_labels
+    sunrgbd_results['gt_bboxes_3d_mask'] = sunrgbd_gt_bboxes_3d_mask
     sunrgbd_results = sunrgbd_load_annotations3D(sunrgbd_results)
     sunrgbd_gt_boxes = sunrgbd_results.get('gt_bboxes_3d', None)
     sunrgbd_gt_lbaels = sunrgbd_results.get('gt_labels', None)
@@ -48,17 +58,27 @@ def test_load_annotations3D():
     assert sunrgbd_gt_lbaels.shape == (3, 1)
     assert sunrgbd_gt_boxes_mask.shape == (3, 1)
 
-    scannet_info = mmcv.load('./tests/data/scannet/scannet_infos.pkl')
+    scannet_info = mmcv.load('./tests/data/scannet/scannet_infos.pkl')[0]
     scannet_load_annotations3D = LoadAnnotations3D()
     scannet_results = dict()
     data_path = './tests/data/scannet/scannet_train_instance_data'
-    scannet_info = scannet_info[0]
+    if scannet_info['annos']['gt_num'] != 0:
+        scannet_gt_bboxes_3d = scannet_info['annos']['gt_boxes_upright_depth']
+        scannet_gt_labels = scannet_info['annos']['class'].reshape(-1, 1)
+        scannet_gt_bboxes_3d_mask = np.ones_like(scannet_gt_labels)
+    else:
+        scannet_gt_bboxes_3d = np.zeros((1, 6), dtype=np.float32)
+        scannet_gt_labels = np.zeros((1, 1))
+        scannet_gt_bboxes_3d_mask = np.zeros((1, 1))
     scan_name = scannet_info['point_cloud']['lidar_idx']
     scannet_results['ins_labelname'] = osp.join(data_path,
                                                 scan_name + '_ins_label.npy')
     scannet_results['sem_labelname'] = osp.join(data_path,
                                                 scan_name + '_sem_label.npy')
     scannet_results['info'] = scannet_info
+    scannet_results['gt_bboxes_3d'] = scannet_gt_bboxes_3d
+    scannet_results['gt_labels'] = scannet_gt_labels
+    scannet_results['gt_bboxes_3d_mask'] = scannet_gt_bboxes_3d_mask
     scannet_results = scannet_load_annotations3D(scannet_results)
     scannet_gt_boxes = scannet_results.get('gt_bboxes_3d', None)
     scannet_gt_lbaels = scannet_results.get('gt_labels', None)
