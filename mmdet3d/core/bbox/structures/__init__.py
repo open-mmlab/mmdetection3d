4dca2f55bc7a9e6bfe6eea83bfdb6bf6dd95fa1d
wuyuefeng <wuyuefeng@sensetime.com>
2020-06-08 22:27:47 +0800

    Depth box3d

--- mmdet3d/core/bbox/structures/__init__.py
+++ mmdet3d/core/bbox/structures/__init__.py
@@ -1,5 +1,9 @@
 from .box_3d_mode import Box3DMode
 from .cam_box3d import CameraInstance3DBoxes
+from .depth_box3d import DepthInstance3DBoxes
 from .lidar_box3d import LiDARInstance3DBoxes
 
-__all__ = ['Box3DMode', 'LiDARInstance3DBoxes', 'CameraInstance3DBoxes']
+__all__ = [
+    'Box3DMode', 'LiDARInstance3DBoxes', 'CameraInstance3DBoxes',
+    'DepthInstance3DBoxes'
+]
