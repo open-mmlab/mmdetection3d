98cfb2ee05af09ec1598aea3f2adec38ae882e58
wangtai <wangtai@sensetime.com>
2020-07-04 02:13:57 +0800

    Add API to support Lyft

--- mmdet3d/datasets/__init__.py
+++ mmdet3d/datasets/__init__.py
@@ -2,6 +2,7 @@ from mmdet.datasets.builder import DATASETS, build_dataloader, build_dataset
 from .custom_3d import Custom3DDataset
 from .kitti2d_dataset import Kitti2DDataset
 from .kitti_dataset import KittiDataset
+from .lyft_dataset import LyftDataset
 from .nuscenes_dataset import NuScenesDataset
 from .pipelines import (GlobalRotScaleTrans, IndoorPointSample,
                         LoadAnnotations3D, LoadPointsFromFile,
@@ -14,9 +15,9 @@ from .sunrgbd_dataset import SUNRGBDDataset
 __all__ = [
     'KittiDataset', 'GroupSampler', 'DistributedGroupSampler',
     'build_dataloader', 'RepeatFactorDataset', 'DATASETS', 'build_dataset',
-    'CocoDataset', 'Kitti2DDataset', 'NuScenesDataset', 'ObjectSample',
-    'RandomFlip3D', 'ObjectNoise', 'GlobalRotScaleTrans', 'PointShuffle',
-    'ObjectRangeFilter', 'PointsRangeFilter', 'Collect3D',
+    'CocoDataset', 'Kitti2DDataset', 'NuScenesDataset', 'LyftDataset',
+    'ObjectSample', 'RandomFlip3D', 'ObjectNoise', 'GlobalRotScaleTrans',
+    'PointShuffle', 'ObjectRangeFilter', 'PointsRangeFilter', 'Collect3D',
     'LoadPointsFromFile', 'NormalizePointsColor', 'IndoorPointSample',
     'LoadAnnotations3D', 'SUNRGBDDataset', 'ScanNetDataset', 'Custom3DDataset'
 ]
