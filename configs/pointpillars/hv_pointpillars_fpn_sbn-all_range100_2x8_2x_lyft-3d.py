644142270463ad9f018e3a051b4d94b67dc6741e
wangtai <wangtai@sensetime.com>
2020-07-04 03:42:02 +0800

    Update configs/pointpillars/hv_pointpillars_secfpn_sbn-all_4x8_2x_lyft-3d.py, configs/regnet/hv_pointpillars_regnet-400mf_secfpn_sbn-all_4x8_2x_lyft-3d.py, configs/_base_/datasets/lyft-3d.py files

---
+++ configs/pointpillars/hv_pointpillars_fpn_sbn-all_range100_2x8_2x_lyft-3d.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/hv_pointpillars_fpn_range100_lyft.py',
+    '../_base_/datasets/range100_lyft-3d.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
