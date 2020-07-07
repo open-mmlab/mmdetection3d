5517c8eb8a191f4f1a727a6ec44064f994263559
liyinhao <liyinhao@sensetime.com>
2020-07-07 18:19:09 +0800

    Openlidar benchmark config

--- configs/benchmark/hv_second_secfpn_4x8_80e_pcdet_kitti-3d-3class.py
+++ configs/benchmark/hv_second_secfpn_4x8_80e_pcdet_kitti-3d-3class.py
@@ -201,21 +201,19 @@ data = dict(
         classes=class_names,
         test_mode=True))
 # optimizer
-lr = 0.0018  # max learning rate
+lr = 0.0003  # max learning rate
 optimizer = dict(type='AdamW', lr=lr, betas=(0.95, 0.99), weight_decay=0.01)
 optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
 lr_config = dict(
     policy='cyclic',
     target_ratio=(10, 1e-4),
     cyclic_times=1,
-    step_ratio_up=0.4,
-)
+    step_ratio_up=0.4)
 momentum_config = dict(
     policy='cyclic',
     target_ratio=(0.85 / 0.95, 1),
     cyclic_times=1,
-    step_ratio_up=0.4,
-)
+    step_ratio_up=0.4)
 checkpoint_config = dict(interval=1)
 evaluation = dict(interval=2)
 # yapf:disable
