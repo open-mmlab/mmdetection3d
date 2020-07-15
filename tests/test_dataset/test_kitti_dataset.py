3f67e1bce666cb22a9bd8206ba4da6c343d327cb
liyinhao <liyinhao@sensetime.com>
2020-07-15 10:04:35 +0800

    Change to 230

--- tests/test_dataset/test_kitti_dataset.py
+++ tests/test_dataset/test_kitti_dataset.py
@@ -81,7 +81,45 @@ def test_getitem():
         [[7.3341, -7.3156, -1.0855, 0.4693, 1.1732, 1.8477, 0.7635],
          [5.6783, -4.5259, -1.6217, 0.4693, 1.1732, 1.8477, 0.5641]])
     expected_gt_labels_3d = torch.tensor([0, 0])
-    assert points.shape == (988, 4)
+    assert points.shape == (230, 4)
     assert torch.allclose(
         gt_bboxes_3d.tensor, expected_gt_bboxes_3d, atol=1e-4)
     assert torch.all(gt_labels_3d == expected_gt_labels_3d)
+
+
+# def test_evaluate():
+#     from mmdet3d.core.bbox import LiDARInstance3DBoxes
+#     data_root = 'tests/data/kitti'
+#     ann_file = 'tests/data/kitti/kitti_infos_train.pkl'
+#     classes = ['Pedestrian', 'Cyclist', 'Car']
+#     pts_prefix = 'velodyne_reduced'
+#     pipeline = [
+#         {'type': 'LoadPointsFromFile', 'load_dim': 4, 'use_dim':
+#         4, 'file_client_args': {'backend': 'disk'}},
+#         {'type': 'MultiScaleFlipAug3D', 'img_scale': (1333, 800),
+#         'pts_scale_ratio': 1, 'flip': False, 'transforms': [
+#             {'type': 'GlobalRotScaleTrans', 'rot_range': [0, 0],
+#             'scale_ratio_range': [1.0, 1.0],
+#              'translation_std': [0, 0, 0]}, {'type': 'RandomFlip3D'},
+#             {'type': 'PointsRangeFilter', 'point_cloud_range':
+#             [0, -40, -3, 70.4, 40, 1]},
+#             {'type': 'DefaultFormatBundle3D', 'class_names':
+#             ['Pedestrian', 'Cyclist', 'Car'], 'with_label': False},
+#             {'type': 'Collect3D', 'keys': ['points']}]}]
+#     modality = {'use_lidar': True, 'use_camera': False}
+#     split = 'training'
+#     self = KittiDataset(data_root, ann_file, split, pts_prefix,
+#     pipeline, classes, modality, )
+#     boxes_3d = LiDARInstance3DBoxes([[7.3341, -7.3156, -1.0855,
+#     0.4693, 1.1732, 1.8477, 0.7635],
+#          [5.6783, -4.5259, -1.6217, 0.4693, 1.1732, 1.8477, 0.5641]])
+#     labels_3d = torch.tensor([0, 0])
+#     scores_3d = torch.tensor([0.5, 0.5])
+#     metric = ['mAP']
+#     result = dict(
+#         boxes_3d = boxes_3d,
+#         labels_3d = labels_3d,
+#         scores_3d = scores_3d
+#     )
+#     ap_dict = self.evaluate([result], metric)
+#     print(ap_dict)
