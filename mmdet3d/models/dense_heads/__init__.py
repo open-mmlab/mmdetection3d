f0dfd7369e4503eeaa4b700901fec97c2e1aa92b
zhoujiaming <zhoujiaming@sensetime.com>
2020-08-05 19:22:42 +0800

    register of primitive head

--- mmdet3d/models/dense_heads/__init__.py
+++ mmdet3d/models/dense_heads/__init__.py
@@ -1,6 +1,10 @@
 from .anchor3d_head import Anchor3DHead
 from .free_anchor3d_head import FreeAnchor3DHead
 from .parta2_rpn_head import PartA2RPNHead
+from .primitive_head import PrimitiveHead
 from .vote_head import VoteHead
 
-__all__ = ['Anchor3DHead', 'FreeAnchor3DHead', 'PartA2RPNHead', 'VoteHead']
+__all__ = [
+    'Anchor3DHead', 'FreeAnchor3DHead', 'PartA2RPNHead', 'PrimitiveHead',
+    'VoteHead'
+]
