f554c11eeb13cf2ca6c31e7891fe8cc60563709b
liyinhao <liyinhao@sensetime.com>
2020-08-05 09:07:35 +0800

    Add get_targets.

--- mmdet3d/models/roi_heads/bbox_heads/multi_group_head.py
+++ mmdet3d/models/roi_heads/bbox_heads/multi_group_head.py
@@ -1,4 +1,5 @@
 import copy
+import numpy as np
 import torch
 from collections import defaultdict
 from mmcv.cnn import build_conv_layer, build_norm_layer, kaiming_init
@@ -6,6 +7,58 @@ from torch import nn
 
 from mmdet.models import FeatureAdaption
 from ...builder import HEADS, build_loss
+
+
+def gaussian2D(shape, sigma=1):
+    m, n = [(ss - 1.) / 2. for ss in shape]
+    y, x = np.ogrid[-m:m + 1, -n:n + 1]
+
+    h = np.exp(-(x * x + y * y) / (2 * sigma * sigma))
+    h[h < np.finfo(h.dtype).eps * h.max()] = 0
+    return h
+
+
+def draw_umich_gaussian(heatmap, center, radius, k=1):
+    diameter = 2 * radius + 1
+    gaussian = gaussian2D((diameter, diameter), sigma=diameter / 6)
+
+    x, y = int(center[0]), int(center[1])
+
+    height, width = heatmap.shape[0:2]
+
+    left, right = min(x, radius), min(width - x, radius + 1)
+    top, bottom = min(y, radius), min(height - y, radius + 1)
+
+    masked_heatmap = heatmap[y - top:y + bottom, x - left:x + right]
+    masked_gaussian = gaussian[radius - top:radius + bottom,
+                               radius - left:radius + right]
+    if min(masked_gaussian.shape) > 0 and min(
+            masked_heatmap.shape) > 0:  # TODO debug
+        np.maximum(masked_heatmap, masked_gaussian * k, out=masked_heatmap)
+    return heatmap
+
+
+def gaussian_radius(det_size, min_overlap=0.5):
+    height, width = det_size
+
+    a1 = 1
+    b1 = (height + width)
+    c1 = width * height * (1 - min_overlap) / (1 + min_overlap)
+    sq1 = np.sqrt(b1**2 - 4 * a1 * c1)
+    r1 = (b1 + sq1) / 2
+
+    a2 = 4
+    b2 = 2 * (height + width)
+    c2 = (1 - min_overlap) * width * height
+    sq2 = np.sqrt(b2**2 - 4 * a2 * c2)
+    r2 = (b2 + sq2) / 2
+
+    a3 = 4 * min_overlap
+    b3 = -2 * min_overlap * (height + width)
+    c3 = (min_overlap - 1) * width * height
+    sq3 = np.sqrt(b3**2 - 4 * a3 * c3)
+    r3 = (b3 + sq3) / 2
+    return min(r1, r2, r3)
 
 
 class CenterPointFeatureAdaption(FeatureAdaption):
@@ -259,6 +312,7 @@ class CenterHead(nn.Module):
                 128,
             ],
             tasks=[],
+            train_cfg=None,
             dataset='nuscenes',
             weight=0.25,
             code_weights=[],
@@ -279,6 +333,7 @@ class CenterHead(nn.Module):
         self.code_weights = code_weights
         self.weight = weight  # weight between hm loss and loc loss
         self.dataset = dataset
+        self.train_cfg = train_cfg
 
         self.encode_background_as_zeros = True
         self.use_sigmoid_score = True
@@ -364,6 +419,155 @@ class CenterHead(nn.Module):
             feat = feat[mask]
             feat = feat.view(-1, dim)
         return feat
+
+    def get_targets(self, gt_bboxes_3d, gt_labels_3d, gt_names_3d):
+        max_objs = self._max_objs * self.dense_reg
+        class_names_by_task = [t.class_names for t in self.tasks]
+
+        # Calculate output featuremap size
+        # grid_size = res["lidar"]["voxels"]["shape"]  # 448 x 512
+        grid_size = self.train_cfg['grid_size']  # 448 x 512
+        pc_range = self.train_cfg['point_cloud_range']
+        voxel_size = self.train_cfg['voxel_size']
+
+        feature_map_size = grid_size[:2] // self.out_size_factor
+        example = {}
+
+        # reorganize the gt_dict by tasks
+        task_masks = []
+        flag = 0
+        for class_name in class_names_by_task:
+            # print("classes: ", gt_dict["gt_classes"], "name", class_name)
+            task_masks.append([
+                torch.where(gt_labels_3d == class_name.index(i) + 1 + flag)
+                for i in class_name
+            ])
+            flag += len(class_name)
+
+        task_boxes = []
+        task_classes = []
+        task_names = []
+        flag2 = 0
+        for idx, mask in enumerate(task_masks):
+            task_box = []
+            task_class = []
+            task_name = []
+            for m in mask:
+                task_box.append(gt_bboxes_3d[m])
+                task_class.append(gt_labels_3d[m] - flag2)
+                task_name.append(gt_names_3d[m])
+            task_boxes.append(torch.cat(task_box, axis=0))
+            task_classes.append(torch.cat(task_class))
+            task_names.append(torch.cat(task_name))
+            flag2 += len(mask)
+
+        # for task_box in task_boxes:
+        #     # limit rad to [-pi, pi]
+        #     task_box[:, -1] = box_np_ops.limit_period(
+        #         task_box[:, -1], offset=0.5, period=np.pi * 2
+        #     )
+
+        # print(gt_dict.keys())
+        # gt_dict["gt_classes"] = task_classes
+        # gt_dict["gt_names"] = task_names
+        # gt_dict["gt_boxes"] = task_boxes
+
+        # res["lidar"]["annotations"] = gt_dict
+
+        draw_gaussian = draw_umich_gaussian
+
+        hms, anno_boxes, inds, masks, cats = [], [], [], [], []
+
+        for idx, task in enumerate(self.tasks):
+            hm = torch.zeros((len(class_names_by_task[idx]),
+                              feature_map_size[1], feature_map_size[0]),
+                             dtype=torch.float32)
+
+            anno_box = torch.zeros((max_objs, 10), dtype=torch.float32)
+
+            ind = torch.zeros((max_objs), dtype=torch.int64)
+            mask = torch.zeros((max_objs), dtype=torch.uint8)
+            cat = torch.zeros((max_objs), dtype=torch.int64)
+
+            num_objs = min(task_boxes[idx].shape[0], max_objs)
+
+            for k in range(num_objs):
+                cls_id = task_classes[idx][k] - 1
+
+                width, length, _ = task_boxes[idx][k][3], task_boxes[idx][k][
+                    4], task_boxes[idx][k][5]
+                width, length = width / voxel_size[
+                    0] / self.out_size_factor, length / voxel_size[
+                        1] / self.out_size_factor
+                if width > 0 and length > 0:
+                    radius = gaussian_radius((length, width),
+                                             min_overlap=self.gaussian_overlap)
+                    radius = max(self._min_radius, int(radius))
+
+                    # be really careful for the coordinate system of
+                    # your box annotation.
+                    x, y, z = task_boxes[idx][k][0], task_boxes[idx][k][
+                        1], task_boxes[idx][k][2]
+
+                    coor_x, coor_y = (
+                        x - pc_range[0]
+                    ) / voxel_size[0] / self.out_size_factor, (
+                        y - pc_range[1]) / voxel_size[1] / self.out_size_factor
+
+                    ct = torch.tensor([coor_x, coor_y], dtype=torch.float32)
+                    ct_int = ct.astype(torch.int32)
+
+                    # throw out not in range objects to avoid out of array
+                    # area when creating the heatmap
+                    if not (0 <= ct_int[0] < feature_map_size[0]
+                            and 0 <= ct_int[1] < feature_map_size[1]):
+                        continue
+
+                    draw_gaussian(hm[cls_id], ct, radius)
+
+                    new_idx = k
+                    x, y = ct_int[0], ct_int[1]
+
+                    if not (y * feature_map_size[0] + x <
+                            feature_map_size[0] * feature_map_size[1]):
+                        # a double check, should never happen
+                        print(x, y, y * feature_map_size[0] + x)
+                        assert False
+
+                    cat[new_idx] = cls_id
+                    ind[new_idx] = y * feature_map_size[0] + x
+                    mask[new_idx] = 1
+
+                    vx, vy = task_boxes[idx][k][6:8]
+                    rot = task_boxes[idx][k][8]
+                    if not self.no_log:
+                        anno_box[new_idx] = torch.cat(
+                            (ct - (x, y), z, torch.log(
+                                task_boxes[idx][k][3:6]), torch.tensor(vx),
+                             torch.tensor(vy), torch.sin(rot), torch.cos(rot)),
+                            axis=None)
+                    else:
+                        anno_box[new_idx] = torch.cat(
+                            (ct - (x, y), z, task_boxes[idx][k][3:6],
+                             torch.tensor(vx), torch.tensor(vy),
+                             torch.sin(rot), torch.cos(rot)),
+                            axis=None)
+
+            hms.append(hm)
+            anno_boxes.append(anno_box)
+            masks.append(mask)
+            inds.append(ind)
+            cats.append(cat)
+
+        example.update({
+            'hm': hms,
+            'anno_box': anno_boxes,
+            'ind': inds,
+            'mask': masks,
+            'cat': cats
+        })
+
+        return hms, anno_boxes, inds, masks, cats
 
     def loss(self, example, preds_dicts, **kwargs):
         """Loss function for CenterHead.
