b5b18a1e3017ca6c0d54d11ffe8c44ff51c18ea6
zhoujiaming <zhoujiaming@sensetime.com>
2020-08-17 16:24:28 +0800

    add op fps with distance

--- mmdet3d/ops/furthest_point_sample/__init__.py
+++ mmdet3d/ops/furthest_point_sample/__init__.py
@@ -1,3 +1,4 @@
-from .furthest_point_sample import furthest_point_sample
+from .furthest_point_sample import (furthest_point_sample,
+                                    furthest_point_sample_with_dist)
 
-__all__ = ['furthest_point_sample']
+__all__ = ['furthest_point_sample', 'furthest_point_sample_with_dist']
--- mmdet3d/ops/furthest_point_sample/furthest_point_sample.py
+++ mmdet3d/ops/furthest_point_sample/furthest_point_sample.py
@@ -39,4 +39,50 @@ class FurthestPointSampling(Function):
         return None, None
 
 
+class FurthestPointSamplingWithDist(Function):
+    """Furthest Point Sampling With Distance.
+
+    Uses iterative furthest point sampling to select a set of features whose
+    corresponding points have the furthest distance.
+    """
+
+    @staticmethod
+    def forward(ctx, points_dist: torch.Tensor,
+                num_points: int) -> torch.Tensor:
+        """forward.
+
+        Args:
+            points_dist (Tensor): (B, N, N) Distance between each point pair.
+            num_points (int): Number of points in the sampled set.
+
+        Returns:
+             Tensor: (B, num_points) indices of the sampled points.
+        """
+        assert points_dist.is_contiguous()
+
+        B, N, _ = points_dist.size()
+        output = torch.cuda.IntTensor(B, num_points)
+        temp = torch.cuda.FloatTensor(B, N).fill_(1e10)
+
+        furthest_point_sample_ext.furthest_point_sampling_with_dist_wrapper(
+            B, N, num_points, points_dist, temp, output)
+        ctx.mark_non_differentiable(output)
+        return output
+
+    @staticmethod
+    def backward(xyz, a=None):
+        return None, None
+
+
 furthest_point_sample = FurthestPointSampling.apply
+furthest_point_sample_with_dist = FurthestPointSamplingWithDist.apply
+
+if __name__ == '__main__':
+    import numpy as np
+    fps_idx = np.load('fps_idx.npy')
+    features_for_fps_distance = np.load('features_for_fps_distance.npy')
+    fps_idx = torch.from_numpy(fps_idx).cuda()
+    features_for_fps_distance = torch.from_numpy(
+        features_for_fps_distance).cuda()
+    fps_idx_t = furthest_point_sample_with_dist(features_for_fps_distance, 512)
+    assert (fps_idx_t - fps_idx).sum() == 0
