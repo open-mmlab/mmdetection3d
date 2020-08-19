0fef1f8eec23059afa3896b187b69df0b8cc9410
zhoujiaming <zhoujiaming@sensetime.com>
2020-08-19 20:53:40 +0800

    add h3dnet init files

--- mmdet3d/models/detectors/__init__.py
+++ mmdet3d/models/detectors/__init__.py
@@ -1,5 +1,6 @@
 from .base import Base3DDetector
 from .dynamic_voxelnet import DynamicVoxelNet
+from .h3dnet import H3DNet
 from .mvx_faster_rcnn import DynamicMVXFasterRCNN, MVXFasterRCNN
 from .mvx_two_stage import MVXTwoStageDetector
 from .parta2 import PartA2
@@ -8,5 +9,5 @@ from .voxelnet import VoxelNet
 
 __all__ = [
     'Base3DDetector', 'VoxelNet', 'DynamicVoxelNet', 'MVXTwoStageDetector',
-    'DynamicMVXFasterRCNN', 'MVXFasterRCNN', 'PartA2', 'VoteNet'
+    'DynamicMVXFasterRCNN', 'MVXFasterRCNN', 'PartA2', 'VoteNet', 'H3DNet'
 ]
