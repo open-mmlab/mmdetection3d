38e961cb535506d7269ed5fa1313a4bf46117b17
twang <30491025+Tai-Wang@users.noreply.github.com>
2020-08-19 19:57:53 +0800

    Fix ambiguous initialization of cuda tensors

--- mmdet3d/ops/iou3d/iou3d_utils.py
+++ mmdet3d/ops/iou3d/iou3d_utils.py
@@ -13,8 +13,8 @@ def boxes_iou_bev(boxes_a, boxes_b):
     Returns:
         ans_iou (torch.Tensor): IoU result with shape (M, N).
     """
-    ans_iou = torch.cuda.FloatTensor(
-        torch.Size((boxes_a.shape[0], boxes_b.shape[0]))).zero_()
+    ans_iou = boxes_a.new_zeros(
+        torch.Size((boxes_a.shape[0], boxes_b.shape[0])))
 
     iou3d_cuda.boxes_iou_bev_gpu(boxes_a.contiguous(), boxes_b.contiguous(),
                                  ans_iou)
@@ -37,7 +37,7 @@ def nms_gpu(boxes, scores, thresh):
 
     boxes = boxes[order].contiguous()
 
-    keep = torch.LongTensor(boxes.size(0))
+    keep = boxes.new_zeros(boxes.size(0))
     num_out = iou3d_cuda.nms_gpu(boxes, keep, thresh, boxes.device.index)
     return order[keep[:num_out].cuda(boxes.device)].contiguous()
 
@@ -57,7 +57,7 @@ def nms_normal_gpu(boxes, scores, thresh):
 
     boxes = boxes[order].contiguous()
 
-    keep = torch.LongTensor(boxes.size(0))
+    keep = boxes.new_zeros(boxes.size(0))
     num_out = iou3d_cuda.nms_normal_gpu(boxes, keep, thresh,
                                         boxes.device.index)
     return order[keep[:num_out].cuda(boxes.device)].contiguous()
