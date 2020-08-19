7ebedabbbcc39ed6d5dbea5e51db7566ddc30bb2
liyinhao <liyinhao@sensetime.com>
2020-08-19 15:27:52 +0800

    Change nus_info.pkl.

--- tests/test_dataset/test_nuscene_dataset.py
+++ tests/test_dataset/test_nuscene_dataset.py
@@ -1,5 +1,4 @@
 import numpy as np
-import torch
 
 from mmdet3d.datasets import NuScenesDataset
 
@@ -22,27 +21,40 @@ def test_getitem():
             type='LoadPointsFromMultiSweeps',
             sweeps_num=10,
             file_client_args=file_client_args),
-        dict(type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
         dict(
-            type='GlobalRotScaleTrans',
-            rot_range=[-0.3925, 0.3925],
-            scale_ratio_range=[0.95, 1.05],
-            translation_std=[0, 0, 0]),
-        dict(type='RandomFlip3D', flip_ratio_bev_horizontal=0.5),
-        dict(type='PointsRangeFilter', point_cloud_range=point_cloud_range),
-        dict(type='ObjectRangeFilter', point_cloud_range=point_cloud_range),
-        dict(type='ObjectNameFilter', classes=class_names),
-        dict(type='PointShuffle'),
-        dict(type='DefaultFormatBundle3D', class_names=class_names),
-        dict(
-            type='Collect3D', keys=['points', 'gt_bboxes_3d', 'gt_labels_3d'])
+            type='MultiScaleFlipAug3D',
+            img_scale=(1333, 800),
+            pts_scale_ratio=1,
+            flip=False,
+            transforms=[
+                dict(
+                    type='GlobalRotScaleTrans',
+                    rot_range=[0, 0],
+                    scale_ratio_range=[1., 1.],
+                    translation_std=[0, 0, 0]),
+                dict(type='RandomFlip3D'),
+                dict(
+                    type='PointsRangeFilter',
+                    point_cloud_range=point_cloud_range),
+                dict(
+                    type='DefaultFormatBundle3D',
+                    class_names=class_names,
+                    with_label=False),
+                dict(type='Collect3D', keys=['points'])
+            ])
     ]
 
-    nus_dataset = NuScenesDataset('tests/data/nuscenes/nus_info.pkl', pipeline,
-                                  'tests/data/nuscenes')
+    nus_dataset = NuScenesDataset(
+        'tests/data/nuscenes/nus_info.pkl',
+        pipeline,
+        'tests/data/nuscenes',
+        test_mode=True)
     data = nus_dataset[0]
-    assert data['img_metas'].data['flip'] is True
-    assert data['img_metas'].data['pcd_horizontal_flip'] is True
-    assert data['points']._data.shape == (100, 4)
-    assert data['gt_bboxes_3d']._data.tensor.shape == (35, 9)
-    assert data['gt_labels_3d']._data.shape == torch.Size([35])
+    assert data['img_metas'][0].data['flip'] is False
+    assert data['img_metas'][0].data['pcd_horizontal_flip'] is False
+    assert data['points'][0]._data.shape == (100, 4)
+
+    data = nus_dataset[1]
+    assert data['img_metas'][0].data['flip'] is False
+    assert data['img_metas'][0].data['pcd_horizontal_flip'] is False
+    assert data['points'][0]._data.shape == (597, 4)
