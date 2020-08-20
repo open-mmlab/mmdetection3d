3ea408f92ae1a9902aacc23c5592513d6821df55
StarGazer1995 <gz19951230@outlook.com>
2020-08-20 15:39:53 +0800

    update:create config file for the special course

---
+++ configs/votenet/votenet_16x8_sunrgbd-3d-10class_with_att.py
@@ -0,0 +1,21 @@
+_base_ = [
+    '../_base_/datasets/sunrgbd-3d-10class.py', '../_base_/models/votenet.py',
+    '../_base_/schedules/schedule_3x.py', '../_base_/default_runtime.py'
+]
+# model settings
+model = dict(
+    bbox_head=dict(
+        num_classes=10,
+        bbox_coder=dict(
+            type='PartialBinBasedBBoxCoder',
+            num_sizes=10,
+            num_dir_bins=12,
+            with_rot=True,
+            mean_sizes=[
+                [2.114256, 1.620300, 0.927272], [0.791118, 1.279516, 0.718182],
+                [0.923508, 1.867419, 0.845495], [0.591958, 0.552978, 0.827272],
+                [0.699104, 0.454178, 0.75625], [0.69519, 1.346299, 0.736364],
+                [0.528526, 1.002642, 1.172878], [0.500618, 0.632163, 0.683424],
+                [0.404671, 1.071108, 1.688889], [0.76584, 1.398258, 0.472728]
+            ]),
+    ))
