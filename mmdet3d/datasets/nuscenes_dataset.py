bd3bd97cdc443da9d0f78ab15d5bf0881ac79b98
liyinhao <liyinhao@sensetime.com>
2020-08-22 15:33:54 +0800

    Add bottom2gravity and balance_class option.

--- mmdet3d/datasets/nuscenes_dataset.py
+++ mmdet3d/datasets/nuscenes_dataset.py
@@ -111,8 +111,11 @@ class NuScenesDataset(Custom3DDataset):
                  box_type_3d='LiDAR',
                  filter_empty_gt=True,
                  test_mode=False,
-                 eval_version='detection_cvpr_2019'):
+                 bottom2gravity=False,
+                 eval_version='detection_cvpr_2019',
+                 balance_class=False):
         self.load_interval = load_interval
+        self.balance_class = balance_class
         super().__init__(
             data_root=data_root,
             ann_file=ann_file,
@@ -127,7 +130,7 @@ class NuScenesDataset(Custom3DDataset):
         self.eval_version = eval_version
         from nuscenes.eval.detection.config import config_factory
         self.eval_detection_configs = config_factory(self.eval_version)
-
+        self.bottom2gravity = bottom2gravity
         if self.modality is None:
             self.modality = dict(
                 use_camera=False,
@@ -147,8 +150,39 @@ class NuScenesDataset(Custom3DDataset):
             list[dict]: List of annotations sorted by timestamps.
         """
         data = mmcv.load(ann_file)
-        data_infos = list(sorted(data['infos'], key=lambda e: e['timestamp']))
-        data_infos = data_infos[::self.load_interval]
+        if self.balance_class is False:
+            data_infos = list(
+                sorted(data['infos'], key=lambda e: e['timestamp']))
+            data_infos = data_infos[::self.load_interval]
+        elif not self.test_mode:  # if training
+
+            _cls_infos = {name: [] for name in self.CLASSES}
+            for info in data['infos']:
+                for name in set(info['gt_names']):
+                    if name in self.CLASSES:
+                        _cls_infos[name].append(info)
+            duplicated_samples = sum([len(v) for _, v in _cls_infos.items()])
+            _cls_dist = {
+                k: len(v) / duplicated_samples
+                for k, v in _cls_infos.items()
+            }
+
+            data_infos = []
+
+            frac = 1.0 / len(self.CLASSES)
+            ratios = [frac / v for v in _cls_dist.values()]
+            for cls_infos, ratio in zip(list(_cls_infos.values()), ratios):
+                # data_infos += np.random.choice(
+                #     cls_infos, int(len(cls_infos) * ratio)
+                # ).tolist()
+                data_infos += cls_infos[:int(len(cls_infos) * ratio)]
+        else:
+            if isinstance(data['infos'], dict):
+                data_infos = []
+                for v in data['infos'].values():
+                    data_infos.extend(v)
+            else:
+                data_infos = data['infos']
         self.metadata = data['metadata']
         self.version = self.metadata['version']
         return data_infos
@@ -247,6 +281,8 @@ class NuScenesDataset(Custom3DDataset):
 
         # the nuscenes box center is [0.5, 0.5, 0.5], we change it to be
         # the same as KITTI (0.5, 0.5, 0)
+        if self.bottom2gravity:
+            gt_bboxes_3d[:, 2] = gt_bboxes_3d[:, 2] + gt_bboxes_3d[:, 5] * 0.5
         gt_bboxes_3d = LiDARInstance3DBoxes(
             gt_bboxes_3d,
             box_dim=gt_bboxes_3d.shape[-1],
