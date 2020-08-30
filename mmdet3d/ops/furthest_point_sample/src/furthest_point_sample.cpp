299d666a47cfeee4071959d5a2f2e9991723bee5
encore-zhou <zhoujiaming@sensetime.com>
2020-08-30 20:03:45 +0800

    Feature_3dssd_FPS_With_Dist_OP (#66)
    * add op fps with distance
    
    * add op fps with distance
    
    * modify F-DFS unittest
    
    * modify sa module
    
    * modify sa module
    
    * SA Module support D-FPS and F-FPS
    
    * modify sa module
    
    * update points sa module
    
    * modify point_sa_module
    
    * modify point sa module
    
    * reconstruct FPS
    
    * reconstruct FPS
    
    * modify docstring
    
    * modify docstring

--- mmdet3d/ops/furthest_point_sample/src/furthest_point_sample.cpp
+++ mmdet3d/ops/furthest_point_sample/src/furthest_point_sample.cpp
@@ -19,6 +19,16 @@ void furthest_point_sampling_kernel_launcher(int b, int n, int m,
                                              const float *dataset, float *temp,
                                              int *idxs, cudaStream_t stream);
 
+int furthest_point_sampling_with_dist_wrapper(int b, int n, int m,
+                                              at::Tensor points_tensor,
+                                              at::Tensor temp_tensor,
+                                              at::Tensor idx_tensor);
+
+void furthest_point_sampling_with_dist_kernel_launcher(int b, int n, int m,
+                                                       const float *dataset,
+                                                       float *temp, int *idxs,
+                                                       cudaStream_t stream);
+
 int furthest_point_sampling_wrapper(int b, int n, int m,
                                     at::Tensor points_tensor,
                                     at::Tensor temp_tensor,
@@ -32,7 +42,24 @@ int furthest_point_sampling_wrapper(int b, int n, int m,
   return 1;
 }
 
+int furthest_point_sampling_with_dist_wrapper(int b, int n, int m,
+                                              at::Tensor points_tensor,
+                                              at::Tensor temp_tensor,
+                                              at::Tensor idx_tensor) {
+
+  const float *points = points_tensor.data<float>();
+  float *temp = temp_tensor.data<float>();
+  int *idx = idx_tensor.data<int>();
+
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
+  furthest_point_sampling_with_dist_kernel_launcher(b, n, m, points, temp, idx, stream);
+  return 1;
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("furthest_point_sampling_wrapper", &furthest_point_sampling_wrapper,
         "furthest_point_sampling_wrapper");
+  m.def("furthest_point_sampling_with_dist_wrapper",
+        &furthest_point_sampling_with_dist_wrapper,
+        "furthest_point_sampling_with_dist_wrapper");
 }
