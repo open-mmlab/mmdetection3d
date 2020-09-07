309760aebae0bc831c56e5e06c48237a46810ef6
liyinhao <liyinhao@sensetime.com>
2020-09-07 21:53:17 +0800

    Add centerpoint.

--- mmdet3d/models/detectors/__init__.py
+++ mmdet3d/models/detectors/__init__.py
@@ -1,4 +1,5 @@
 from .base import Base3DDetector
+from .centerpoint import CenterPoint
 from .dynamic_voxelnet import DynamicVoxelNet
 from .h3dnet import H3DNet
 from .mvx_faster_rcnn import DynamicMVXFasterRCNN, MVXFasterRCNN
@@ -9,5 +10,6 @@ from .voxelnet import VoxelNet
 
 __all__ = [
     'Base3DDetector', 'VoxelNet', 'DynamicVoxelNet', 'MVXTwoStageDetector',
-    'DynamicMVXFasterRCNN', 'MVXFasterRCNN', 'PartA2', 'VoteNet', 'H3DNet'
+    'DynamicMVXFasterRCNN', 'MVXFasterRCNN', 'PartA2', 'VoteNet',
+    'CenterPoint', 'H3DNet'
 ]
