35f508a7992b650e37bb25babd36a723173a042b
liyinhao <liyinhao@sensetime.com>
2020-09-07 17:39:54 +0800

    Add 0075 voxel configs.

--- configs/_base_/models/centerpoint_0075_voxel_circle_nms.py
+++ configs/_base_/models/centerpoint_0075_voxel_circle_nms.py
@@ -1,42 +1,43 @@
-voxel_size = [0.2, 0.2, 8]
+voxel_size = [0.075, 0.075, 0.2]
+point_cloud_range = [-54, -54, -5.0, 54, 54, 3.0]
 model = dict(
     type='CenterPoint',
     pts_voxel_layer=dict(
-        max_num_points=20,
-        point_cloud_range=[-51.2, -51.2, -5.0, 51.2, 51.2, 3.0],
+        max_num_points=10,
+        point_cloud_range=point_cloud_range,
         voxel_size=voxel_size,
-        max_voxels=(30000, 40000)),
-    pts_voxel_encoder=dict(
-        type='PillarFeatureNet',
-        in_channels=5,
-        feat_channels=[64],
-        with_distance=False,
-        voxel_size=(0.2, 0.2, 8),
-        point_cloud_range=(-51.2, -51.2, -5.0, 51.2, 51.2, 3.0),
-        norm_cfg=dict(type='BN1d', eps=1e-3, momentum=0.01),
-        legacy=False),
+        max_voxels=(90000, 120000)),
+    pts_voxel_encoder=dict(type='HardSimpleVFE', num_features=5),
     pts_middle_encoder=dict(
-        type='PointPillarsScatter', in_channels=64, output_shape=(512, 512)),
+        type='SparseEncoder',
+        in_channels=5,
+        sparse_shape=[41, 1440, 1440],
+        output_channels=128,
+        order=('conv', 'norm', 'act'),
+        encoder_channels=((16, 16, 32), (32, 32, 64), (64, 64, 128), (128,
+                                                                      128)),
+        encoder_paddings=((0, 0, 1), (0, 0, 1), (0, 0, [0, 1, 1]), (0, 0)),
+        block_type='basicblock'),
     pts_backbone=dict(
         type='SECOND',
-        in_channels=64,
-        out_channels=[64, 128, 256],
-        layer_nums=[3, 5, 5],
-        layer_strides=[2, 2, 2],
+        in_channels=256,
+        out_channels=[128, 256],
+        layer_nums=[5, 5],
+        layer_strides=[1, 2],
         norm_cfg=dict(type='BN', eps=1e-3, momentum=0.01),
         conv_cfg=dict(type='Conv2d', bias=False)),
     pts_neck=dict(
         type='SECONDFPN',
-        in_channels=[64, 128, 256],
-        out_channels=[128, 128, 128],
-        upsample_strides=[0.5, 1, 2],
+        in_channels=[128, 256],
+        out_channels=[256, 256],
+        upsample_strides=[1, 2],
         norm_cfg=dict(type='BN', eps=1e-3, momentum=0.01),
         upsample_cfg=dict(type='deconv', bias=False),
         use_conv_for_no_stride=True),
     pts_bbox_head=dict(
         type='CenterHead',
         mode='3d',
-        in_channels=sum([128, 128, 128]),
+        in_channels=sum([256, 256]),
         tasks=[
             dict(num_class=1, class_names=['car']),
             dict(num_class=2, class_names=['truck', 'construction_vehicle']),
@@ -58,8 +59,8 @@ model = dict(
             post_center_range=[-61.2, -61.2, -10.0, 61.2, 61.2, 10.0],
             max_num=500,
             score_threshold=0.1,
-            pc_range=[-51.2, -51.2],
-            out_size_factor=4,
+            pc_range=point_cloud_range[:2],
+            out_size_factor=8,
             voxel_size=voxel_size[:2],
             code_size=9),
         dcn_head=False,
@@ -68,16 +69,16 @@ model = dict(
 # model training and testing settings
 train_cfg = dict(
     pts=dict(
-        grid_size=[512, 512, 1],
-        point_cloud_range=[-51.2, -51.2, -5., 51.2, 51.2, 3.],
+        grid_size=[1440, 1440, 40],
+        point_cloud_range=point_cloud_range,
         voxel_size=voxel_size,
-        out_size_factor=4,
+        out_size_factor=8,
         dense_reg=1,
         gaussian_overlap=0.1,
         max_objs=500,
         min_radius=2,
-        no_log=False,
-        code_weights=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.2, 0.2, 1.0, 1.0]))
+        code_weights=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.2, 0.2, 1.0, 1.0],
+        no_log=False))
 test_cfg = dict(
     pts=dict(
         post_center_limit_range=[-61.2, -61.2, -10.0, 61.2, 61.2, 10.0],
@@ -86,11 +87,8 @@ test_cfg = dict(
         min_radius=[4, 12, 10, 1, 0.85, 0.175],
         post_max_size=83,
         score_threshold=0.1,
-        pc_range=[-51.2, -51.2],
-        out_size_factor=4,
+        pc_range=point_cloud_range[:2],
+        out_size_factor=8,
         voxel_size=voxel_size[:2],
-        nms_type='rotate',
-        nms_pre_max_size=1000,
-        nms_post_max_size=83,
-        nms_iou_threshold=0.2,
+        nms_type='circle',
         no_log=False))
