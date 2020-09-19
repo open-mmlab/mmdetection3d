1bc57c8b374273049b77de4fb2886c8bdf1a9196
liyinhao <liyinhao@sensetime.com>
2020-09-19 10:51:35 +0800

    Add cyclic_20e.py

--- configs/centerpoint/centerpoint_02pillar_second_secfpn_4x8_cyclic_20e_nus.py
+++ configs/centerpoint/centerpoint_02pillar_second_secfpn_4x8_cyclic_20e_nus.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/datasets/nus-3d.py',
     '../_base_/models/centerpoint_02pillar_second_secfpn_nus.py',
-    '../_base_/default_runtime.py'
+    '../_base_/schedules/cyclic_20e.py', '../_base_/default_runtime.py'
 ]
 
 # If point cloud range is changed, the models should also change their point
