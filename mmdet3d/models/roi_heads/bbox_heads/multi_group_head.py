9b3f6ec540328915a4d912ab4d6ccce8fa7d90aa
liyinhao <liyinhao@sensetime.com>
2020-09-19 11:55:26 +0800

    Change bbox order in rotate nms.

--- mmdet3d/models/roi_heads/bbox_heads/multi_group_head.py
+++ mmdet3d/models/roi_heads/bbox_heads/multi_group_head.py
@@ -770,8 +770,7 @@ class CenterHead(nn.Module):
                     top_labels = top_labels[top_scores_keep]
 
                 boxes_for_nms = xywhr2xyxyr(img_metas[i]['box_type_3d'](
-                    box_preds[:, [0, 1, 2, 3, 4, 5, 8, 6, 7]],
-                    self.bbox_coder.code_size).bev)
+                    box_preds[:, :], self.bbox_coder.code_size).bev)
                 # the nms in 3d detection just remove overlap boxes.
 
                 selected = nms_gpu(
