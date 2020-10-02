f97fc70679555bb92486ba984360cfc3017735e4
liyinhao <liyinhao@sensetime.com>
2020-10-02 21:32:45 +0800

    Able to merge then nms for tta.

--- mmdet3d/models/detectors/centerpoint.py
+++ mmdet3d/models/detectors/centerpoint.py
@@ -1,4 +1,6 @@
-from mmdet3d.core import bbox3d2result, merge_aug_bboxes_3d
+import torch
+
+from mmdet3d.core import bbox3d2result
 from mmdet.models import DETECTORS
 from .mvx_two_stage import MVXTwoStageDetector
 
@@ -72,29 +74,62 @@ class CenterPoint(MVXTwoStageDetector):
     def simple_test_pts(self, x, img_metas, rescale=False):
         """Test function of point cloud branch."""
         outs = self.pts_bbox_head(x)
+        import pdb
+        pdb.set_trace()
         bbox_list = self.pts_bbox_head.get_bboxes(
             outs, img_metas, rescale=rescale)
         bbox_results = [
             bbox3d2result(bboxes, scores, labels)
             for bboxes, scores, labels in bbox_list
         ]
+        pdb.set_trace()
         return bbox_results
 
     def aug_test_pts(self, feats, img_metas, rescale=False):
         """Test function of point cloud branch with augmentaiton."""
         # only support aug_test for one sample
-        aug_bboxes = []
+        outs_list = []
         for x, img_meta in zip(feats, img_metas):
             outs = self.pts_bbox_head(x)
-            bbox_list = self.pts_bbox_head.get_bboxes(
-                outs, img_meta, rescale=rescale)
-            bbox_list = [
-                dict(boxes_3d=bboxes, scores_3d=scores, labels_3d=labels)
-                for bboxes, scores, labels in bbox_list
-            ]
-            aug_bboxes.append(bbox_list[0])
+            for task_id, out in enumerate(outs):
+                for key in out[0].keys():
+                    if img_meta[0]['pcd_horizontal_flip']:
+                        outs[task_id][0][key] = torch.flip(
+                            outs[task_id][0][key], dims=[2])
+                    if img_meta[0]['pcd_vertical_flip']:
+                        outs[task_id][0][key] = torch.flip(
+                            outs[task_id][0][key], dims=[3])
+            outs_list.append(outs)
 
-        # after merging, bboxes will be rescaled to the original image size
-        merged_bboxes = merge_aug_bboxes_3d(aug_bboxes, img_metas,
-                                            self.pts_bbox_head.test_cfg)
-        return merged_bboxes
+        preds_dict = outs_list[0]
+        for i, outs in enumerate(outs_list):
+            if i == 0:
+                preds_dict = outs
+            else:
+                for task_id, out in enumerate(outs):
+                    for key in out[0].keys():
+                        preds_dict[task_id][0][key] += out[0][key]
+        for task_id, pred_dict in enumerate(preds_dict):
+            for key in pred_dict[0].keys():
+                preds_dict[task_id][0][key] /= 4
+
+        bbox_list = self.pts_bbox_head.get_bboxes(
+            preds_dict, img_metas[0], rescale=rescale)
+        bbox_results = [
+            bbox3d2result(bboxes, scores, labels)
+            for bboxes, scores, labels in bbox_list
+        ]
+        return bbox_results
+
+    def aug_test(self, points, img_metas, imgs=None, rescale=False):
+        """Test function with augmentaiton."""
+        img_feats, pts_feats = self.extract_feats(points, img_metas, imgs)
+        # import pdb
+        # pdb.set_trace()
+        bbox_list = dict()
+        if pts_feats and self.with_pts_bbox:
+            bbox_pts = self.aug_test_pts(pts_feats, img_metas, rescale)
+            bbox_list.update(pts_bbox=bbox_pts)
+        import pdb
+        pdb.set_trace()
+        return [[bbox_list]]
