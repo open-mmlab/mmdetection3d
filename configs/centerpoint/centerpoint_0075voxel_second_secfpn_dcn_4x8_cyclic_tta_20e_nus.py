1e3df83b8806fd16c74303666af17ed3c676b165
liyinhao <liyinhao@sensetime.com>
2020-10-15 17:02:48 +0800

    Delete circle_nms tta config, change max_num.

--- configs/centerpoint/centerpoint_0075voxel_second_secfpn_dcn_4x8_cyclic_tta_20e_nus.py
+++ configs/centerpoint/centerpoint_0075voxel_second_secfpn_dcn_4x8_cyclic_tta_20e_nus.py
@@ -1,6 +1,6 @@
 _base_ = './centerpoint_0075voxel_second_secfpn_dcn_4x8_cyclic_20e_nus.py'
 
-test_cfg = dict(pts=dict(use_rotate_nms=True, max_num=83))
+test_cfg = dict(pts=dict(use_rotate_nms=True, max_num=500))
 
 point_cloud_range = [-54, -54, -5.0, 54, 54, 3.0]
 file_client_args = dict(backend='disk')
