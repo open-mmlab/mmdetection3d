aa623ea753e2cfdb73e89eb4d86357fc137153b5
liyinhao <liyinhao@sensetime.com>
2020-10-20 22:40:30 +0800

    Add comment for double flip change.

--- mmdet3d/datasets/pipelines/test_time_aug.py
+++ mmdet3d/datasets/pipelines/test_time_aug.py
@@ -73,6 +73,10 @@ class MultiScaleFlipAug3D(object):
                 different scales and flips.
         """
         aug_data = []
+
+        # modified from `flip_aug = [False, True] if self.flip else [False]`
+        # to reduce unnecessary scenes when using double flip augmentation
+        # during test time
         flip_aug = [True] if self.flip else [False]
         pcd_horizontal_flip_aug = [False, True] \
             if self.flip and self.pcd_horizontal_flip else [False]
