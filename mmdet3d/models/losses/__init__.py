2040080cc08235d5e175eff5153973638c0b0fb1
THU17cyz <congyezhen71@hotmail.com>
2020-11-10 16:07:58 +0800

    rename and format fix

--- mmdet3d/models/losses/__init__.py
+++ mmdet3d/models/losses/__init__.py
@@ -1,6 +1,6 @@
 from mmdet.models.losses import FocalLoss, SmoothL1Loss, binary_cross_entropy
+from .axis_aligned_iou_loss import AxisAlignedIoULoss, axis_aligned_iou_loss
 from .chamfer_distance import ChamferDistance, chamfer_distance
-from .iou_loss_axis_aligned import AxisAlignedIoULoss, axis_aligned_iou_loss
 
 __all__ = [
     'FocalLoss', 'SmoothL1Loss', 'binary_cross_entropy', 'ChamferDistance',
