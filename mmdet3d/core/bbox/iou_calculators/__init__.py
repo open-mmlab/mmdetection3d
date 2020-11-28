04cf892994ff98a098a4092a085c76d402a65b6c
Yezhen Cong <52420115+THU17cyz@users.noreply.github.com>
2020-11-28 22:43:47 +0800

    Added axis-aligned IoU loss to VoteNet (#194)
    * support axis-aligned iou loss for votenet
    
    * added doc for iou loss
    
    * fixed problems in format
    
    * updated docstring
    
    * rename and format fix
    
    * rename and format fix
    
    * rename and format fix
    
    * rename and format fix
    
    * modified config
    
    * abstracted axis_aligned_iou3d
    
    * abstracted a bbox corner decode func
    
    * fix docstring

--- mmdet3d/core/bbox/iou_calculators/__init__.py
+++ mmdet3d/core/bbox/iou_calculators/__init__.py
@@ -1,7 +1,10 @@
-from .iou3d_calculator import (BboxOverlaps3D, BboxOverlapsNearest3D,
-                               bbox_overlaps_3d, bbox_overlaps_nearest_3d)
+from .iou3d_calculator import (AxisAlignedBboxOverlaps3D, BboxOverlaps3D,
+                               BboxOverlapsNearest3D,
+                               axis_aligned_bbox_overlaps_3d, bbox_overlaps_3d,
+                               bbox_overlaps_nearest_3d)
 
 __all__ = [
     'BboxOverlapsNearest3D', 'BboxOverlaps3D', 'bbox_overlaps_nearest_3d',
-    'bbox_overlaps_3d'
+    'bbox_overlaps_3d', 'AxisAlignedBboxOverlaps3D',
+    'axis_aligned_bbox_overlaps_3d'
 ]
--- mmdet3d/models/losses/__init__.py
+++ mmdet3d/models/losses/__init__.py
@@ -1,7 +1,8 @@
 from mmdet.models.losses import FocalLoss, SmoothL1Loss, binary_cross_entropy
+from .axis_aligned_iou_loss import AxisAlignedIoULoss, axis_aligned_iou_loss
 from .chamfer_distance import ChamferDistance, chamfer_distance
 
 __all__ = [
     'FocalLoss', 'SmoothL1Loss', 'binary_cross_entropy', 'ChamferDistance',
-    'chamfer_distance'
+    'chamfer_distance', 'axis_aligned_iou_loss', 'AxisAlignedIoULoss'
 ]
