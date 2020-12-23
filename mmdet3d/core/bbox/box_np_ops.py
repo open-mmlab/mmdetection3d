dd74422e1455a6005c75ab73e08a11a8f4f22b92
twang <30491025+Tai-Wang@users.noreply.github.com>
2020-12-23 17:42:25 +0800

    [Fix] Minor adjustments (#241)
    * Fix an old bug in box_np_ops, not used or influencing results
    
    * Update a minor filter criterion in KITTI evaluation
    
    * Update the fixed criterion, not influencing KITTI results

--- mmdet3d/core/bbox/box_np_ops.py
+++ mmdet3d/core/bbox/box_np_ops.py
@@ -347,7 +347,7 @@ def points_cam2img(points_3d, proj_mat):
     """
     points_shape = list(points_3d.shape)
     points_shape[-1] = 1
-    points_4 = np.concatenate([points_3d, np.zeros(points_shape)], axis=-1)
+    points_4 = np.concatenate([points_3d, np.ones(points_shape)], axis=-1)
     point_2d = points_4 @ proj_mat.T
     point_2d_res = point_2d[..., :2] / point_2d[..., 2:3]
     return point_2d_res
