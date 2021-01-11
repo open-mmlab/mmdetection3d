114724828385e8de1ca985771e030d069fc908c9
Xi Liu <xiliu8006@gmail.com>
2021-01-11 23:24:37 +0800

    Adopted Nuscenes dataset in demo

--- mmdet3d/apis/inference.py
+++ mmdet3d/apis/inference.py
@@ -10,6 +10,21 @@ from mmdet3d.core.bbox import get_box_type
 from mmdet3d.datasets.pipelines import Compose
 from mmdet3d.models import build_detector
 
+def convert_SyncBN(config):
+    if isinstance(config,dict):
+        for item in config:
+            if item == 'norm_cfg':
+                if config[item]['type'] == 'naiveSyncBN1d':
+                    config[item]['type'] = 'BN1d'
+                elif config[item]['type'] == 'naiveSyncBN2d':
+                    config[item]['type'] = 'BN2d'
+                else:
+                    return
+            else:
+                convert_SyncBN(config[item]) 
+    return        
+
+
 
 def init_detector(config, checkpoint=None, device='cuda:0'):
     """Initialize a detector from config file.
@@ -30,6 +45,7 @@ def init_detector(config, checkpoint=None, device='cuda:0'):
         raise TypeError('config must be a filename or Config object, '
                         f'but got {type(config)}')
     config.model.pretrained = None
+    convert_SyncBN(config.model)
     model = build_detector(config.model, test_cfg=config.test_cfg)
     if checkpoint is not None:
         checkpoint = load_checkpoint(model, checkpoint)
@@ -50,47 +66,6 @@ def inference_detector(model, pcd):
         model (nn.Module): The loaded detector.
         pcd (str): Point cloud files.
 
-    Returns:
-        tuple: Predicted results and data from pipeline.
-    """
-    cfg = model.cfg
-    device = next(model.parameters()).device  # model device
-    # build the data pipeline
-    test_pipeline = deepcopy(cfg.data.test.pipeline)
-    test_pipeline = Compose(test_pipeline)
-    box_type_3d, box_mode_3d = get_box_type(cfg.data.test.box_type_3d)
-    data = dict(
-        pts_filename=pcd,
-        box_type_3d=box_type_3d,
-        box_mode_3d=box_mode_3d,
-        img_fields=[],
-        bbox3d_fields=[],
-        pts_mask_fields=[],
-        pts_seg_fields=[],
-        bbox_fields=[],
-        mask_fields=[],
-        seg_fields=[])
-    data = test_pipeline(data)
-    data = collate([data], samples_per_gpu=1)
-    if next(model.parameters()).is_cuda:
-        # scatter to specified GPU
-        data = scatter(data, [device.index])[0]
-    else:
-        # this is a workaround to avoid the bug of MMDataParallel
-        data['img_metas'] = data['img_metas'][0].data
-        data['points'] = data['points'][0].data
-    # forward the model
-    with torch.no_grad():
-        result = model(return_loss=False, rescale=True, **data)
-    return result, data
-
-def inference_nuscenes_detector(model, pcd):
-    """Inference NuScenes dataset point cloud with the detector.
-
-    Args:
-        model (nn.Module): The loaded detector.
-        config (str or :obj:`mmcv.Config`): Config file path or the config
-            object.
     Returns:
         tuple: Predicted results and data from pipeline.
     """
@@ -140,35 +115,10 @@ def show_result_meshlab(data, result, out_dir):
     file_name = osp.split(pts_filename)[-1].split('.')[0]
 
     assert out_dir is not None, 'Expect out_dir, got none.'
-
-    pred_bboxes = result[0]['boxes_3d'].tensor.numpy()
-    # for now we convert points into depth mode
-    if data['img_metas'][0][0]['box_mode_3d'] != Box3DMode.DEPTH:
-        points = points[..., [1, 0, 2]]
-        points[..., 0] *= -1
-        pred_bboxes = Box3DMode.convert(pred_bboxes,
-                                        data['img_metas'][0][0]['box_mode_3d'],
-                                        Box3DMode.DEPTH)
-        pred_bboxes[..., 2] += pred_bboxes[..., 5] / 2
+    if 'pts_bbox' in result[0].keys():
+        pred_bboxes = result[0]['pts_bbox']['boxes_3d'].tensor.numpy()
     else:
-        pred_bboxes[..., 2] += pred_bboxes[..., 5] / 2
-    show_result(points, None, pred_bboxes, out_dir, file_name)
-
-def show_nuscenes_result_meshlab(data, result, out_dir):
-    """Show result by meshlab.
-
-    Args:
-        data (dict): Contain data from pipeline.
-        result (dict): Predicted result from model.
-        out_dir (str): Directory to save visualized result.
-    """
-    points = data['points'][0][0].cpu().numpy()
-    pts_filename = data['img_metas'][0][0]['pts_filename']
-    file_name = osp.split(pts_filename)[-1].split('.')[0]
-
-    assert out_dir is not None, 'Expect out_dir, got none.'
-
-    pred_bboxes = result[0]['pts_bbox']['boxes_3d'].tensor.numpy()
+        pred_bboxes = result[0]['boxes_3d'].tensor.numpy()
     # for now we convert points into depth mode
     if data['img_metas'][0][0]['box_mode_3d'] != Box3DMode.DEPTH:
         points = points[..., [1, 0, 2]]
