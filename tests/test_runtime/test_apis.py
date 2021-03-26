768497851aa136ec630c2b99991008825767777a
xiliu8006 <75658786+xiliu8006@users.noreply.github.com>
2021-03-26 13:39:56 +0800

    [Enhance] Support nuscenes demo (#353)
    * support nuscenes dataset in demo
    
    * add convert_SyncBN in __init__
    
    * fix meshlab visualization bug
    
    * modify meshlab unittest
    
    * add docstring
    
    * add empty line in docstring
    
    Co-authored-by: xiliu8006 <xiliu800@gmail.com>

--- tests/test_runtime/test_apis.py
+++ tests/test_runtime/test_apis.py
@@ -1,9 +1,15 @@
+import numpy as np
+import os
 import pytest
+import tempfile
 import torch
 from mmcv.parallel import MMDataParallel
 from os.path import dirname, exists, join
 
-from mmdet3d.apis import inference_detector, init_detector, single_gpu_test
+from mmdet3d.apis import (convert_SyncBN, inference_detector, init_detector,
+                          show_result_meshlab, single_gpu_test)
+from mmdet3d.core import Box3DMode
+from mmdet3d.core.bbox import LiDARInstance3DBoxes
 from mmdet3d.datasets import build_dataloader, build_dataset
 from mmdet3d.models import build_detector
 
@@ -30,6 +36,48 @@ def _get_config_module(fname):
     config_fpath = join(config_dpath, fname)
     config_mod = Config.fromfile(config_fpath)
     return config_mod
+
+
+def test_convert_SyncBN():
+    cfg = _get_config_module(
+        'pointpillars/hv_pointpillars_fpn_sbn-all_4x8_2x_nus-3d.py')
+    model_cfg = cfg.model
+    convert_SyncBN(model_cfg)
+    assert model_cfg['pts_voxel_encoder']['norm_cfg']['type'] == 'BN1d'
+    assert model_cfg['pts_backbone']['norm_cfg']['type'] == 'BN2d'
+    assert model_cfg['pts_neck']['norm_cfg']['type'] == 'BN2d'
+
+
+def test_show_result_meshlab():
+    pcd = 'tests/data/nuscenes/samples/LIDAR_TOP/n015-2018-08-02-17-16-37+' \
+              '0800__LIDAR_TOP__1533201470948018.pcd.bin'
+    box_3d = LiDARInstance3DBoxes(
+        torch.tensor(
+            [[8.7314, -1.8559, -1.5997, 0.4800, 1.2000, 1.8900, 0.0100]]))
+    labels_3d = torch.tensor([0])
+    scores_3d = torch.tensor([0.5])
+    points = np.random.rand(100, 4)
+    img_meta = dict(
+        pts_filename=pcd, boxes_3d=box_3d, box_mode_3d=Box3DMode.LIDAR)
+    data = dict(points=[[torch.tensor(points)]], img_metas=[[img_meta]])
+    result = [
+        dict(
+            pts_bbox=dict(
+                boxes_3d=box_3d, labels_3d=labels_3d, scores_3d=scores_3d))
+    ]
+    temp_out_dir = tempfile.mkdtemp()
+    out_dir, file_name = show_result_meshlab(data, result, temp_out_dir)
+    expected_outfile_ply = file_name + '_pred.ply'
+    expected_outfile_obj = file_name + '_points.obj'
+    expected_outfile_ply_path = os.path.join(out_dir, file_name,
+                                             expected_outfile_ply)
+    expected_outfile_obj_path = os.path.join(out_dir, file_name,
+                                             expected_outfile_obj)
+    assert os.path.exists(expected_outfile_ply_path)
+    assert os.path.exists(expected_outfile_obj_path)
+    os.remove(expected_outfile_obj_path)
+    os.remove(expected_outfile_ply_path)
+    os.removedirs(os.path.join(temp_out_dir, file_name))
 
 
 def test_inference_detector():
