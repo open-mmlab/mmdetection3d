da64143db897948efd22f1d669c2d28642359b74
Tai-Wang <tab_wang@outlook.com>
2021-04-07 15:01:58 +0800

    Merge nus_mono3d and update dataset init

--- mmdet3d/datasets/__init__.py
+++ mmdet3d/datasets/__init__.py
@@ -2,6 +2,7 @@ from mmdet.datasets.builder import build_dataloader
 from .builder import DATASETS, build_dataset
 from .custom_3d import Custom3DDataset
 from .kitti_dataset import KittiDataset
+from .kitti_mono_dataset import KittiMonoDataset
 from .lyft_dataset import LyftDataset
 from .nuscenes_dataset import NuScenesDataset
 from .nuscenes_mono_dataset import NuScenesMonoDataset
@@ -17,13 +18,14 @@ from .sunrgbd_dataset import SUNRGBDDataset
 from .waymo_dataset import WaymoDataset
 
 __all__ = [
-    'KittiDataset', 'GroupSampler', 'DistributedGroupSampler',
-    'build_dataloader', 'RepeatFactorDataset', 'DATASETS', 'build_dataset',
-    'CocoDataset', 'NuScenesDataset', 'NuScenesMonoDataset', 'LyftDataset',
-    'ObjectSample', 'RandomFlip3D', 'ObjectNoise', 'GlobalRotScaleTrans',
-    'PointShuffle', 'ObjectRangeFilter', 'PointsRangeFilter', 'Collect3D',
-    'LoadPointsFromFile', 'NormalizePointsColor', 'IndoorPointSample',
-    'LoadAnnotations3D', 'SUNRGBDDataset', 'ScanNetDataset',
-    'SemanticKITTIDataset', 'Custom3DDataset', 'LoadPointsFromMultiSweeps',
-    'WaymoDataset', 'BackgroundPointsFilter', 'VoxelBasedPointSampler'
+    'KittiDataset', 'KittiMonoDataset', 'GroupSampler',
+    'DistributedGroupSampler', 'build_dataloader', 'RepeatFactorDataset',
+    'DATASETS', 'build_dataset', 'CocoDataset', 'NuScenesDataset',
+    'NuScenesMonoDataset', 'LyftDataset', 'ObjectSample', 'RandomFlip3D',
+    'ObjectNoise', 'GlobalRotScaleTrans', 'PointShuffle', 'ObjectRangeFilter',
+    'PointsRangeFilter', 'Collect3D', 'LoadPointsFromFile',
+    'NormalizePointsColor', 'IndoorPointSample', 'LoadAnnotations3D',
+    'SUNRGBDDataset', 'ScanNetDataset', 'SemanticKITTIDataset',
+    'Custom3DDataset', 'LoadPointsFromMultiSweeps', 'WaymoDataset',
+    'BackgroundPointsFilter', 'VoxelBasedPointSampler'
 ]
