808d1ec7358475e23b91931294a65e9133aa540e
Wuziyi616 <dazitu616@gmail.com>
2021-04-09 14:18:26 +0800

    reuse pipeline in waymo dataset

--- configs/_base_/datasets/waymoD5-3d-3class.py
+++ configs/_base_/datasets/waymoD5-3d-3class.py
@@ -84,6 +84,21 @@ test_pipeline = [
             dict(type='Collect3D', keys=['points'])
         ])
 ]
+# construct a pipeline for data and gt loading in show function
+# please keep its loading function consistent with test_pipeline (e.g. client)
+eval_pipeline = [
+    dict(
+        type='LoadPointsFromFile',
+        coord_type='LIDAR',
+        load_dim=6,
+        use_dim=5,
+        file_client_args=file_client_args),
+    dict(
+        type='DefaultFormatBundle3D',
+        class_names=class_names,
+        with_label=False),
+    dict(type='Collect3D', keys=['points'])
+]
 
 data = dict(
     samples_per_gpu=2,
@@ -126,4 +141,4 @@ data = dict(
         test_mode=True,
         box_type_3d='LiDAR'))
 
-evaluation = dict(interval=24)
+evaluation = dict(interval=24, pipeline=eval_pipeline)
