ef23977f87bef862157bec8b248f542f8f2ee446
Tai-Wang <tab_wang@outlook.com>
2021-04-14 21:17:38 +0800

    Support FCOS3D head

--- mmdet3d/models/dense_heads/__init__.py
+++ mmdet3d/models/dense_heads/__init__.py
@@ -3,6 +3,7 @@ from .anchor_free_mono3d_head import AnchorFreeMono3DHead
 from .base_conv_bbox_head import BaseConvBboxHead
 from .base_mono3d_dense_head import BaseMono3DDenseHead
 from .centerpoint_head import CenterHead
+from .fcos_mono3d_head import FCOSMono3DHead
 from .free_anchor3d_head import FreeAnchor3DHead
 from .parta2_rpn_head import PartA2RPNHead
 from .shape_aware_head import ShapeAwareHead
@@ -12,5 +13,5 @@ from .vote_head import VoteHead
 __all__ = [
     'Anchor3DHead', 'FreeAnchor3DHead', 'PartA2RPNHead', 'VoteHead',
     'SSD3DHead', 'BaseConvBboxHead', 'CenterHead', 'ShapeAwareHead',
-    'BaseMono3DDenseHead', 'AnchorFreeMono3DHead'
+    'BaseMono3DDenseHead', 'AnchorFreeMono3DHead', 'FCOSMono3DHead'
 ]
