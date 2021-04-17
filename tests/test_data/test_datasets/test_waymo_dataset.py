e0b826c8f0d9e058e685057969a72a5e5df66c28
Wuziyi616 <dazitu616@gmail.com>
2021-04-17 20:20:51 +0800

    KITTI format evaluate done, Waymo format uncomplete

--- tests/test_data/test_datasets/test_waymo_dataset.py
+++ tests/test_data/test_datasets/test_waymo_dataset.py
@@ -6,52 +6,13 @@ import torch
 from mmdet3d.datasets import WaymoDataset
 
 
-def _generate_waymo_dataset_config():
+def _generate_waymo_train_dataset_config():
     data_root = 'tests/data/waymo/kitti_format/'
     ann_file = 'tests/data/waymo/kitti_format/waymo_infos_train.pkl'
     classes = ['Car', 'Pedestrian', 'Cyclist']
     pts_prefix = 'velodyne'
     point_cloud_range = [-74.88, -74.88, -2, 74.88, 74.88, 4]
     file_client_args = dict(backend='disk')
-    pipeline = [
-        dict(
-            type='LoadPointsFromFile',
-            coord_type='LIDAR',
-            load_dim=6,
-            use_dim=5,
-            file_client_args=file_client_args),
-        dict(
-            type='MultiScaleFlipAug3D',
-            img_scale=(1333, 800),
-            pts_scale_ratio=1,
-            flip=False,
-            transforms=[
-                dict(
-                    type='GlobalRotScaleTrans',
-                    rot_range=[0, 0],
-                    scale_ratio_range=[1., 1.],
-                    translation_std=[0, 0, 0]),
-                dict(type='RandomFlip3D'),
-                dict(
-                    type='PointsRangeFilter',
-                    point_cloud_range=point_cloud_range),
-                dict(
-                    type='DefaultFormatBundle3D',
-                    class_names=classes,
-                    with_label=False),
-                dict(type='Collect3D', keys=['points'])
-            ])
-    ]
-    modality = dict(use_lidar=True, use_camera=False)
-    split = 'training'
-    return data_root, ann_file, classes, pts_prefix, pipeline, modality, \
-        split, point_cloud_range, file_client_args
-
-
-def test_getitem():
-    np.random.seed(0)
-    data_root, ann_file, classes, pts_prefix, _, modality, split, \
-        point_cloud_range, file_client_args = _generate_waymo_dataset_config()
     db_sampler = dict(
         data_root=data_root,
         info_path=data_root + 'waymo_dbinfos_train.pkl',
@@ -95,6 +56,56 @@ def test_getitem():
         dict(
             type='Collect3D', keys=['points', 'gt_bboxes_3d', 'gt_labels_3d'])
     ]
+    modality = dict(use_lidar=True, use_camera=False)
+    split = 'training'
+    return data_root, ann_file, classes, pts_prefix, pipeline, modality, split
+
+
+def _generate_waymo_val_dataset_config():
+    data_root = 'tests/data/waymo/kitti_format/'
+    ann_file = 'tests/data/waymo/kitti_format/waymo_infos_val.pkl'
+    classes = ['Car', 'Pedestrian', 'Cyclist']
+    pts_prefix = 'velodyne'
+    point_cloud_range = [-74.88, -74.88, -2, 74.88, 74.88, 4]
+    file_client_args = dict(backend='disk')
+    pipeline = [
+        dict(
+            type='LoadPointsFromFile',
+            coord_type='LIDAR',
+            load_dim=6,
+            use_dim=5,
+            file_client_args=file_client_args),
+        dict(
+            type='MultiScaleFlipAug3D',
+            img_scale=(1333, 800),
+            pts_scale_ratio=1,
+            flip=False,
+            transforms=[
+                dict(
+                    type='GlobalRotScaleTrans',
+                    rot_range=[0, 0],
+                    scale_ratio_range=[1., 1.],
+                    translation_std=[0, 0, 0]),
+                dict(type='RandomFlip3D'),
+                dict(
+                    type='PointsRangeFilter',
+                    point_cloud_range=point_cloud_range),
+                dict(
+                    type='DefaultFormatBundle3D',
+                    class_names=classes,
+                    with_label=False),
+                dict(type='Collect3D', keys=['points'])
+            ])
+    ]
+    modality = dict(use_lidar=True, use_camera=False)
+    split = 'training'
+    return data_root, ann_file, classes, pts_prefix, pipeline, modality, split
+
+
+def test_getitem():
+    np.random.seed(0)
+    data_root, ann_file, classes, pts_prefix, pipeline, \
+        modality, split = _generate_waymo_train_dataset_config()
     waymo_dataset = WaymoDataset(data_root, ann_file, split, pts_prefix,
                                  pipeline, classes, modality)
     data = waymo_dataset[0]
@@ -114,28 +125,37 @@ def test_evaluate():
     if not torch.cuda.is_available():
         pytest.skip('test requires GPU and torch+cuda')
     from mmdet3d.core.bbox import LiDARInstance3DBoxes
-    data_root, ann_file, classes, pts_prefix, pipeline, modality, split, \
-        point_cloud_range, file_client_args = _generate_waymo_dataset_config()
+    data_root, ann_file, classes, pts_prefix, pipeline, \
+        modality, split = _generate_waymo_val_dataset_config()
     waymo_dataset = WaymoDataset(data_root, ann_file, split, pts_prefix,
                                  pipeline, classes, modality)
     boxes_3d = LiDARInstance3DBoxes(
         torch.tensor(
-            [[31.4750, -4.5690, 2.1857, 2.3519, 6.0931, 3.1756, -1.2895]]))
+            # [[31.9723, -2.3247, 2.2145, 2.3900, 6.0800, 3.1000, -1.3200]]))
+            [[
+                6.9684e+01, 3.3335e+01, 4.1465e-02, 2.0100e+00, 4.3600e+00,
+                1.4600e+00, -9.0000e-02
+            ]]))
     labels_3d = torch.tensor([
         0,
     ])
     scores_3d = torch.tensor([0.5])
     result = dict(boxes_3d=boxes_3d, labels_3d=labels_3d, scores_3d=scores_3d)
+
     # kitti protocol
-    # kitti box and waymo box are in different format
     metric = ['kitti']
-    _ = waymo_dataset.evaluate([result], metric=metric)
-    # assert np.isclose(ap_dict['KITTI/Overall_3D_easy'], 3.030303030)
-    # assert np.isclose(ap_dict['KITTI/Overall_3D_moderate'], 3.030303030)
-    # assert np.isclose(ap_dict['KITTI/Overall_3D_hard'], 3.030303030)
+    ap_dict = waymo_dataset.evaluate([result], metric=metric)
+    assert np.isclose(ap_dict['KITTI/Overall_3D_easy'], 3.0303030303030307)
+    assert np.isclose(ap_dict['KITTI/Overall_3D_moderate'], 3.0303030303030307)
+    assert np.isclose(ap_dict['KITTI/Overall_3D_hard'], 3.0303030303030307)
 
     # waymo protocol
-    # missing gt file tests/data/waymo/waymo_format/gt.bin
+    if not torch.cuda.is_available():
+        pytest.skip('test requires tensorflow-gpu')
+    metric = ['waymo']
+    ap_dict = waymo_dataset.evaluate([result], metric=metric)
+    import pdb
+    pdb.set_trace()
 
 
 def test_show():
@@ -147,8 +167,8 @@ def test_show():
     # Waymo shares show function with KITTI so I just copy it here
     tmp_dir = tempfile.TemporaryDirectory()
     temp_dir = tmp_dir.name
-    data_root, ann_file, classes, pts_prefix, pipeline, modality, split, \
-        point_cloud_range, file_client_args = _generate_waymo_dataset_config()
+    data_root, ann_file, classes, pts_prefix, pipeline, \
+        modality, split = _generate_waymo_val_dataset_config()
     waymo_dataset = WaymoDataset(
         data_root, ann_file, split=split, modality=modality, pipeline=pipeline)
     boxes_3d = LiDARInstance3DBoxes(
@@ -163,9 +183,9 @@ def test_show():
     result = dict(boxes_3d=boxes_3d, scores_3d=scores_3d, labels_3d=labels_3d)
     results = [result]
     waymo_dataset.show(results, temp_dir, show=False)
-    pts_file_path = osp.join(temp_dir, '0000000', '0000000_points.obj')
-    gt_file_path = osp.join(temp_dir, '0000000', '0000000_gt.obj')
-    pred_file_path = osp.join(temp_dir, '0000000', '0000000_pred.obj')
+    pts_file_path = osp.join(temp_dir, '1000000', '1000000_points.obj')
+    gt_file_path = osp.join(temp_dir, '1000000', '1000000_gt.obj')
+    pred_file_path = osp.join(temp_dir, '1000000', '1000000_pred.obj')
     mmcv.check_file_exist(pts_file_path)
     mmcv.check_file_exist(gt_file_path)
     mmcv.check_file_exist(pred_file_path)
