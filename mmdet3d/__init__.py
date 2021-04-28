32e7186ebbb94a40e071b50b811ecea3c96d3fa3
Wuziyi616 <dazitu616@gmail.com>
2021-04-28 16:36:19 +0800

    min mmcv version to 1.3.2

--- mmdet3d/__init__.py
+++ mmdet3d/__init__.py
@@ -16,7 +16,7 @@ def digit_version(version_str):
     return digit_version
 
 
-mmcv_minimum_version = '1.3.1'
+mmcv_minimum_version = '1.3.2'
 mmcv_maximum_version = '1.4.0'
 mmcv_version = digit_version(mmcv.__version__)
 
