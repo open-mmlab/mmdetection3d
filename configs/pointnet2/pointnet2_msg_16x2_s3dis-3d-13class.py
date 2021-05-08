bdc9e4846fae9265f441b0322a7d8c55a772f795
Wuziyi616 <dazitu616@gmail.com>
2021-05-08 13:20:53 +0800

    add configs for pn2 on s3dis

---
+++ configs/pointnet2/pointnet2_msg_16x2_s3dis-3d-13class.py
@@ -0,0 +1,32 @@
+_base_ = [
+    '../_base_/datasets/s3dis_seg-3d-13class.py',
+    '../_base_/models/pointnet2_msg.py', '../_base_/default_runtime.py'
+]
+
+# data settings
+data_root = './data/s3dis/'
+data = dict(samples_per_gpu=16)
+evaluation = dict(interval=5)
+
+# model settings
+model = dict(
+    backbone=dict(in_channels=9),  # [xyz, rgb, normalized_xyz]
+    decode_head=dict(
+        num_classes=13, ignore_index=13,
+        loss_decode=dict(class_weight=None)),  # S3DIS doesn't use class_weight
+    test_cfg=dict(
+        num_points=4096,
+        block_size=1.0,
+        sample_rate=0.5,
+        use_normalized_coord=True,
+        batch_size=24))
+
+# optimizer
+lr = 0.001  # max learning rate
+optimizer = dict(type='Adam', lr=lr, weight_decay=1e-4)
+optimizer_config = dict(grad_clip=None)
+lr_config = dict(policy='CosineAnnealing', warmup=None, min_lr=1e-5)
+
+# runtime settings
+checkpoint_config = dict(interval=5)
+runner = dict(type='EpochBasedRunner', max_epochs=150)
