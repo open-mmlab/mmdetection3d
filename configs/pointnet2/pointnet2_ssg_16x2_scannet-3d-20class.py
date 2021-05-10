bb4333084b21cdef76e77e11390e738b485830f8
Wuziyi616 <dazitu616@gmail.com>
2021-05-10 16:17:58 +0800

    hard-code class_weight into configs

--- configs/pointnet2/pointnet2_ssg_16x2_scannet-3d-20class.py
+++ configs/pointnet2/pointnet2_ssg_16x2_scannet-3d-20class.py
@@ -4,7 +4,6 @@ _base_ = [
 ]
 
 # data settings
-data_root = './data/scannet/'
 data = dict(samples_per_gpu=16)
 evaluation = dict(interval=5)
 
@@ -13,8 +12,12 @@ model = dict(
     decode_head=dict(
         num_classes=20,
         ignore_index=20,
-        loss_decode=dict(class_weight=data_root +
-                         'seg_info/train_label_weight.npy')),
+        loss_decode=dict(class_weight=[
+            2.389689, 2.7215734, 4.5944676, 4.8543367, 4.096086, 4.907941,
+            4.690836, 4.512031, 4.623311, 4.9242644, 5.358117, 5.360071,
+            5.019636, 4.967126, 5.3502126, 5.4023647, 5.4027233, 5.4169416,
+            5.3954206, 4.6971426
+        ])),
     test_cfg=dict(
         num_points=8192,
         block_size=1.5,
