459e9e58274f232af83400573ad2f1d5bda6a04d
Wuziyi616 <dazitu616@gmail.com>
2021-05-12 15:30:38 +0800

    add comments for class_weight

--- configs/pointnet2/pointnet2_ssg_16x2_scannet-3d-20class.py
+++ configs/pointnet2/pointnet2_ssg_16x2_scannet-3d-20class.py
@@ -12,6 +12,10 @@ model = dict(
     decode_head=dict(
         num_classes=20,
         ignore_index=20,
+        # `class_weight` is generated in data pre-processing, saved in
+        # `data/scannet/seg_info/train_label_weight.npy`
+        # you can copy paste the values here, or input the file path as
+        # `class_weight=data/scannet/seg_info/train_label_weight.npy`
         loss_decode=dict(class_weight=[
             2.389689, 2.7215734, 4.5944676, 4.8543367, 4.096086, 4.907941,
             4.690836, 4.512031, 4.623311, 4.9242644, 5.358117, 5.360071,
