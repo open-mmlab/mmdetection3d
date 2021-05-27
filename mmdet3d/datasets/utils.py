c6c5a6865265efd58162216095732a2f0d7ab7d4
Wuziyi616 <dazitu616@gmail.com>
2021-05-27 14:01:07 +0800

    add comment

--- mmdet3d/datasets/utils.py
+++ mmdet3d/datasets/utils.py
@@ -115,6 +115,11 @@ def get_loading_pipeline(pipeline):
 def extract_result_dict(results, key):
     """Extract and return the data corresponding to key in result dict.
 
+    ``results`` is a dict output from `pipeline(input_dict)`, which is the
+        loaded data from ``Dataset`` class.
+    The data terms inside may be wrapped in list, tuple and DataContainer, so
+        this function essentially extracts data from these wrappers.
+
     Args:
         results (dict): Data loaded using pipeline.
         key (str): Key of the desired data.
