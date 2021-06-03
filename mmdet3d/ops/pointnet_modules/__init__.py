aeed3cc38c22dfd82dc23fa208198b11c9ebcab9
Wuziyi616 <dazitu616@gmail.com>
2021-06-03 22:20:24 +0800

    move paconv sa modules to a new file

--- mmdet3d/ops/pointnet_modules/__init__.py
+++ mmdet3d/ops/pointnet_modules/__init__.py
@@ -1,8 +1,8 @@
 from .builder import build_sa_module
+from .paconv_sa_module import (PAConvSAModule, PAConvSAModuleCUDA,
+                               PAConvSAModuleMSG, PAConvSAModuleMSGCUDA)
 from .point_fp_module import PointFPModule
-from .point_sa_module import (PAConvSAModule, PAConvSAModuleCUDA,
-                              PAConvSAModuleMSG, PAConvSAModuleMSGCUDA,
-                              PointSAModule, PointSAModuleMSG)
+from .point_sa_module import PointSAModule, PointSAModuleMSG
 
 __all__ = [
     'build_sa_module', 'PointSAModuleMSG', 'PointSAModule', 'PointFPModule',
