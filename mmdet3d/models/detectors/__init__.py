318499acd0c05fe1fee638d22186067a92a0684f
hjin2902 <61953644+hjin2902@users.noreply.github.com>
2021-07-01 13:12:16 +0800

    [Feature] Group-Free-3D head (#539)
    * group-free-3d head
    
    * GroupFree3DNet->VoteNet
    
    * modify docstring
    
    * bugfix: calculate pts_instance_label, decoder self/cross posembed init
    
    * support GroupFree3DNet, modify scannnet train config
    
    * support point cloud input features dim = 0
    
    * add groupfree3dnet test case
    
    * bugfix: softmax in decode boxes
    
    * support multi-stage predictions
    
    * modify GroupFree3DMultiheadAttention input parameters
    
    * refactor: support sunrgbd-based train
    
    * refactor: support sunrgbd-based train
    
    * fix parts of bug
    
    * modify multi-stage prediction
    
    * fixbug: conv_channels
    
    * bugfix: permute
    
    * bugfix: permute
    
    * bugfix: expand
    
    * fix MAX_NUM_OBJ=64
    
    * 4 gpu training, score_thr = 0.0
    
    * modify config, repeattime=1
    
    * bigfix: expand
    
    * modify: GroupFree3DMHA, build_positional_encoding
    
    * modify: GroupFree3DMHA, build_positional_encoding
    
    * bugfix: torch.nn
    
    * bugfix: mean loss
    
    * residual -> identity
    
    * fix name: DropOut -> Dropout
    
    * delete sunrgbd-based congfig
    
    * Fix: trailing whitespace
    
    * suffix -> prefix
    
    * bugfix: groupfree3d config
    
    Co-authored-by: jinhui <PJLAB\jinhui@shai14001019l.pjlab.org>

--- mmdet3d/models/detectors/__init__.py
+++ mmdet3d/models/detectors/__init__.py
@@ -2,6 +2,7 @@ from .base import Base3DDetector
 from .centerpoint import CenterPoint
 from .dynamic_voxelnet import DynamicVoxelNet
 from .fcos_mono3d import FCOSMono3D
+from .groupfree3dnet import GroupFree3DNet
 from .h3dnet import H3DNet
 from .imvotenet import ImVoteNet
 from .imvoxelnet import ImVoxelNet
@@ -17,5 +18,5 @@ __all__ = [
     'Base3DDetector', 'VoxelNet', 'DynamicVoxelNet', 'MVXTwoStageDetector',
     'DynamicMVXFasterRCNN', 'MVXFasterRCNN', 'PartA2', 'VoteNet', 'H3DNet',
     'CenterPoint', 'SSD3DNet', 'ImVoteNet', 'SingleStageMono3DDetector',
-    'FCOSMono3D', 'ImVoxelNet'
+    'FCOSMono3D', 'ImVoxelNet', 'GroupFree3DNet'
 ]
