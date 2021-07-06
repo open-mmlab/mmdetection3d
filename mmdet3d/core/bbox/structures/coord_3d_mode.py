dda857b5dbdb31e0a3682b4ac1b82e43fa57c7e9
Gopi Krishna Erabati <gopi231091@gmail.com>
2021-07-06 11:18:38 +0100

    converted rt_mat from ndarray to tensor to make it further compatible

--- mmdet3d/core/bbox/structures/coord_3d_mode.py
+++ mmdet3d/core/bbox/structures/coord_3d_mode.py
@@ -243,6 +243,9 @@ class Coord3DMode(IntEnum):
                 f'Conversion from Coord3DMode {src} to {dst} '
                 'is not supported yet')
 
+        if isinstance(rt_mat, np.ndarray):
+            rt_mat = arr.new_tensor(rt_mat)
+
         if rt_mat.size(1) == 4:
             extended_xyz = torch.cat(
                 [arr[:, :3], arr.new_ones(arr.size(0), 1)], dim=-1)
