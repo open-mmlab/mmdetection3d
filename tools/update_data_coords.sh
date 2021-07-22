1248abfec305f4c980b35c711fca6245056dc7f2
THU17cyz <congyezhen71@hotmail.com>
2021-07-22 12:30:29 +0800

    Add data update scripts

---
+++ tools/update_data_coords.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -x
+export PYTHONPATH=`pwd`:$PYTHONPATH
+
+PARTITION=$1
+JOB_NAME=$2
+CONFIG=$3
+WORK_DIR=$4
+GPUS=${GPUS:-1}
+GPUS_PER_NODE=${GPUS_PER_NODE:-1}
+SRUN_ARGS=${SRUN_ARGS:-""}
+JOB_NAME=convert_coord_sys_data
+
+srun -p ${PARTITION} \
+    --job-name=${JOB_NAME} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --kill-on-bad-exit=1 \
+    ${SRUN_ARGS} \
+    python -u tools/coord_sys_data_converter.py kitti \
+            --root-path ./data/kitti \
+            --out-dir ./data/kitti
