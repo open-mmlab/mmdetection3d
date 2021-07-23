7de4bf5e26a7cc9ee46009f0a5ac470041b99e8e
xiliu8006 <xiliu8006@gmail.com>
2021-07-23 11:46:33 +0800

    fix conflicts

--- mmdet3d/models/roi_heads/roi_extractors/__init__.py
+++ mmdet3d/models/roi_heads/roi_extractors/__init__.py
@@ -2,6 +2,7 @@ from mmdet.models.roi_heads.roi_extractors import SingleRoIExtractor
 from .single_roiaware_extractor import Single3DRoIAwareExtractor
 from .single_roipoint_extractor import Single3DRoIPointExtractor
 
-__all__ = ['SingleRoIExtractor', 
-           'Single3DRoIAwareExtractor', 
-           'Single3DRoIPointExtractor']
+__all__ = [
+    'SingleRoIExtractor', 'Single3DRoIAwareExtractor',
+    'Single3DRoIPointExtractor'
+]
