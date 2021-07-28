9684b58f9cf181b27daaf725ea857dc67b14945b
Danila Rukhovich <danrukh@gmail.com>
2021-07-28 15:26:43 +0300

    fix lint

--- mmdet3d/core/bbox/structures/__init__.py
+++ mmdet3d/core/bbox/structures/__init__.py
@@ -4,9 +4,9 @@ from .cam_box3d import CameraInstance3DBoxes
 from .coord_3d_mode import Coord3DMode
 from .depth_box3d import DepthInstance3DBoxes
 from .lidar_box3d import LiDARInstance3DBoxes
-from .utils import (get_box_type, limit_period, mono_cam_box2vis,
-                    points_cam2img, rotation_3d_in_axis, xywhr2xyxyr,
-                    get_proj_mat_by_coord_type)
+from .utils import (get_box_type, get_proj_mat_by_coord_type, limit_period,
+                    mono_cam_box2vis, points_cam2img, rotation_3d_in_axis,
+                    xywhr2xyxyr)
 
 __all__ = [
     'Box3DMode', 'BaseInstance3DBoxes', 'LiDARInstance3DBoxes',
