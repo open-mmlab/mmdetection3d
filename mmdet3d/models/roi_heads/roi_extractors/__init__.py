c84bf9d5e0c9f84a589eda44cbfe01e4229123d2
xiliu8006 <xiliu8006@gmail.com>
2021-08-02 12:49:50 +0800

    add roi extractor

--- mmdet3d/models/roi_heads/roi_extractors/__init__.py
+++ mmdet3d/models/roi_heads/roi_extractors/__init__.py
@@ -1,4 +1,8 @@
 from mmdet.models.roi_heads.roi_extractors import SingleRoIExtractor
 from .single_roiaware_extractor import Single3DRoIAwareExtractor
+from .single_roipoint_extractor import Single3DRoIPointExtractor
 
-__all__ = ['SingleRoIExtractor', 'Single3DRoIAwareExtractor']
+__all__ = [
+    'SingleRoIExtractor', 'Single3DRoIAwareExtractor',
+    'Single3DRoIPointExtractor'
+]
