8df9f442f1a26e28ed44869e7e63ab2b8ca703ac
ChaimZhu <zhuchenming@pjlab.org.cn>
2021-08-04 16:15:31 +0800

    add smoke detecotor and it's backbone and neck

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -4,8 +4,9 @@ from .nostem_regnet import NoStemRegNet
 from .pointnet2_sa_msg import PointNet2SAMSG
 from .pointnet2_sa_ssg import PointNet2SASSG
 from .second import SECOND
+from .dla import DLANet
 
 __all__ = [
     'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'NoStemRegNet',
-    'SECOND', 'PointNet2SASSG', 'PointNet2SAMSG', 'MultiBackbone'
+    'SECOND', 'PointNet2SASSG', 'PointNet2SAMSG', 'MultiBackbone', 'DLANet'
 ]
