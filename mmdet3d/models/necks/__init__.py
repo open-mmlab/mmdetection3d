297b1bf7e1ac57768594ffaa542787aea0e03fbe
ChaimZhu <zhuchenming@pjlab.org.cn>
2021-08-06 18:01:39 +0800

    fix typo

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmdet.models.necks.fpn import FPN
+from .dla_neck import DLA_Neck
 from .imvoxel_neck import OutdoorImVoxelNeck
 from .second_fpn import SECONDFPN
-from .dla_neck import DLA_Neck
 
 __all__ = ['FPN', 'SECONDFPN', 'OutdoorImVoxelNeck', 'DLA_Neck']
