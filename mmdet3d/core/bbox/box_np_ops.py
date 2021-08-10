8f0669de7589cecaa3f34b934ea35fb7c9e2c94f
THU17cyz <congyezhen71@hotmail.com>
2021-08-10 23:27:01 +0800

    Fix comment

--- mmdet3d/core/bbox/box_np_ops.py
+++ mmdet3d/core/bbox/box_np_ops.py
@@ -189,7 +189,7 @@ def center_to_corner_box3d(centers,
         np.ndarray: Corners with the shape of (N, 8, 3).
     """
     # 'length' in kitti format is in x axis.
-    # yzx(hwl)(kitti label file)<->xyz(lhw)(camera)<->z(-x)(-y)(wlh)(lidar)
+    # yzx(hwl)(kitti label file)<->xyz(lhw)(camera)<->z(-x)(-y)(lwh)(lidar)
     # center in kitti format is [0.5, 1.0, 0.5] in xyz.
     corners = corners_nd(dims, origin=origin)
     # corners: [N, 8, 3]
