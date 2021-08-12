5fbbcdcabc0972635b9bf4d3b573578baf3cbd16
xiliu8006 <xiliu8006@gmail.com>
2021-08-12 17:47:10 +0800

    support pointrcnn backbone

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,5 +1,6 @@
 from mmdet.models.necks.fpn import FPN
 from .imvoxel_neck import OutdoorImVoxelNeck
+from .pointnet2_fp_neck import PointNetFPNeck
 from .second_fpn import SECONDFPN
 
-__all__ = ['FPN', 'SECONDFPN', 'OutdoorImVoxelNeck']
+__all__ = ['FPN', 'SECONDFPN', 'OutdoorImVoxelNeck', 'PointNetFPNeck']
