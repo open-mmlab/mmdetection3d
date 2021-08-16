cc5e857905bfd14906be9754fa5e9be06bdc7791
Xi Liu <75658786+xiliu8006@users.noreply.github.com>
2021-08-16 18:00:33 +0800

    [Feature] PointXYZWHLRBBoxCoder (#856)
    * support PointBasedBoxCoder
    
    * fix unittest bug
    
    * support unittest in gpu
    
    * support unittest in gpu
    
    * modified docstring
    
    * add args
    
    * add args

--- mmdet3d/core/bbox/coders/__init__.py
+++ mmdet3d/core/bbox/coders/__init__.py
@@ -5,8 +5,10 @@ from .centerpoint_bbox_coders import CenterPointBBoxCoder
 from .delta_xyzwhlr_bbox_coder import DeltaXYZWLHRBBoxCoder
 from .groupfree3d_bbox_coder import GroupFree3DBBoxCoder
 from .partial_bin_based_bbox_coder import PartialBinBasedBBoxCoder
+from .point_xyzwhlr_bbox_coder import PointXYZWHLRBBoxCoder
 
 __all__ = [
     'build_bbox_coder', 'DeltaXYZWLHRBBoxCoder', 'PartialBinBasedBBoxCoder',
-    'CenterPointBBoxCoder', 'AnchorFreeBBoxCoder', 'GroupFree3DBBoxCoder'
+    'CenterPointBBoxCoder', 'AnchorFreeBBoxCoder', 'GroupFree3DBBoxCoder',
+    'PointXYZWHLRBBoxCoder'
 ]
