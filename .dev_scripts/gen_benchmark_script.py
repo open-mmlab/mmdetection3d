c821ce5229d9f3245d22b9f93b39dd9d02a4bc5d
THU17cyz <congyezhen71@hotmail.com>
2021-08-27 15:44:47 +0800

    Refined script gen

--- .dev_scripts/gen_benchmark_script.py
+++ .dev_scripts/gen_benchmark_script.py
@@ -1,5 +1,6 @@
 import argparse
 import re
+from os import path as osp
 
 
 def parse_args():
@@ -18,7 +19,32 @@ def parse_args():
         help='Output file containing the '
         'commands to train/test selected models.')
     parser.add_argument(
+        '--gpus_per_node',
+        type=int,
+        default=8,
+        help='GPUs per node config for slurm, '
+        'should be set according to your slurm environment')
+    parser.add_argument(
+        '--cpus_per_task',
+        type=int,
+        default=5,
+        help='CPUs per task config for slurm, '
+        'should be set according to your slurm environment')
+    parser.add_argument(
         '--mode', type=str, default='train', help='Train or test')
+    parser.add_argument(
+        '--long_work_dir',
+        action='store_true',
+        help='Whether use full relative path of config as work dir')
+    parser.add_argument(
+        '--max_keep_ckpts',
+        type=int,
+        default=1,
+        help='The max number of checkpoints saved in training')
+    parser.add_argument(
+        '--full_log',
+        action='store_true',
+        help='Whether save full log in a file')
 
     args = parser.parse_args()
     return args
@@ -49,18 +75,44 @@ CHECKPOINT_DIR=$2
 '''
 
 for i, config_path in enumerate(config_paths):
+    root_dir = osp.dirname(osp.dirname(osp.abspath(__file__)))
+    if not osp.exists(osp.join(root_dir, config_path)):
+        print(f'Invalid config path (does not exist):\n{config_path}')
+        continue
+
     config_name = config_path.split('/')[-1][:-3]
-    match_obj = re.match(r'.*_[0-9]+x([0-9]+)_.*', config_name)
+    match_obj = re.match(r'^.*_[0-9]+x([0-9]+)_.*$', config_name)
+    if match_obj is None:
+        print(f'Invalid config path (no GPU num in '
+              f'config name):\n{config_path}')
+        continue
 
     gpu_num = int(match_obj.group(1))
+    work_dir_name = config_path if args.long_work_dir else config_name
+
     script += f"echo '{config_path}' &\n"
-    script += f'GPUS={gpu_num}  GPUS_PER_NODE={gpu_num}  CPUS_PER_TASK=5 ' \
+    if args.full_log:
+        script += f'mkdir -p $CHECKPOINT_DIR/{work_dir_name}\n'
+
+    # training commands
+    script += f'GPUS={gpu_num} GPUS_PER_NODE={args.gpus_per_node} ' \
+              f'CPUS_PER_TASK={args.cpus_per_task} ' \
               f'./tools/slurm_train.sh $PARTITION {config_name} ' \
               f'{config_path} \\\n'
-    script += f'$CHECKPOINT_DIR/{config_name} --cfg-options ' \
-              f'checkpoint_config.max_keep_ckpts=1 >/dev/null &\n'
+    script += f'$CHECKPOINT_DIR/{work_dir_name} --cfg-options ' \
+              f'checkpoint_config.max_keep_ckpts={args.max_keep_ckpts} \\\n' \
+
+    # if output full log, redirect stdout and stderr to
+    # another log file in work dir
+    if args.full_log:
+        script += f'2>&1|tee $CHECKPOINT_DIR/{work_dir_name}/FULL_LOG.txt &\n'
+    else:
+        script += '>/dev/null &\n'
+
     if i != len(config_paths) - 1:
         script += '\n'
+
+    print(f'Successfully generated script for {config_name}')
 
 with open(args.output_file, 'w') as fo:
     fo.write(script)
