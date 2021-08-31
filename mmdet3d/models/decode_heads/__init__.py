b35d180fab2c084b88a8ef3d73618f838bd56811
hudingchang.vendor <hudingchang.vendor@sensetime.com>
2021-08-31 19:15:53 +0800

    support dgcnn

--- mmdet3d/models/decode_heads/__init__.py
+++ mmdet3d/models/decode_heads/__init__.py
@@ -3,4 +3,4 @@ from .dgcnn_head import DGCNNHead
 from .paconv_head import PAConvHead
 from .pointnet2_head import PointNet2Head
 
-__all__ = ['PointNet2Head', 'PAConvHead', 'DGCNNHead']
+__all__ = ['PointNet2Head', 'DGCNNHead', 'PAConvHead']
