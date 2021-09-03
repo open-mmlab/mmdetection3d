cad6a28e200a496b82bc5303e6597825fdd967d1
ChaimZhu <zhuchenming@pjlab.org.cn>
2021-09-03 14:58:10 +0800

    fix unitest

--- tests/test_models/test_heads/test_heads.py
+++ tests/test_models/test_heads/test_heads.py
@@ -395,13 +395,13 @@ def test_smoke_mono3d_head():
         conv_bias=True,
         dcn_on_last_conv=False)
 
-    self = build_head(head_cfg).cuda()
+    self = build_head(head_cfg)
 
-    feats = [torch.rand([2, 64, 32, 32], dtype=torch.float32).cuda()]
+    feats = [torch.rand([2, 64, 32, 32], dtype=torch.float32)]
 
     # test forward
     ret_dict = self(feats)
-    print(ret_dict)
+
     assert len(ret_dict) == 2
     assert len(ret_dict[0]) == 1
     assert ret_dict[0][0].shape == torch.Size([2, 3, 32, 32])
@@ -410,23 +410,20 @@ def test_smoke_mono3d_head():
     # test loss
     gt_bboxes = [
         torch.Tensor([[1.0, 2.0, 20.0, 40.0], [45.0, 50.0, 80.0, 70.1],
-                      [34.0, 39.0, 65.0, 64.0]]).cuda(),
+                      [34.0, 39.0, 65.0, 64.0]]),
         torch.Tensor([[11.0, 22.0, 29.0, 31.0], [41.0, 55.0, 60.0, 99.0],
-                      [29.0, 29.0, 65.0, 56.0]]).cuda()
+                      [29.0, 29.0, 65.0, 56.0]])
     ]
     gt_bboxes_3d = [
-        CameraInstance3DBoxes(torch.rand([3, 7], device='cuda'), box_dim=7),
-        CameraInstance3DBoxes(torch.rand([3, 7], device='cuda'), box_dim=7)
+        CameraInstance3DBoxes(torch.rand([3, 7]), box_dim=7),
+        CameraInstance3DBoxes(torch.rand([3, 7]), box_dim=7)
     ]
-    gt_labels = [torch.randint(0, 3, [3], device='cuda') for i in range(2)]
+    gt_labels = [torch.randint(0, 3, [3]) for i in range(2)]
     gt_labels_3d = gt_labels
-    centers2d = [
-        torch.randint(0, 60, (3, 2)).cuda(),
-        torch.randint(0, 40, (3, 2)).cuda()
-    ]
+    centers2d = [torch.randint(0, 60, (3, 2)), torch.randint(0, 40, (3, 2))]
     depths = [
-        torch.rand([3], dtype=torch.float32).cuda(),
-        torch.rand([3], dtype=torch.float32).cuda()
+        torch.rand([3], dtype=torch.float32),
+        torch.rand([3], dtype=torch.float32)
     ]
     attr_labels = None
     img_metas = [
