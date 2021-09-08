a5622c58f8e47c8603198d8ebe7162c3243ea963
ChaimZhu <zhuchenming@pjlab.org.cn>
2021-09-08 15:31:39 +0800

    add assert message

--- mmdet3d/core/visualizer/image_vis.py
+++ mmdet3d/core/visualizer/image_vis.py
@@ -188,10 +188,9 @@ def draw_camera_bbox3d_on_img(bboxes3d,
     points_3d = corners_3d.reshape(-1, 3)
     if not isinstance(cam2img, torch.Tensor):
         cam2img = torch.from_numpy(np.array(cam2img))
-    if cam2img.shape[0] == 3:
-        cam2img = cam2img.reshape(3, 3).float().cpu()
-    else:
-        cam2img = cam2img.reshape(4, 4).float().cpu()
+    assert (cam2img.shape == torch.size([3, 3])
+            or cam2img.shape == torch.size([4, 4]))
+    cam2img = cam2img.float().cpu()
 
     # project to 2d to get image coords (uv)
     uv_origin = points_cam2img(points_3d, cam2img)
