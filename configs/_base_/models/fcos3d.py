911a3333c150c1e816cbc283496770d90688dfe8
Tai-Wang <tab_wang@outlook.com>
2021-09-15 21:31:40 +0800

    [Feature] FCOS3D BBox Coder (#940)
    * FCOS3D BBox Coder
    
    * Add unit tests
    
    * Change the value from long to float/double
    
    * Rename bbox_out as bbox
    
    * Add comments to forward returns

--- configs/_base_/models/fcos3d.py
+++ configs/_base_/models/fcos3d.py
@@ -55,6 +55,7 @@ model = dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
         loss_centerness=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        bbox_coder=dict(type='FCOS3DBBoxCoder', code_size=9),
         norm_on_bbox=True,
         centerness_on_reg=True,
         center_sampling=True,
