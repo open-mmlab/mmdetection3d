a10df3198fc98c50f9e808b9d81780c3e6fb33ca
Tai-Wang <tab_wang@outlook.com>
2021-09-15 20:17:50 +0800

    Support array converter and add unit tests

--- mmdet3d/core/bbox/structures/utils.py
+++ mmdet3d/core/bbox/structures/utils.py
@@ -213,36 +213,36 @@ def points_cam2img(points_3d, proj_mat, with_depth=False):
     return point_2d_res
 
 
-def points_img2cam(points, view):
+@array_converter(apply_to=('points', 'cam2img'))
+def points_img2cam(points, cam2img):
     """Project points in image coordinates to camera coordinates.
 
     Args:
         points (torch.Tensor): 2.5D points in 2D images, [N, 3],
             3 corresponds with x, y in the image and depth.
-        view (np.ndarray): Camera instrinsic matrix. The shape can be [3, 3],
-            [3, 4] or [4, 4].
+        cam2img (torch.Tensor): Camera instrinsic matrix. The shape can be
+            [3, 3], [3, 4] or [4, 4].
 
     Returns:
         torch.Tensor: points in 3D space. [N, 3],
             3 corresponds with x, y, z in 3D space.
     """
-    assert view.shape[0] <= 4
-    assert view.shape[1] <= 4
+    assert cam2img.shape[0] <= 4
+    assert cam2img.shape[1] <= 4
     assert points.shape[1] == 3
 
-    points2D = points[:, :2]
+    xys = points[:, :2]
     depths = points[:, 2].view(-1, 1)
-    unnorm_points2D = torch.cat([points2D * depths, depths], dim=1)
+    unnormed_xys = torch.cat([xys * depths, depths], dim=1)
 
-    viewpad = torch.eye(4, dtype=points2D.dtype, device=points2D.device)
-    viewpad[:view.shape[0], :view.shape[1]] = points2D.new_tensor(view)
-    inv_viewpad = torch.inverse(viewpad).transpose(0, 1)
+    pad_cam2img = torch.eye(4, dtype=xys.dtype, device=xys.device)
+    pad_cam2img[:cam2img.shape[0], :cam2img.shape[1]] = cam2img
+    inv_pad_cam2img = torch.inverse(pad_cam2img).transpose(0, 1)
 
     # Do operation in homogenous coordinates.
-    nbr_points = unnorm_points2D.shape[0]
-    homo_points2D = torch.cat(
-        [unnorm_points2D, points2D.new_ones((nbr_points, 1))], dim=1)
-    points3D = torch.mm(homo_points2D, inv_viewpad)[:, :3]
+    num_points = unnormed_xys.shape[0]
+    homo_xys = torch.cat([unnormed_xys, xys.new_ones((num_points, 1))], dim=1)
+    points3D = torch.mm(homo_xys, inv_pad_cam2img)[:, :3]
 
     return points3D
 
