25a18af6013259751c7f6320b47134ed6e77dafa
fengzongbao <mickeyouou@gmail.com>
2021-09-26 17:41:59 +0800

    dynamic voxelization with centerpoint

--- mmdet3d/models/detectors/centerpoint.py
+++ mmdet3d/models/detectors/centerpoint.py
@@ -1,6 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
 
+from mmcv.runner import force_fp32
+from torch.nn import functional as F
 from mmdet3d.core import bbox3d2result, merge_aug_bboxes_3d
 from mmdet.models import DETECTORS
 from .mvx_two_stage import MVXTwoStageDetector
@@ -37,9 +39,13 @@ class CenterPoint(MVXTwoStageDetector):
         """Extract features of points."""
         if not self.with_pts_bbox:
             return None
-        voxels, num_points, coors = self.voxelize(pts)
-
-        voxel_features = self.pts_voxel_encoder(voxels, num_points, coors)
+        if self.pts_voxel_layer.max_num_points == -1:
+            voxels, coors = self.voxelize(pts)
+            voxel_features, coors = self.pts_voxel_encoder(
+                voxels, coors)
+        else:
+            voxels, num_points, coors = self.voxelize(pts)
+            voxel_features = self.pts_voxel_encoder(voxels, num_points, coors)
         batch_size = coors[-1, 0] + 1
         x = self.pts_middle_encoder(voxel_features, coors, batch_size)
         x = self.pts_backbone(x)
@@ -193,3 +199,27 @@ class CenterPoint(MVXTwoStageDetector):
             pts_bbox = self.aug_test_pts(pts_feats, img_metas, rescale)
             bbox_list.update(pts_bbox=pts_bbox)
         return [bbox_list]
+
+    @torch.no_grad()
+    @force_fp32()
+    def voxelize(self, points):
+        """Apply dynamic voxelization to points.
+
+        Args:
+            points (list[torch.Tensor]): Points of each sample.
+
+        Returns:
+            tuple[torch.Tensor]: Concatenated points and coordinates.
+        """
+        coors = []
+        # dynamic voxelization only provide a coors mapping
+        for res in points:
+            res_coors = self.pts_voxel_layer(res)
+            coors.append(res_coors)
+        points = torch.cat(points, dim=0)
+        coors_batch = []
+        for i, coor in enumerate(coors):
+            coor_pad = F.pad(coor, (1, 0), mode='constant', value=i)
+            coors_batch.append(coor_pad)
+        coors_batch = torch.cat(coors_batch, dim=0)
+        return points, coors_batch
