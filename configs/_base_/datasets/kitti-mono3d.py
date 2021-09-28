9777d76ee2e62a6fcd47818446c8465f3d9c5a92
ChaimZhu <zhuchenming@pjlab.org.cn>
2021-09-28 11:49:12 +0800

    update pipelines

--- configs/_base_/datasets/kitti-mono3d.py
+++ configs/_base_/datasets/kitti-mono3d.py
@@ -1,6 +1,6 @@
 dataset_type = 'KittiMonoDataset'
 data_root = 'data/kitti/'
-class_names = ['Pedestrain', 'Cyclist', 'Car']
+class_names = ['Pedestrian', 'Cyclist', 'Car']
 # Input modality for kitti dataset, this is consistent with the submission
 # format which requires the information in input_modality.
 input_modality = dict(
@@ -11,8 +11,18 @@ input_modality = dict(
     use_external=False)
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+file_client_args = dict(backend='disk')
+# Uncomment the following if use ceph or other file clients.
+# See https://mmcv.readthedocs.io/en/latest/api.html#mmcv.fileio.FileClient
+# for more details.
+# file_client_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/kitti/': 's3://openmmlab/datasets/detection3d/kitti/',
+#         'data/kitti/': 's3://openmmlab/datasets/detection3d/kitti/'
+#     }))
 train_pipeline = [
-    dict(type='LoadImageFromFileMono3D'),
+    dict(type='LoadImageFromFileMono3D', file_client_args=file_client_args),
     dict(
         type='LoadAnnotations3D',
         with_bbox=True,
@@ -21,8 +31,9 @@ train_pipeline = [
         with_bbox_3d=True,
         with_label_3d=True,
         with_bbox_depth=True),
-    dict(type='AffineResize', img_scale=(1280, 384), down_ratio=4),
     dict(type='RandomFlip3D', flip_ratio_bev_horizontal=0.5),
+    dict(type='RandomShiftScale', shift_scale=(0.2, 0.4), aug_prob=0.3),
+    dict(type='AffineResize', img_scale=(1280, 384), down_ratio=4),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle3D', class_names=class_names),
@@ -34,14 +45,13 @@ train_pipeline = [
         ]),
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFileMono3D'),
+    dict(type='LoadImageFromFileMono3D', file_client_args=file_client_args),
     dict(
         type='MultiScaleFlipAug',
         img_scale=(1280, 384),
         flip=False,
         transforms=[
             dict(type='AffineResize', img_scale=(1280, 384), down_ratio=4),
-            dict(type='RandomFlip3D'),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='Pad', size_divisor=32),
             dict(
@@ -56,10 +66,23 @@ test_pipeline = [
 eval_pipeline = [
     dict(type='LoadImageFromFileMono3D'),
     dict(
+        type='LoadAnnotations3D',
+        with_bbox=True,
+        with_label=True,
+        with_attr_label=False,
+        with_bbox_3d=True,
+        with_label_3d=True,
+        with_bbox_depth=True),
+    dict(
         type='DefaultFormatBundle3D',
         class_names=class_names,
         with_label=False),
-    dict(type='Collect3D', keys=['img'])
+    dict(
+        type='Collect3D',
+        keys=[
+            'img', 'gt_bboxes', 'gt_labels', 'gt_bboxes_3d', 'gt_labels_3d',
+            'centers2d', 'depths'
+        ])
 ]
 
 data = dict(
