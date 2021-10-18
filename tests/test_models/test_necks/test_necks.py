5e686da8649221019b85afe1154c37ccf3bc5ea3
Wenhao Wu <wenhaowu.chn@gmail.com>
2021-10-18 11:55:46 +0800

    refine unittest

--- tests/test_models/test_necks/test_necks.py
+++ tests/test_models/test_necks/test_necks.py
@@ -61,43 +61,33 @@ def test_imvoxel_neck():
 
 def test_fp_neck():
 
-    points = torch.rand(3, 100, 4)
+    xyzs = [16384, 4096, 1024, 256, 64]
+    feat_channels = [1, 96, 256, 512, 1024]
+    channel_num = 5
 
-    backbone_cfg = dict(
-        type='PointNet2SAMSG',
-        in_channels=4,
-        num_points=(4096, 1024, 256, 64),
-        radii=((0.1, 0.5), (0.5, 1.0), (1.0, 2.0), (2.0, 4.0)),
-        num_samples=((16, 32), (16, 32), (16, 32), (16, 32)),
-        sa_channels=(((16, 16, 32), (32, 32, 64)), ((64, 64, 128), (64, 96,
-                                                                    128)),
-                     ((128, 196, 256), (128, 196, 256)), ((256, 256, 512),
-                                                          (256, 384, 512))),
-        fps_mods=(('D-FPS'), ('D-FPS'), ('D-FPS'), ('D-FPS')),
-        fps_sample_range_lists=((-1), (-1), (-1), (-1)),
-        aggregation_channels=(None, None, None, None),
-        dilated_group=(False, False, False, False),
-        out_indices=(0, 1, 2, 3),
-        norm_cfg=dict(type='BN2d', eps=1e-3, momentum=0.1),
-        sa_cfg=dict(
-            type='PointSAModuleMSG',
-            pool_mod='max',
-            use_xyz=True,
-            normalize_xyz=False))
+    sa_xyz = [torch.rand(3, xyzs[i], 3) for i in range(channel_num)]
+    sa_features = [
+        torch.rand(3, feat_channels[i], xyzs[i]) for i in range(channel_num)
+    ]
+
     neck_cfg = dict(
         type='PointNetFPNeck',
         fp_channels=((1536, 512, 512), (768, 512, 512), (608, 256, 256),
                      (257, 128, 128)))
 
-    backbone = build_backbone(backbone_cfg)
-    backbone.init_weights()
     neck = build_neck(neck_cfg)
     neck.init_weights()
 
-    feats_sa = backbone(points)
+    if torch.cuda.is_available():
+        sa_xyz = [x.cuda() for x in sa_xyz]
+        sa_features = [x.cuda() for x in sa_features]
+        neck.cuda()
+
+    feats_sa = {'sa_xyz': sa_xyz, 'sa_features': sa_features}
     outputs = neck(feats_sa)
-    assert outputs['fp_xyz'].cpu().numpy().shape == (3, 100, 3)
-    assert outputs['fp_features'].detach().cpu().numpy().shape == (3, 128, 100)
+    assert outputs['fp_xyz'].cpu().numpy().shape == (3, 16384, 3)
+    assert outputs['fp_features'].detach().cpu().numpy().shape == (3, 128,
+                                                                   16384)
 
 
 def test_dla_neck():
