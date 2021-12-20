3f26c71be6b38ee2f539eca19fc803729e100601
ChaimZhu <zhuchenming@pjlab.org.cn>
2021-12-20 17:40:57 +0800

    fix coder

--- mmdet3d/core/bbox/coders/monoflex_bbox_coder.py
+++ mmdet3d/core/bbox/coders/monoflex_bbox_coder.py
@@ -13,9 +13,10 @@ class MonoFlexCoder(BaseBBoxCoder):
     Args:
         depth_mode (str): The depth mode for direct depth calculation.
         base_depth (tuple[float]): Depth references for decode box depth.
-        depth_range (str): Depth range of predicted depth.
+        depth_range (list): Depth range of predicted depth.
         use_combined_depth (bool): Whether to use combined depth (direct depth
             and depth from keypoints).
+        uncertainty_range(list): Uncertainty range of predicted depth.
         dims_mean (tuple[tuple[float]]): Dimensions mean of decode bbox
             dimensions [l, h, w] for each category.
         dims_mean (tuple[tuple[float]]): Dimensions std of decode bbox
@@ -35,6 +36,7 @@ class MonoFlexCoder(BaseBBoxCoder):
         base_depth,
         depth_range,
         use_combined_depth,
+        uncertainty_range,
         dims_mean,
         dims_std,
         dims_modes,
@@ -50,6 +52,7 @@ class MonoFlexCoder(BaseBBoxCoder):
         self.base_depth = base_depth
         self.depth_range = depth_range
         self.use_combined_depth = use_combined_depth
+        self.uncertainty_range = uncertainty_range
 
         # dimensions related
         self.dims_mean = dims_mean
@@ -91,59 +94,66 @@ class MonoFlexCoder(BaseBBoxCoder):
 
         return batch_bboxes
 
-    def decode(self, reg, points, labels, down_ratio, cam2imgs):
+    def decode(self, reg, labels, down_ratio, cam2imgs):
         """Decode regression into 3D predictions.
 
         Args:
             reg (Tensor): Batch regression for each predict center2d point.
-                shape: (batch * K (max_objs), C)
-            points(Tensor): Batch projected bbox centers on image plane.
-                shape: (batch * K (max_objs) , 2)
+                shape: (N, C)
             labels (Tensor): Batch predict class label for each predict
                 center2d point.
-                shape: (batch * K (max_objs), )
+                shape: (N, )
             down_ratio (int): The stride of feature map.
             cam2imgs (Tensor): Batch images' camera intrinsic matrix.
-                shape: kitti (batch, 4, 4)  nuscenes (batch, 3, 3)
+                shape: kitti (N, 4, 4)  nuscenes (N, 3, 3)
 
         Return:
-            tuple(Tensor): The tuple has components below:
-                - locations (Tensor): Centers of 3D boxes.
-                    shape: (batch * K (max_objs), 3)
-                - dimensions (Tensor): Dimensions of 3D boxes.
-                    shape: (batch * K (max_objs), 3)
-                - orientations (Tensor): Orientations of 3D
-                    boxes.
-                    shape: (batch * K (max_objs), 1)
+            dict: The 3D prediction dict decoded from regression map.
         """
 
+        # 4 dimensions for FCOS style regression
+        pred_bbox2d = reg[:, 0:4]
+        # 2 dimensions for projected centers2d offsets
         pred_offsets2d = reg[:, 4:6]
+
+        # 3 dimensions for 3D bbox dimensions offsets
         pred_dimensions_offsets3d = reg[:, 29:32]
+
+        # the first 8 dimensions are for orientation bin classification
+        # and the second 8 dimensions are for orientation offsets.
         pred_orientations = torch.cat((reg[:, 32:40], reg[:, 40:48]), dim=1)
+
+        # 3 dimensions for the uncertainties of the solved depths from
+        # groups of keypoints
+        pred_keypoints_depth_uncertainty = reg[:, 26:29]
+
+        # 1 dimension for the uncertainty of directly regressed depth
+        pred_direct_depth_uncertainty = reg[:, 49:50].squeeze(-1)
+
+        # 2 dimension of offsets x keypoints (8 corners + top/bottom center)
+        pred_keypoints2d = reg[:, 6:26]
+
+        # 1 dimension for depth offsets
+        pred_depth_offsets = reg[:, 48:49].squeeze(-1)
+
         # decode the pred residual dimensions to real dimensions
-        # (B* max_objs, 3)
         pred_dimensions = self.decode_dimension(labels,
                                                 pred_dimensions_offsets3d)
-        # decode the depth with direct regression
-        pred_depth_offsets = reg[:, 48:49].squeeze(-1)
-        # (B* max_objs, )
         pred_direct_depth = self.decode_direct_depth(pred_depth_offsets)
-        # predict uncertainty of directly regressed depth
-        pred_depth_uncertainty = \
-            reg[:, 49:50].squeeze(-1)
-        # predictions for keypoints
-        pred_keypoints2d = reg[:, 6:26]
-        # (B * max_objs, 3)
         pred_keypoints_depth = self.decode_depth_from_keypoints(
             pred_keypoints2d, pred_dimensions, cam2imgs, down_ratio)
-        # predict the uncertainties of the solved depths from
-        # groups of keypoints
-        pred_corner_offset_uncertainty = \
-            reg[:, 26:29]
+
+        pred_direct_depth_uncertainty = torch.clamp(
+            pred_direct_depth_uncertainty, self.uncertainty_range[0],
+            self.uncertainty_range[1])
+        pred_keypoints_depth_uncertainty = torch.clamp(
+            pred_keypoints_depth_uncertainty, self.uncertainty_range[0],
+            self.uncertainty_range[1])
+
         if self.use_combined_depth:
             pred_combined_uncertainty = torch.cat(
-                (pred_depth_uncertainty.unsqueeze(-1),
-                 pred_corner_offset_uncertainty),
+                (pred_direct_depth_uncertainty.unsqueeze(-1),
+                 pred_keypoints_depth_uncertainty),
                 dim=1).exp()
             pred_combined_depth = torch.cat(
                 (pred_direct_depth.unsqueeze(-1), pred_keypoints_depth), dim=1)
@@ -151,29 +161,34 @@ class MonoFlexCoder(BaseBBoxCoder):
             pred_uncertainty_weights = \
                 pred_uncertainty_weights / \
                 pred_uncertainty_weights.sum(dim=1, keepdim=True)
-            pred_corner_depth = torch.sum(
+            pred_combined_depth = torch.sum(
                 pred_combined_depth * pred_uncertainty_weights, dim=1)
+        else:
+            pred_combined_depth = None
 
-        # compute the corners
-        pred_locations = self.decode_location(points, pred_offsets2d,
-                                              pred_corner_depth, cam2imgs,
-                                              down_ratio)
-        # decode yaws and alphas
-        pred_orientations, _ = self.decode_orientation(pred_orientations,
-                                                       pred_locations)
+        preds = dict(
+            bbox2d=pred_bbox2d,
+            dimensions=pred_dimensions,
+            offsets2d=pred_offsets2d,
+            direct_depth=pred_direct_depth,
+            keypoints=pred_keypoints2d,
+            keypoints_depth=pred_keypoints_depth,
+            combined_depth=pred_combined_depth,
+            orientations=pred_orientations,
+        )
 
-        return pred_locations, pred_dimensions, pred_orientations,
+        return preds
 
     def decode_direct_depth(self, depth_offsets):
         """Transform depth offset to depth.
 
         Args:
             depth_offsets (torch.Tensor): Predicted depth offsets.
-                shape: (B * max_objs, )
+                shape: (N, )
 
         Return:
             torch.Tensor: Directly regressed depth.
-                shape: (B * max_objs, )
+                shape: (N, )
         """
         if self.depth_mode == 'exp':
             depth = depth_offsets.exp()
@@ -203,29 +218,25 @@ class MonoFlexCoder(BaseBBoxCoder):
         Args:
             centers2d_target (torch.Tensor): Projected 3D target centers
                 onto 2D images.
-                shape: (B * max_objs, 2)
+                shape: (N, 2)
             offsets2d (torch.Tensor): The offsets between real centers2d
                 and centers2d_target.
-                shape: (B * max_objs , 2)
+                shape: (N , 2)
             depths (torch.Tensor): Depths of objects.
-                shape: (B * max_objs, )
+                shape: (N, )
             cam2imgs (torch.Tensor): Batch images' camera intrinsic matrix.
-                shape: kitti (batch, 4, 4)  nuscenes (batch, 3, 3)
+                shape: kitti (N, 4, 4)  nuscenes (N, 3, 3)
             down_ratio (int): The stride of feature map.
             pad_mode (str, optional): Padding mode used in
                 training data augmentation.
 
         Return:
             tuple(torch.Tensor): Centers of 3D boxes.
-                shape: (B * max_objs, 3)
+                shape: (N, 3)
         """
-        N = centers2d_target.shape[0]
-        N_batch = cam2imgs.shape[0]
-        batch_id = torch.arange(N_batch).unsqueeze(1)
-        obj_id = batch_id.repeat(1, N // N_batch).flatten()
-
-        # (B * max_objs, 4, 4)
-        cam2imgs_inv = cam2imgs.inverse()[obj_id]
+        N = cam2imgs.shape[0]
+        # (N, 4, 4)
+        cam2imgs_inv = cam2imgs.inverse()
         if pad_mode == 'default':
             centers2d_img = (centers2d_target + offsets2d) * down_ratio
         else:
@@ -241,78 +252,119 @@ class MonoFlexCoder(BaseBBoxCoder):
 
         return locations[:, :3]
 
-    def decode_depth_from_keypoints(self, pred_keypoints2d, pred_dimensions,
-                                    cam2imgs, down_ratio):
-        """Retrieve object depth.
+    def decode_depth_from_keypoints(self,
+                                    keypoints2d,
+                                    dimensions,
+                                    cam2imgs,
+                                    down_ratio=4,
+                                    group0_index=[(7, 3), (0, 4)],
+                                    group1_index=[(2, 6), (1, 5)]):
+        """Decode depth form three groups of keypoints and geometry projection
+        model. 2D keypoints inlucding 8 coreners and top/bottom centers will be
+        divided into three groups which will be used to calculate three depths
+        of object.
+
+        .. code-block:: none
+
+                Group center keypoints:
+
+                             + --------------- +
+                            /|   top center   /|
+                           / |      .        / |
+                          /  |      |       /  |
+                         + ---------|----- +   +
+                         |  /       |      |  /
+                         | /        .      | /
+                         |/ bottom center  |/
+                         + --------------- +
+
+                Group 0 keypoints:
+
+                             0
+                             + -------------- +
+                            /|               /|
+                           / |              / |
+                          /  |            5/  |
+                         + -------------- +   +
+                         |  /3            |  /
+                         | /              | /
+                         |/               |/
+                         + -------------- + 6
+
+                Group 1 keypoints:
+
+                                               4
+                             + -------------- +
+                            /|               /|
+                           / |              / |
+                          /  |             /  |
+                       1 + -------------- +   + 7
+                         |  /             |  /
+                         | /              | /
+                         |/               |/
+                       2 + -------------- +
+
 
         Args:
-            pred_keypoints2d (torch.Tensor): Keypoints of objects.
-                shape: (B * max_objs, 10, 3)
-            pred_dimensions (torch.Tensor): Dimensions of objetcts.
-                shape: (B * max_objs , 3)
+            keypoints2d (torch.Tensor): Keypoints of objects.
+                8 vertices + top/bottom center.
+                shape: (N, 10, 2)
+            dimensions (torch.Tensor): Dimensions of objetcts.
+                shape: (N, 3)
             cam2imgs (torch.Tensor): Batch images' camera intrinsic matrix.
-                shape: kitti (batch, 4, 4)  nuscenes (batch, 3, 3)
-            down_ratio (int): The stride of feature map.
+                shape: kitti (N, 4, 4)  nuscenes (N, 3, 3)
+            down_ratio (int, opitonal): The stride of feature map.
+                Defaults: 4.
+            group0_index(list[tuple[int]], optional): Keypoints group 0
+                of index to calculate the depth.
+                Defaults: [0, 3, 4, 7].
+            group1_index(list[tuple[int]], optional): Keypoints group 1
+                of index to calculate the depth.
+                Defaults: [1, 2, 5, 6]
 
         Return:
-            tuple(torch.Tensor): Centers of 3D boxes.
-                shape: (B * max_objs, 3)
+            tuple(torch.Tensor): Depth computed from three groups of
+                keypoints (top/bottom, group0, group1)
+                shape: (N, 3)
         """
-        # (B * max_objs, 10, 2) 8 projected corners,
-        # top center and bottom center
-        pred_keypoints2d = pred_keypoints2d.view(-1, 10, 2)
 
-        N = pred_keypoints2d.shape[0]
-        # (B * max_objs, 3)
-        pred_height_3d = pred_dimensions[:, 1].clone()
-        # (B, 4, 4)
-        N_batch = cam2imgs.shape[0]
-        batch_id = torch.arange(N_batch).unsqueeze(1)
-        obj_id = batch_id.repeat(1, N // N_batch).flatten()
-        # (B * max_objs, 4, 4)
-        cam2imgs = cam2imgs[obj_id]
-        # (B * max_objs, )
+        pred_height_3d = dimensions[:, 1].clone()
         f_u = cam2imgs[:, 0, 0]
-        # (B * max_objs, )
-        center_height = pred_keypoints2d[:, -2, 1] - pred_keypoints2d[:, -1, 1]
-        # (B * max_objs, 2)
-        corner_02_height = \
-            pred_keypoints2d[:, [0, 2], 1] - pred_keypoints2d[:, [4, 6], 1]
-        corner_13_height = \
-            pred_keypoints2d[:, [1, 3], 1] - pred_keypoints2d[:, [5, 7], 1]
-
+        center_height = keypoints2d[:, -2, 1] - keypoints2d[:, -1, 1]
+        corner_group0_height = keypoints2d[:, group0_index[0], 1] \
+            - keypoints2d[:, group0_index[1], 1]
+        corner_group1_height = keypoints2d[:, group1_index[0], 1] \
+            - keypoints2d[:, group1_index[1], 1]
         center_depth = f_u * pred_height_3d / (
             F.relu(center_height) * down_ratio + self.eps)
-        corner_02_depth = f_u * pred_height_3d / (
-            F.relu(corner_02_height) * down_ratio + self.eps)
-        corner_13_depth = f_u * pred_height_3d / (
-            F.relu(corner_13_height) * down_ratio + self.eps)
-        corner_02_depth = corner_02_depth.mean(dim=1)
-        corner_13_depth = corner_13_depth.mean(dim=1)
+        corner_group0_depth = (f_u * pred_height_3d).unsqueeze(-1) / (
+            F.relu(corner_group0_height) * down_ratio + self.eps)
+        corner_group1_depth = (f_u * pred_height_3d).unsqueeze(-1) / (
+            F.relu(corner_group1_height) * down_ratio + self.eps)
 
-        pred_keypoints_depth = torch.stack(
-            (center_depth, corner_02_depth, corner_13_depth), dim=1)
-        # (B*max_objs, 3)
-        pred_keypoints_depth = torch.clamp(
-            pred_keypoints_depth,
-            min=self.depth_range[0],
-            max=self.depth_range[1])
+        corner_group0_depth = corner_group0_depth.mean(dim=1)
+        corner_group1_depth = corner_group1_depth.mean(dim=1)
 
-        return pred_keypoints_depth
+        keypoints_depth = torch.stack(
+            (center_depth, corner_group0_depth, corner_group1_depth), dim=1)
+        keypoints_depth = torch.clamp(
+            keypoints_depth, min=self.depth_range[0], max=self.depth_range[1])
+
+        return keypoints_depth
 
     def decode_dimension(self, labels, dims_offset):
         """Retrieve object dimensions.
 
         Args:
             labels (torch.Tensor): Each points' category id.
-                shape (B* max_objs, K)
+                shape (N, K)
             dims_offset (torch.Tensor): Dimension offsets.
-                shape (B* max_objs, 3)
+                shape (N, 3)
 
         Returns:
             torch.Tensor: Shape (N, 3)
         """
-        labels = labels.flatten().long()
+        labels = labels.long()
         dims_std = dims_offset.new_tensor(self.dims_std)
         dims_mean = dims_offset.new_tensor(self.dims_mean)
         cls_dimension_mean = dims_mean[labels, :]
@@ -334,9 +386,9 @@ class MonoFlexCoder(BaseBBoxCoder):
         Args:
             vector_ori(torch.Tensor): Local vector orientation
                 in [axis_cls, head_cls, sin, cos] format.
-                shape (B * max_objs, num_dir_bin * 4)
+                shape (N, num_dir_bin * 4)
             locations(torch.Tensor): Object location.
-                shape (B * max_objs, 3)
+                shape (N, 3)
 
         Returns:
             tuple[torch.Tensor]: yaws and alphas of 3d bboxes.
