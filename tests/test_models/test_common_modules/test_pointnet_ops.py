27e6af29a40fd84623fc32ed80d954ea5f6c9b13
Danila Rukhovich <danrukh@gmail.com>
2022-03-01 12:12:03 +0300

    try revert imports

--- tests/test_models/test_common_modules/test_pointnet_ops.py
+++ tests/test_models/test_common_modules/test_pointnet_ops.py
@@ -2,16 +2,9 @@
 import pytest
 import torch
 
-from mmdet3d.ops import (
-    ball_query,
-    furthest_point_sample,
-    furthest_point_sample_with_dist,
-    gather_points,
-    grouping_operation,
-    knn,
-    three_interpolate,
-    three_nn,
-)
+from mmdet3d.ops import (ball_query, furthest_point_sample,
+                         furthest_point_sample_with_dist, gather_points,
+                         grouping_operation, knn, three_interpolate, three_nn)
 
 
 def test_fps():
