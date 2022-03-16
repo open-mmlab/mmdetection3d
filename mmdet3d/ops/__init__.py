c0a0bf14984f21a05c196a7146d4529b51e39431
Wenhao Wu <wenhaowu.chn@gmail.com>
2022-03-16 10:38:06 +0800

    recover ops/__init__.py for bc breaking

--- mmdet3d/ops/__init__.py
+++ mmdet3d/ops/__init__.py
@@ -1,7 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmcv.ops import (RoIAlign, SigmoidFocalLoss, get_compiler_version,
-                      get_compiling_cuda_version, nms, roi_align,
-                      sigmoid_focal_loss)
+from mmcv.ops import DynamicScatter, GroupAll
+from mmcv.ops import PointsSampler as Points_Sampler
+from mmcv.ops import (QueryAndGroup, RoIAlign, RoIAwarePool3d, RoIPointPool3d,
+                      SigmoidFocalLoss, Voxelization, assign_score_withk,
+                      ball_query, dynamic_scatter, furthest_point_sample,
+                      furthest_point_sample_with_dist, gather_points,
+                      get_compiler_version, get_compiling_cuda_version,
+                      group_points, grouping_operation, knn, nms,
+                      points_in_boxes_all, points_in_boxes_cpu,
+                      points_in_boxes_part, roi_align, sigmoid_focal_loss,
+                      three_interpolate, three_nn, voxelization)
 
 from .dgcnn_modules import DGCNNFAModule, DGCNNFPModule, DGCNNGFModule
 from .norm import NaiveSyncBatchNorm1d, NaiveSyncBatchNorm2d
@@ -16,11 +24,17 @@ from .sparse_block import (SparseBasicBlock, SparseBottleneck,
 __all__ = [
     'nms', 'soft_nms', 'RoIAlign', 'roi_align', 'get_compiler_version',
     'get_compiling_cuda_version', 'NaiveSyncBatchNorm1d',
-    'NaiveSyncBatchNorm2d', 'batched_nms', 'sigmoid_focal_loss',
-    'SigmoidFocalLoss', 'PointSAModule', 'PointSAModuleMSG', 'PointFPModule',
-    'DGCNNFPModule', 'DGCNNGFModule', 'DGCNNFAModule', 'get_compiler_version',
-    'get_compiling_cuda_version', 'build_sa_module', 'PAConv', 'PAConvCUDA',
+    'NaiveSyncBatchNorm2d', 'batched_nms', 'Voxelization', 'voxelization',
+    'dynamic_scatter', 'DynamicScatter', 'sigmoid_focal_loss',
+    'SigmoidFocalLoss', 'SparseBasicBlock', 'SparseBottleneck',
+    'RoIAwarePool3d', 'points_in_boxes_part', 'points_in_boxes_cpu',
+    'make_sparse_convmodule', 'ball_query', 'knn', 'furthest_point_sample',
+    'furthest_point_sample_with_dist', 'three_interpolate', 'three_nn',
+    'gather_points', 'grouping_operation', 'group_points', 'GroupAll',
+    'QueryAndGroup', 'PointSAModule', 'PointSAModuleMSG', 'PointFPModule',
+    'DGCNNFPModule', 'DGCNNGFModule', 'DGCNNFAModule', 'points_in_boxes_all',
+    'get_compiler_version', 'assign_score_withk', 'get_compiling_cuda_version',
+    'Points_Sampler', 'build_sa_module', 'PAConv', 'PAConvCUDA',
     'PAConvSAModuleMSG', 'PAConvSAModule', 'PAConvCUDASAModule',
-    'PAConvCUDASAModuleMSG', 'SparseBasicBlock', 'SparseBottleneck',
-    'make_sparse_convmodule'
+    'PAConvCUDASAModuleMSG', 'RoIPointPool3d'
 ]
