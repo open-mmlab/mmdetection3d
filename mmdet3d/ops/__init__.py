6a703790cb6e1012a311ef08e32bf9dd6712aadc
Wenhao Wu <wenhaowu.chn@gmail.com>
2022-03-16 10:54:55 +0800

    add source of ops from mmcv

--- mmdet3d/ops/__init__.py
+++ mmdet3d/ops/__init__.py
@@ -1,15 +1,23 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmcv.ops import DynamicScatter, GroupAll
-from mmcv.ops import PointsSampler as Points_Sampler
-from mmcv.ops import (QueryAndGroup, RoIAlign, RoIAwarePool3d, RoIPointPool3d,
-                      SigmoidFocalLoss, Voxelization, assign_score_withk,
-                      ball_query, dynamic_scatter, furthest_point_sample,
-                      furthest_point_sample_with_dist, gather_points,
-                      get_compiler_version, get_compiling_cuda_version,
-                      group_points, grouping_operation, knn, nms,
-                      points_in_boxes_all, points_in_boxes_cpu,
-                      points_in_boxes_part, roi_align, sigmoid_focal_loss,
-                      three_interpolate, three_nn, voxelization)
+from mmcv.ops import (RoIAlign, SigmoidFocalLoss, get_compiler_version,
+                      get_compiling_cuda_version, nms, roi_align,
+                      sigmoid_focal_loss)
+from mmcv.ops.assign_score_withk import assign_score_withk
+from mmcv.ops.ball_query import ball_query
+from mmcv.ops.furthest_point_sample import (furthest_point_sample,
+                                            furthest_point_sample_with_dist)
+from mmcv.ops.gather_points import gather_points
+from mmcv.ops.group_points import GroupAll, QueryAndGroup, grouping_operation
+from mmcv.ops.knn import knn
+from mmcv.ops.points_in_boxes import (points_in_boxes_all, points_in_boxes_cpu,
+                                      points_in_boxes_part)
+from mmcv.ops.points_sampler import PointsSampler as Points_Sampler
+from mmcv.ops.roiaware_pool3d import RoIAwarePool3d
+from mmcv.ops.roipoint_pool3d import RoIPointPool3d
+from mmcv.ops.scatter_points import DynamicScatter, dynamic_scatter
+from mmcv.ops.three_interpolate import three_interpolate
+from mmcv.ops.three_nn import three_nn
+from mmcv.ops.voxelize import Voxelization, voxelization
 
 from .dgcnn_modules import DGCNNFAModule, DGCNNFPModule, DGCNNGFModule
 from .norm import NaiveSyncBatchNorm1d, NaiveSyncBatchNorm2d
@@ -30,9 +38,9 @@ __all__ = [
     'RoIAwarePool3d', 'points_in_boxes_part', 'points_in_boxes_cpu',
     'make_sparse_convmodule', 'ball_query', 'knn', 'furthest_point_sample',
     'furthest_point_sample_with_dist', 'three_interpolate', 'three_nn',
-    'gather_points', 'grouping_operation', 'group_points', 'GroupAll',
-    'QueryAndGroup', 'PointSAModule', 'PointSAModuleMSG', 'PointFPModule',
-    'DGCNNFPModule', 'DGCNNGFModule', 'DGCNNFAModule', 'points_in_boxes_all',
+    'gather_points', 'grouping_operation', 'GroupAll', 'QueryAndGroup',
+    'PointSAModule', 'PointSAModuleMSG', 'PointFPModule', 'DGCNNFPModule',
+    'DGCNNGFModule', 'DGCNNFAModule', 'points_in_boxes_all',
     'get_compiler_version', 'assign_score_withk', 'get_compiling_cuda_version',
     'Points_Sampler', 'build_sa_module', 'PAConv', 'PAConvCUDA',
     'PAConvSAModuleMSG', 'PAConvSAModule', 'PAConvCUDASAModule',
