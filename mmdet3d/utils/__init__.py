ff159fe7ea2a45f9138487c1c4977d2f67676ebc
VVsssssk <88368822+VVsssssk@users.noreply.github.com>
2022-04-20 19:29:26 +0800

    [Feature]Add loader args to config (#1388)
    * update pr
    
    * add ut
    
    * fix ut
    
    * fix ut err
    
    * fix ut name
    
    * resolve comment

--- mmdet3d/utils/__init__.py
+++ mmdet3d/utils/__init__.py
@@ -2,10 +2,11 @@
 from mmcv.utils import Registry, build_from_cfg, print_log
 
 from .collect_env import collect_env
+from .compat_cfg import compat_cfg
 from .logger import get_root_logger
 from .setup_env import setup_multi_processes
 
 __all__ = [
     'Registry', 'build_from_cfg', 'get_root_logger', 'collect_env',
-    'print_log', 'setup_multi_processes'
+    'print_log', 'setup_multi_processes', 'compat_cfg'
 ]
