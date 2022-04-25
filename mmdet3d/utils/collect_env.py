0e9341c644efb6b4222a1fab36c865c9494bbe03
VVsssssk <shenkun@pjlab.org.cn>
2022-04-25 11:39:32 +0800

    add spconv2 check in collect.env

--- mmdet3d/utils/collect_env.py
+++ mmdet3d/utils/collect_env.py
@@ -5,6 +5,7 @@ from mmcv.utils import get_git_hash
 import mmdet
 import mmdet3d
 import mmseg
+from mmdet3d.ops.spconv import spconv2_is_avalible
 
 
 def collect_env():
@@ -13,7 +14,7 @@ def collect_env():
     env_info['MMDetection'] = mmdet.__version__
     env_info['MMSegmentation'] = mmseg.__version__
     env_info['MMDetection3D'] = mmdet3d.__version__ + '+' + get_git_hash()[:7]
-
+    env_info['Spconv2 is avalible'] = spconv2_is_avalible
     return env_info
 
 
