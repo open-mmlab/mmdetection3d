e5c8741090012b3186b1802b4af87559766ee7e9
VVsssssk <shenkun@pjlab.org.cn>
2022-04-27 23:59:04 +0800

    fix docs

--- mmdet3d/ops/spconv/overwrite_spconv/write_spconv2.py
+++ mmdet3d/ops/spconv/overwrite_spconv/write_spconv2.py
@@ -40,10 +40,11 @@ def register_spconv2():
 
 
 def _save_to_state_dict(self, destination, prefix, keep_vars):
-    """Rewrite this func to resolve spconv ops kernel weights incompatible.
+    """Rewrite this func to compat the convolutional kernel weights between
+    spconv 1.x in MMCV and 2.x in spconv2.x.
 
-    MMCV spconv kernel weights is (D,H,W,in_channel,out_channel), spconv2.0
-    spconv kernel weights is (out_channel,D,H,W,in_channel).
+    Kernel weights in MMCV spconv has shape in (D,H,W,in_channel,out_channel) ,
+    while those in spcon2.x is in (out_channel,D,H,W,in_channel).
     """
     for name, param in self._parameters.items():
         if param is not None:
@@ -59,10 +60,11 @@ def _save_to_state_dict(self, destination, prefix, keep_vars):
 
 def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
                           missing_keys, unexpected_keys, error_msgs):
-    """Rewrite this func to resolve spconv ops kernel weights incompatible.
+    """Rewrite this func to compat the convolutional kernel weights between
+    spconv 1.x in MMCV and 2.x in spconv2.x.
 
-    MMCV spconv kernel weights is (D,H,W,in_channel,out_channel), spconv2.0
-    spconv kernel weights is (out_channel,D,H,W,in_channel).
+    Kernel weights in MMCV spconv has shape in (D,H,W,in_channel,out_channel) ,
+    while those in spcon2.x is in (out_channel,D,H,W,in_channel).
     """
     for hook in self._load_state_dict_pre_hooks.values():
         hook(state_dict, prefix, local_metadata, strict, missing_keys,
