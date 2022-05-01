bb25d249f1761668fda52e1e73cc2a4b178e9e87
ChaimZhu <zhuchenming@pjlab.org.cn>
2022-05-01 16:54:34 +0800

    [Fix] Fix registry potential bug (#1443)
    * fix registry bug
    
    * fix ci problems
    
    * add

--- mmdet3d/utils/__init__.py
+++ mmdet3d/utils/__init__.py
@@ -4,9 +4,11 @@ from mmcv.utils import Registry, build_from_cfg, print_log
 from .collect_env import collect_env
 from .compat_cfg import compat_cfg
 from .logger import get_root_logger
+from .misc import find_latest_checkpoint
 from .setup_env import setup_multi_processes
 
 __all__ = [
     'Registry', 'build_from_cfg', 'get_root_logger', 'collect_env',
-    'print_log', 'setup_multi_processes', 'compat_cfg'
+    'print_log', 'setup_multi_processes', 'find_latest_checkpoint',
+    'compat_cfg'
 ]
