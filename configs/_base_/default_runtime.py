3fa5a430d73f33ff2f4aab07d77b72637134896b
ZCMax <zcm952742165@gmail.com>
2022-05-25 08:46:46 +0000

    Update default runtime in config

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,23 +1,22 @@
-checkpoint_config = dict(interval=1)
-# yapf:disable push
-# By default we use textlogger hook and tensorboard
-# For more loggers see
-# https://mmcv.readthedocs.io/en/latest/api.html#mmcv.runner.LoggerHook
-log_config = dict(
-    interval=50,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        dict(type='TensorboardLoggerHook')
-    ])
-# yapf:enable
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
-work_dir = None
-load_from = None
-resume_from = None
-workflow = [('train', 1)]
+default_scope = 'mmdet3d'
 
-# disable opencv multithreading to avoid system being overloaded
-opencv_num_threads = 0
-# set multi-process start method as `fork` to speed up the training
-mp_start_method = 'fork'
+default_hooks = dict(
+    optimizer=dict(type='OptimizerHook', grad_clip=None),
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', interval=1),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+)
+
+env_cfg = dict(
+    cudnn_benchmark=False,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'),
+)
+
+log_level = 'INFO'
+load_from = None
+resume = False
+
+# TODO: support auto scaling lr
