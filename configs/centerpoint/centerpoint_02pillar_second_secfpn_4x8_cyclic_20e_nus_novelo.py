458216c95fe84ca73b615b78150f9e73ecd0d591
JoeyforJoy <654780480@qq.com>
2022-06-21 17:07:23 +0800

    update the config of the no velocity version centerpoint

--- configs/centerpoint/centerpoint_02pillar_second_secfpn_4x8_cyclic_20e_nus_novelo.py
+++ configs/centerpoint/centerpoint_02pillar_second_secfpn_4x8_cyclic_20e_nus_novelo.py
@@ -31,41 +31,42 @@ dataset_type = 'NuScenesDataset'
 data_root = 'data/nuscenes/'
 file_client_args = dict(backend='disk')
 
-# db_sampler = dict(
-#     data_root=data_root,
-#     info_path=data_root + 'nuscenes_dbinfos_train.pkl',
-#     rate=1.0,
-#     prepare=dict(
-#         filter_by_difficulty=[-1],
-#         filter_by_min_points=dict(
-#             car=5,
-#             truck=5,
-#             bus=5,
-#             trailer=5,
-#             construction_vehicle=5,
-#             traffic_cone=5,
-#             barrier=5,
-#             motorcycle=5,
-#             bicycle=5,
-#             pedestrian=5)),
-#     classes=class_names,
-#     sample_groups=dict(
-#         car=2,
-#         truck=3,
-#         construction_vehicle=7,
-#         bus=4,
-#         trailer=6,
-#         barrier=2,
-#         motorcycle=6,
-#         bicycle=6,
-#         pedestrian=2,
-#         traffic_cone=2),
-#     points_loader=dict(
-#         type='LoadPointsFromFile',
-#         coord_type='LIDAR',
-#         load_dim=5,
-#         use_dim=[0, 1, 2, 3, 4],
-#         file_client_args=file_client_args))
+db_sampler = dict(
+    data_root=data_root,
+    info_path=data_root + 'nuscenes_dbinfos_train.pkl',
+    rate=1.0,
+    bbox_code_size=7,
+    prepare=dict(
+        filter_by_difficulty=[-1],
+        filter_by_min_points=dict(
+            car=5,
+            truck=5,
+            bus=5,
+            trailer=5,
+            construction_vehicle=5,
+            traffic_cone=5,
+            barrier=5,
+            motorcycle=5,
+            bicycle=5,
+            pedestrian=5)),
+    classes=class_names,
+    sample_groups=dict(
+        car=2,
+        truck=3,
+        construction_vehicle=7,
+        bus=4,
+        trailer=6,
+        barrier=2,
+        motorcycle=6,
+        bicycle=6,
+        pedestrian=2,
+        traffic_cone=2),
+    points_loader=dict(
+        type='LoadPointsFromFile',
+        coord_type='LIDAR',
+        load_dim=5,
+        use_dim=[0, 1, 2, 3, 4],
+        file_client_args=file_client_args))
 
 train_pipeline = [
     dict(
@@ -82,7 +83,7 @@ train_pipeline = [
         pad_empty_sweeps=True,
         remove_close=True),
     dict(type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
-    # dict(type='ObjectSample', db_sampler=db_sampler),
+    dict(type='ObjectSample', db_sampler=db_sampler),
     dict(
         type='GlobalRotScaleTrans',
         rot_range=[-0.3925, 0.3925],
