4350683ebd353d1f85cd0e71f3bf8e6680c1e392
Beomjun Park <bjpark0805@rideflux.com>
2022-07-04 14:25:58 +0900

    Evaluation File Edited

--- mmdet3d/core/evaluation/indoor_eval.py
+++ mmdet3d/core/evaluation/indoor_eval.py
@@ -263,7 +263,7 @@ def indoor_eval(gt_annos,
 
 
         for i in range(len(labels_3d)):
-            # label = 0 if labels_3d[i]=='Car' else 1
+            label = 0 if labels_3d[i]=='Car' else 1
             bbox = gt_boxes[i]
             if label not in gt:
                 gt[label] = {}
@@ -272,7 +272,6 @@ def indoor_eval(gt_annos,
             gt[label][img_id].append(bbox)
 
 
-    print(gt)
     rec, prec, ap = eval_map_recall(pred, gt, metric)
     ret_dict = dict()
     header = ['classes']
