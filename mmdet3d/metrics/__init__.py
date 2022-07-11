f9ebc59b1e77c462063c1edf6f7781484d43ddfd
VVsssssk <shenkun@pjlab.org.cn>
2022-07-11 03:07:42 +0000

    [Refactor]add lyft metric

--- mmdet3d/metrics/__init__.py
+++ mmdet3d/metrics/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .indoor_metric import IndoorMetric  # noqa: F401,F403
 from .kitti_metric import KittiMetric  # noqa: F401,F403
+from .lyft_metric import LyftMetric  # noqa: F401,F403
 from .nuscenes_metric import NuScenesMetric  # noqa: F401,F403
 
-__all_ = ['KittiMetric', 'NuScenesMetric', 'IndoorMetric']
+__all_ = ['KittiMetric', 'NuScenesMetric', 'IndoorMetric', 'LyftMetric']
