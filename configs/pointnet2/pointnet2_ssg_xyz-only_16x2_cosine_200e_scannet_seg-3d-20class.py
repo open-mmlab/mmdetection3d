55f69336cc6a17605ad6b44fc4cd57d82e6cb538
jshilong <2392587229zsl@gmail.com>
2022-08-02 22:00:20 +0800

    resolve commnets

--- configs/pointnet2/pointnet2_ssg_xyz-only_16x2_cosine_200e_scannet_seg-3d-20class.py
+++ configs/pointnet2/pointnet2_ssg_xyz-only_16x2_cosine_200e_scannet_seg-3d-20class.py
@@ -28,7 +28,7 @@ train_pipeline = [
         with_label_3d=False,
         with_mask_3d=False,
         with_seg_3d=True),
-    dict(type='PointSegClassMapping', ),
+    dict(type='PointSegClassMapping'),
     dict(
         type='IndoorPatchPointSample',
         num_points=num_points,
@@ -73,31 +73,6 @@ test_pipeline = [
             dict(type='Collect3D', keys=['points'])
         ])
 ]
-# construct a pipeline for data and gt loading in show function
-# please keep its loading function consistent with test_pipeline (e.g. client)
-# we need to load gt seg_mask!
-eval_pipeline = [
-    dict(
-        type='LoadPointsFromFile',
-        coord_type='DEPTH',
-        shift_height=False,
-        use_color=False,
-        load_dim=6,
-        use_dim=[0, 1, 2]),
-    dict(
-        type='LoadAnnotations3D',
-        with_bbox_3d=False,
-        with_label_3d=False,
-        with_mask_3d=False,
-        with_seg_3d=True),
-    dict(type='PointSegClassMapping'),
-    dict(
-        type='DefaultFormatBundle3D',
-        with_label=False,
-        class_names=class_names),
-    dict(type='Collect3D', keys=['points', 'pts_semantic_mask'])
-]
-
 data = dict(
     samples_per_gpu=16,
     workers_per_gpu=4,
@@ -127,7 +102,7 @@ data = dict(
         test_mode=True,
         ignore_index=len(class_names)))
 
-evaluation = dict(pipeline=eval_pipeline, interval=5)
+evaluation = dict(interval=5)
 
 # model settings
 model = dict(
