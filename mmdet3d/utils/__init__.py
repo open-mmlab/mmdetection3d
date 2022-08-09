5b9eacdf1bd093243cbe4d473825dcdd8682ffff
VVsssssk <88368822+VVsssssk@users.noreply.github.com>
2022-08-09 10:13:58 +0800

    rm find_latest_checkpoint and get_root_logger (#1707)

--- mmdet3d/utils/__init__.py
+++ mmdet3d/utils/__init__.py
@@ -2,16 +2,14 @@
 from .array_converter import ArrayConverter, array_converter
 from .collect_env import collect_env
 from .compat_cfg import compat_cfg
-from .logger import get_root_logger
 from .misc import replace_ceph_backend
 from .setup_env import register_all_modules, setup_multi_processes
 from .typing import (ConfigType, InstanceList, MultiConfig, OptConfigType,
                      OptInstanceList, OptMultiConfig, OptSamplingResultList)
 
 __all__ = [
-    'get_root_logger', 'collect_env', 'setup_multi_processes', 'compat_cfg',
-    'register_all_modules', 'find_latest_checkpoint', 'array_converter',
-    'ArrayConverter', 'ConfigType', 'OptConfigType', 'MultiConfig',
-    'OptMultiConfig', 'InstanceList', 'OptInstanceList',
-    'OptSamplingResultList', 'replace_ceph_backend'
+    'collect_env', 'setup_multi_processes', 'compat_cfg',
+    'register_all_modules', 'array_converter', 'ArrayConverter', 'ConfigType',
+    'OptConfigType', 'MultiConfig', 'OptMultiConfig', 'InstanceList',
+    'OptInstanceList', 'OptSamplingResultList', 'replace_ceph_backend'
 ]
