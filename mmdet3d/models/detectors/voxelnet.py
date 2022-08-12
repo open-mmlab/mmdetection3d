5b23dfa7eb43a6986e56d2a5c0da51e98fb5cc59
ChaimZhu <zhuchenming@pjlab.org.cn>
2022-08-12 17:18:08 +0800

    update full

--- mmdet3d/models/detectors/voxelnet.py
+++ mmdet3d/models/detectors/voxelnet.py
@@ -1,7 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from typing import Tuple
 
-from mmcv.ops import Voxelization
 from torch import Tensor
 
 from mmdet3d.registry import MODELS
@@ -14,7 +13,6 @@ class VoxelNet(SingleStage3DDetector):
     r"""`VoxelNet <https://arxiv.org/abs/1711.06396>`_ for 3D detection."""
 
     def __init__(self,
-                 voxel_layer: ConfigType,
                  voxel_encoder: ConfigType,
                  middle_encoder: ConfigType,
                  backbone: ConfigType,
@@ -32,19 +30,18 @@ class VoxelNet(SingleStage3DDetector):
             test_cfg=test_cfg,
             data_preprocessor=data_preprocessor,
             init_cfg=init_cfg)
-        self.voxel_layer = Voxelization(**voxel_layer)
         self.voxel_encoder = MODELS.build(voxel_encoder)
         self.middle_encoder = MODELS.build(middle_encoder)
 
     def extract_feat(self, batch_inputs_dict: dict) -> Tuple[Tensor]:
         """Extract features from points."""
-        # TODO: Remove voxelization to datapreprocessor
-        feats = batch_inputs_dict['voxels']['voxels']
-        coors = batch_inputs_dict['voxels']['coors']
-        voxel_num_points = batch_inputs_dict['voxels']['num_points']
-        voxel_features = self.voxel_encoder(feats, voxel_num_points, coors)
-        batch_size = coors[-1, 0].item() + 1
-        x = self.middle_encoder(voxel_features, coors, batch_size)
+        voxel_dict = batch_inputs_dict['voxels']
+        voxel_features = self.voxel_encoder(voxel_dict['voxels'],
+                                            voxel_dict['num_points'],
+                                            voxel_dict['coors'])
+        batch_size = voxel_dict['coors'][-1, 0].item() + 1
+        x = self.middle_encoder(voxel_features, voxel_dict['coors'],
+                                batch_size)
         x = self.backbone(x)
         if self.with_neck:
             x = self.neck(x)
