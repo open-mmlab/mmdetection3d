c8ec508829abd42f18112300c8707793df0b8220
VVsssssk <shenkun@pjlab.org.cn>
2022-09-05 11:27:10 +0800

    add auto_scale_lr in cfg

--- configs/_base_/schedules/cyclic-20e.py
+++ configs/_base_/schedules/cyclic-20e.py
@@ -57,3 +57,9 @@ param_scheduler = [
 train_cfg = dict(by_epoch=True, max_epochs=20, val_interval=20)
 val_cfg = dict()
 test_cfg = dict()
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (4 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=32)
