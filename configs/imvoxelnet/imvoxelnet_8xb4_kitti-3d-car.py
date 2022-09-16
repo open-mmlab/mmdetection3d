ababe32e679199c2e42d89456147f05b73a2ab58
JingweiZhang12 <zjw18@mails.tsinghua.edu.cn>
2022-09-16 09:47:27 +0800

    refactor imvoxelnet config

--- configs/imvoxelnet/imvoxelnet_8xb4_kitti-3d-car.py
+++ configs/imvoxelnet/imvoxelnet_8xb4_kitti-3d-car.py
@@ -1,3 +1,7 @@
+_base_ = [
+    '../_base_/schedules/mmdet-schedule-1x.py', '../_base_/default_runtime.py'
+]
+
 model = dict(
     type='ImVoxelNet',
     data_preprocessor=dict(
@@ -151,7 +155,8 @@ test_evaluator = val_evaluator
 # optimizer
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='AdamW', lr=0.0001, weight_decay=0.0001),
+    optimizer=dict(
+        _delete_=True, type='AdamW', lr=0.0001, weight_decay=0.0001),
     paramwise_cfg=dict(
         custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0)}),
     clip_grad=dict(max_norm=35., norm_type=2))
@@ -166,30 +171,7 @@ param_scheduler = [
 ]
 
 # hooks
-default_hooks = dict(
-    timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=1),
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-)
-
-# training schedule for 2x
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=12, val_interval=1)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
+default_hooks = dict(checkpoint=dict(type='CheckpointHook', max_keep_ckpts=1))
 
 # runtime
-default_scope = 'mmdet3d'
-
-env_cfg = dict(
-    cudnn_benchmark=False,
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
-    dist_cfg=dict(backend='nccl'),
-)
-
-log_level = 'INFO'
-load_from = None
-resume = False
-dist_params = dict(backend='nccl')
 find_unused_parameters = True  # only 1 of 4 FPN outputs is used
