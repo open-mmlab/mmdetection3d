d25c2ebb0f8ad2711228d4679ef0be579869e46a
tpoisonooo <khj.application@aliyun.com>
2022-11-08 10:21:27 +0800

    [Enhance(inference)]: add default value for output dir in show function (#1993)

--- mmdet3d/apis/inference.py
+++ mmdet3d/apis/inference.py
@@ -483,7 +483,7 @@ def show_proj_det_result_meshlab(data,
 
 def show_result_meshlab(data,
                         result,
-                        out_dir,
+                        out_dir='mmdet3d-output',
                         score_thr=0.0,
                         show=False,
                         snapshot=False,
@@ -495,6 +495,7 @@ def show_result_meshlab(data,
         data (dict): Contain data from pipeline.
         result (dict): Predicted result from model.
         out_dir (str): Directory to save visualized result.
+            Default: mmdet3d-output
         score_thr (float, optional): Minimum score of bboxes to be shown.
             Default: 0.0
         show (bool, optional): Visualize the results online. Defaults to False.
