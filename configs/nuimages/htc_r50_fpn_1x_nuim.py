116d9f2335914cdc1b156da6d8358031a7e0bf26
Shaun <cq.xiang@foxmail.com>
2022-11-23 11:19:35 +0800

    [FIX] replace DefaultFormatBundle/3D with Pack(3D)DetInputs (#1987)
    * replace defaultformatbundle3d with pack3ddetinputs
    
    * remove normalize, pad, imagetotensor from configs
    
    * rm unused key 'img_norm_cfg'
    
    * fix lint errors
    
    * fix lint errors
    
    * fix lint error
    
    * xx

--- configs/nuimages/htc_r50_fpn_1x_nuim.py
+++ configs/nuimages/htc_r50_fpn_1x_nuim.py
@@ -18,8 +18,6 @@ model = dict(
             loss_weight=0.2)))
 
 data_root = 'data/nuimages/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
@@ -30,13 +28,8 @@ train_pipeline = [
         multiscale_mode='range',
         keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
     dict(type='SegRescale', scale_factor=1 / 8),
-    dict(type='DefaultFormatBundle'),
-    dict(
-        type='Collect',
-        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_semantic_seg'])
+    dict(type='PackDetInputs')
 ]
 data = dict(
     train=dict(
