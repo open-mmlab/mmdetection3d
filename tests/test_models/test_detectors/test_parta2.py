5489875638555dff4e8b7fee47cc3c9512a817ce
Jingwei Zhang <zjw18@mails.tsinghua.edu.cn>
2022-12-02 16:06:48 +0800

    [Docs] Update all the metafiles acoording to README (#2006)
    * update centerpoint, dgcnn, fcos3d and free_anchor metafile
    
    * check all metafile in configs
    
    * update centerpoint, pointpillars and second yml
    
    * rename PartA2 to parta2 in the name of config
    
    * update metafile in nuimages
    
    * update readme in nuimages

--- tests/test_models/test_detectors/test_parta2.py
+++ tests/test_models/test_detectors/test_parta2.py
@@ -17,7 +17,7 @@ class TestPartA2(unittest.TestCase):
         DefaultScope.get_instance('test_parta2', scope_name='mmdet3d')
         setup_seed(0)
         parta2_cfg = get_detector_cfg(
-            'parta2/PartA2_hv_secfpn_8xb2-cyclic-80e_kitti-3d-3class.py')
+            'parta2/parta2_hv_secfpn_8xb2-cyclic-80e_kitti-3d-3class.py')
         model = MODELS.build(parta2_cfg)
         num_gt_instance = 2
         packed_inputs = create_detector_inputs(num_gt_instance=num_gt_instance)
