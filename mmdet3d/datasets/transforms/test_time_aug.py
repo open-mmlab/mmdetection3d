7d367a8619de92d0c83f727a2965e0a32025c41c
SekiroRong <sekirorong@gmail.com>
2022-12-23 00:42:01 +0800

    polish-code

--- mmdet3d/datasets/transforms/test_time_aug.py
+++ mmdet3d/datasets/transforms/test_time_aug.py
@@ -108,6 +108,7 @@ class MultiScaleFlipAug3D(BaseTransform):
                                     pcd_vertical_flip
                                 data = self.transforms(_results)
                                 aug_data_list.append(data)
+
         return aug_data_list
 
     def __repr__(self) -> str:
