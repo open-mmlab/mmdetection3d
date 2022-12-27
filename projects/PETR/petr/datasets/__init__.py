2ff66e939f1a9ba099cbac7b246e5c430f8fa263
SekiroRong <sekirorong@gmail.com>
2022-12-27 21:05:25 +0800

    fix_bug

--- projects/PETR/petr/datasets/__init__.py
+++ projects/PETR/petr/datasets/__init__.py
@@ -1,9 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .transforms.transforms_3d import (AddCamInfo, GlobalRotScaleTransImage,
                                        LidarBox3dVersionTransfrom,
+                                       NormalizeMultiviewImage,
                                        PadMultiViewImage, ResizeCropFlipImage)
 
 __all__ = [
-    'GlobalRotScaleTransImage', 'LidarBox3dVersionTransfrom', 'PadMultiViewImage', 
-    'ResizeCropFlipImage', 'AddCamInfo'
+    'GlobalRotScaleTransImage', 'LidarBox3dVersionTransfrom',
+    'NormalizeMultiviewImage', 'PadMultiViewImage', 'ResizeCropFlipImage',
+    'AddCamInfo'
 ]
