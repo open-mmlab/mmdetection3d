eaa817e908f290089fae0a668be42e18a2d064b1
Pzzzzz <hello-cd.plus@hotmail.com>
2022-12-29 16:30:54 +0800

    use mmeval.InstanceSeg

--- mmdet3d/evaluation/metrics/instance_seg_metric.py
+++ mmdet3d/evaluation/metrics/instance_seg_metric.py
@@ -1,33 +1,40 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from typing import Dict, Optional, Sequence
 
-from mmengine.evaluator import BaseMetric
-from mmengine.logging import MMLogger
-
-from mmdet3d.evaluation import instance_seg_eval
+import warnings
+from mmeval.metrics import InstanceSeg
 from mmdet3d.registry import METRICS
 
 
 @METRICS.register_module()
-class InstanceSegMetric(BaseMetric):
+class InstanceSegMetric(InstanceSeg):
     """3D instance segmentation evaluation metric.
 
     Args:
-        collect_device (str, optional): Device name used for collecting
-            results from different ranks during distributed training.
-            Must be 'cpu' or 'gpu'. Defaults to 'cpu'.
-        prefix (str): The prefix that will be added in the metric
-            names to disambiguate homonymous metrics of different evaluators.
-            If prefix is not provided in the argument, self.default_prefix
-            will be used instead. Default: None
+        dataset_meta (dict): Provide dataset meta information.
     """
 
-    def __init__(self,
-                 collect_device: str = 'cpu',
-                 prefix: Optional[str] = None,
-                 **kwargs):
+    def __init__(
+        self,
+        dataset_meta: dict,
+        dist_backend: Optional[str] = None,
+        collect_device: Optional[str] = None,
+        prefix: Optional[str] = None,
+        **kwargs
+    ):
+        if collect_device is not None:
+            warnings.warn(
+                "DeprecationWarning: The `collect_device` parameter of "
+                "`InstanceSegMetric` is deprecated, use `dist_backend instead."
+            )
+        if prefix is not None:
+            warnings.warn(
+                "DeprecationWarning: The `prefix` parameter of "
+                "`InstanceSegMetric` is deprecated."
+            )
         super(InstanceSegMetric, self).__init__(
-            prefix=prefix, collect_device=collect_device)
+            dataset_meta=dataset_meta, dist_backend=dist_backend, **kwargs
+        )
 
     def process(self, data_batch: dict, data_samples: Sequence[dict]) -> None:
         """Process one batch of data samples and predictions.
@@ -41,54 +48,29 @@ class InstanceSegMetric(BaseMetric):
             data_samples (Sequence[dict]): A batch of outputs from
                 the model.
         """
+        pred = []
+        gt = []
         for data_sample in data_samples:
-            pred_3d = data_sample['pred_pts_seg']
-            eval_ann_info = data_sample['eval_ann_info']
-            cpu_pred_3d = dict()
+            pred_3d = data_sample["pred_pts_seg"]
+            eval_ann_info = data_sample["eval_ann_info"]
+            cpu_pred_3d = {k: v.clone().cpu().numpy() for k, v in pred_3d.items()}
+            cpu_eval_ann_info = {
+                k: v.clone().cpu().numpy() for k, v in eval_ann_info.items()
+            }
             for k, v in pred_3d.items():
-                if hasattr(v, 'to'):
-                    cpu_pred_3d[k] = v.to('cpu')
+                if hasattr(v, "to"):
+                    cpu_pred_3d[k] = v.to("cpu").clone().numpy()
                 else:
-                    cpu_pred_3d[k] = v
-            self.results.append((eval_ann_info, cpu_pred_3d))
+                    cpu_pred_3d[k] = v.clone().numpy()
+            pred.append(cpu_pred_3d)
+            gt.append(cpu_eval_ann_info)
+        self.add(pred, gt)
 
-    def compute_metrics(self, results: list) -> Dict[str, float]:
-        """Compute the metrics from processed results.
-
-        Args:
-            results (list): The processed results of each batch.
-
-        Returns:
-            Dict[str, float]: The computed metrics. The keys are the names of
-            the metrics, and the values are corresponding results.
+    def evaluate(self, *args, **kwargs):
+        """Evaluate the model performance of the whole dataset after processing
+        all batches.
         """
-        logger: MMLogger = MMLogger.get_current_instance()
+        res = self.compute(*args, **kwargs)
+        self.reset()
 
-        self.classes = self.dataset_meta['classes']
-        self.valid_class_ids = self.dataset_meta['seg_valid_class_ids']
-
-        gt_semantic_masks = []
-        gt_instance_masks = []
-        pred_instance_masks = []
-        pred_instance_labels = []
-        pred_instance_scores = []
-
-        for eval_ann, sinlge_pred_results in results:
-            gt_semantic_masks.append(eval_ann['pts_semantic_mask'])
-            gt_instance_masks.append(eval_ann['pts_instance_mask'])
-            pred_instance_masks.append(
-                sinlge_pred_results['pts_instance_mask'])
-            pred_instance_labels.append(sinlge_pred_results['instance_labels'])
-            pred_instance_scores.append(sinlge_pred_results['instance_scores'])
-
-        ret_dict = instance_seg_eval(
-            gt_semantic_masks,
-            gt_instance_masks,
-            pred_instance_masks,
-            pred_instance_labels,
-            pred_instance_scores,
-            valid_class_ids=self.valid_class_ids,
-            class_labels=self.classes,
-            logger=logger)
-
-        return ret_dict
+        return res
