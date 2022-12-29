690059f6ed3959e89bc22f88141495c03f425bb4
Pzzzzz <hello-cd.plus@hotmail.com>
2022-12-29 16:38:49 +0800

    reformat using yapf

--- mmdet3d/evaluation/functional/__init__.py
+++ mmdet3d/evaluation/functional/__init__.py
@@ -1,34 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .indoor_eval import average_precision, eval_det_cls, eval_map_recall, indoor_eval
+from .indoor_eval import (average_precision, eval_det_cls, eval_map_recall,
+                          indoor_eval)
 from .kitti_utils import do_eval, kitti_eval, kitti_eval_coco_style
-from .lyft_eval import (
-    get_classwise_aps,
-    get_single_class_aps,
-    load_lyft_gts,
-    load_lyft_predictions,
-    lyft_eval,
-)
+from .lyft_eval import (get_classwise_aps, get_single_class_aps, load_lyft_gts,
+                        load_lyft_predictions, lyft_eval)
 from .scannet_utils import evaluate_matches, scannet_eval
 from .seg_eval import fast_hist, get_acc, get_acc_cls, per_class_iou, seg_eval
 
 __all__ = [
-    "average_precision",
-    "eval_det_cls",
-    "eval_map_recall",
-    "indoor_eval",
-    "load_lyft_gts",
-    "load_lyft_predictions",
-    "lyft_eval",
-    "get_classwise_aps",
-    "get_single_class_aps",
-    "fast_hist",
-    "per_class_iou",
-    "get_acc",
-    "get_acc_cls",
-    "seg_eval",
-    "kitti_eval",
-    "kitti_eval_coco_style",
-    "scannet_eval",
-    "evaluate_matches",
-    "do_eval",
+    'average_precision', 'eval_det_cls', 'eval_map_recall', 'indoor_eval',
+    'load_lyft_gts', 'load_lyft_predictions', 'lyft_eval', 'get_classwise_aps',
+    'get_single_class_aps', 'fast_hist', 'per_class_iou', 'get_acc',
+    'get_acc_cls', 'seg_eval', 'kitti_eval', 'kitti_eval_coco_style',
+    'scannet_eval', 'evaluate_matches', 'do_eval'
 ]
