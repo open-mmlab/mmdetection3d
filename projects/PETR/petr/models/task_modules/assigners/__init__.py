f0af246884d370874a3abed24305f06eda2d5948
SekiroRong <sekirorong@gmail.com>
2022-12-29 14:03:21 +0800

    remove-redundent__init__

--- projects/PETR/petr/models/task_modules/assigners/__init__.py
+++ projects/PETR/petr/models/task_modules/assigners/__init__.py
@@ -1,9 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmdet.models.task_modules import (AssignResult, BaseAssigner,
-                                       MaxIoUAssigner)
-
 from .hungarian_assigner_3d import HungarianAssigner3D
 
-__all__ = [
-    'BaseAssigner', 'MaxIoUAssigner', 'AssignResult', 'HungarianAssigner3D'
-]
+__all__ = ['HungarianAssigner3D']
