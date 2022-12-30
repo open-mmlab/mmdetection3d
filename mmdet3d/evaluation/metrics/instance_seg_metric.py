543e1ea60232153f01b67a1a37e369bf89e01b18
Pzzzzz <hello-cd.plus@hotmail.com>
2022-12-30 17:04:18 +0800

    add result print, add pre-commit

--- mmdet3d/evaluation/metrics/instance_seg_metric.py
+++ mmdet3d/evaluation/metrics/instance_seg_metric.py
@@ -1,8 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Dict, Optional, Sequence
-
 import warnings
+from typing import Optional, Sequence
+
+from mmengine.logging import MMLogger, print_log
 from mmeval.metrics import InstanceSeg
+from terminaltables import AsciiTable
+
 from mmdet3d.registry import METRICS
 
 
@@ -22,12 +25,12 @@ class InstanceSegMetric(InstanceSeg):
                  **kwargs):
         if collect_device is not None:
             warnings.warn(
-                "DeprecationWarning: The `collect_device` parameter of "
-                "`InstanceSegMetric` is deprecated, use `dist_backend instead."
+                'DeprecationWarning: The `collect_device` parameter of '
+                '`InstanceSegMetric` is deprecated, use `dist_backend instead.'
             )
         if prefix is not None:
-            warnings.warn("DeprecationWarning: The `prefix` parameter of "
-                          "`InstanceSegMetric` is deprecated.")
+            warnings.warn('DeprecationWarning: The `prefix` parameter of '
+                          '`InstanceSegMetric` is deprecated.')
         super(InstanceSegMetric, self).__init__(
             dataset_meta=dataset_meta, dist_backend=dist_backend, **kwargs)
 
@@ -46,8 +49,8 @@ class InstanceSegMetric(InstanceSeg):
         pred = []
         gt = []
         for data_sample in data_samples:
-            pred_3d = data_sample["pred_pts_seg"]
-            eval_ann_info = data_sample["eval_ann_info"]
+            pred_3d = data_sample['pred_pts_seg']
+            eval_ann_info = data_sample['eval_ann_info']
             cpu_pred_3d = {
                 k: v.clone().cpu().numpy()
                 for k, v in pred_3d.items()
@@ -57,8 +60,8 @@ class InstanceSegMetric(InstanceSeg):
                 for k, v in eval_ann_info.items()
             }
             for k, v in pred_3d.items():
-                if hasattr(v, "to"):
-                    cpu_pred_3d[k] = v.to("cpu").clone().numpy()
+                if hasattr(v, 'to'):
+                    cpu_pred_3d[k] = v.to('cpu').clone().numpy()
                 else:
                     cpu_pred_3d[k] = v.clone().numpy()
             pred.append(cpu_pred_3d)
@@ -67,9 +70,21 @@ class InstanceSegMetric(InstanceSeg):
 
     def evaluate(self, *args, **kwargs):
         """Evaluate the model performance of the whole dataset after processing
-        all batches.
-        """
-        res = self.compute(*args, **kwargs)
+        all batches."""
+        logger: MMLogger = MMLogger.get_current_instance()
+
+        metrics = self.compute(*args, **kwargs)
+        header = ['classes', 'AP_0.25', 'AP_0.50', 'AP']
+        rows = []
+        for label, data in metrics['classes'].items():
+            aps = [data['ap25%'], data['ap50%'], data['ap']]
+            rows.append([label] + [f'{ap:.4f}' for ap in aps])
+        aps = metrics['all_ap_25%'], metrics['all_ap_50%'], metrics['all_ap']
+        footer = ['Overall'] + [f'{ap:.4f}' for ap in aps]
+        table = AsciiTable([header] + rows + [footer])
+        table.inner_footing_row_border = True
+        print_log('\n' + table.table, logger=logger)
+
         self.reset()
 
-        return res
+        return metrics
