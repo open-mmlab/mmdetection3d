6afd2747f11563c83ea325727478fcc83856fa46
xzq <zeqixiao1@gmail.com>
2023-01-18 22:27:24 +0800

    update

--- mmdet3d/datasets/transforms/loading.py
+++ mmdet3d/datasets/transforms/loading.py
@@ -687,7 +687,7 @@ class LoadAnnotations3D(LoadAnnotations):
     - pts_semantic_mask_path (str): Path of semantic mask file.
       Only when `with_seg_3d` is True.
     - pts_panoptic_mask_path (str): Path of panoptic mask file.
-      Only when `with_panoptic_seg_3d` is True.
+      Only when both `with_mask_3d` and `with_seg_3d` is True.
 
     Added Keys:
 
@@ -710,8 +710,6 @@ class LoadAnnotations3D(LoadAnnotations):
       Only when `with_mask_3d` is True.
     - pts_semantic_mask (np.int64): Semantic mask of each point.
       Only when `with_seg_3d` is True.
-    - pts_panoptic_mask (np.int64): Panoptic mask of each point.
-      Only when `with_panoptic_seg_3d` is True.
 
     Args:
         with_bbox_3d (bool): Whether to load 3D boxes. Defaults to True.
@@ -732,6 +730,8 @@ class LoadAnnotations3D(LoadAnnotations):
         poly2mask (bool): Whether to convert polygon annotations to bitmasks.
             Defaults to True.
         seg_3d_dtype (dtype): Dtype of 3D semantic masks. Defaults to int64.
+        seg_offset (int): The offset to split semantic and instance labels from
+            panoptic labels. Defaults to 0.
         file_client_args (dict): Arguments to instantiate a FileClient.
             See :class:`mmengine.fileio.FileClient` for details.
             Defaults to dict(backend='disk').
@@ -744,13 +744,13 @@ class LoadAnnotations3D(LoadAnnotations):
         with_attr_label: bool = False,
         with_mask_3d: bool = False,
         with_seg_3d: bool = False,
-        with_panoptic_seg_3d: bool = False,
         with_bbox: bool = False,
         with_label: bool = False,
         with_mask: bool = False,
         with_seg: bool = False,
         with_bbox_depth: bool = False,
         poly2mask: bool = True,
+        seg_offset: int = 0,
         seg_3d_dtype: np.dtype = np.int64,
         file_client_args: dict = dict(backend='disk')
     ) -> None:
@@ -767,8 +767,8 @@ class LoadAnnotations3D(LoadAnnotations):
         self.with_attr_label = with_attr_label
         self.with_mask_3d = with_mask_3d
         self.with_seg_3d = with_seg_3d
-        self.with_panoptic_seg_3d = with_panoptic_seg_3d
         self.seg_3d_dtype = seg_3d_dtype
+        self.seg_offset = seg_offset
 
     def _load_bboxes_3d(self, results: dict) -> dict:
         """Private function to move the 3D bounding box annotation from
@@ -873,13 +873,16 @@ class LoadAnnotations3D(LoadAnnotations):
             pts_semantic_mask = np.fromfile(
                 pts_semantic_mask_path, dtype=np.int64)
 
+        pts_semantic_mask = pts_semantic_mask.astype(
+            np.int64) // self.seg_offset
         results['pts_semantic_mask'] = pts_semantic_mask
+
         # 'eval_ann_info' will be passed to evaluator
         if 'eval_ann_info' in results:
             results['eval_ann_info']['pts_semantic_mask'] = pts_semantic_mask
         return results
 
-    def _load_panoptic_seg_3d(self, results: dict) -> dict:
+    def _load_panoptic_3d(self, results: dict) -> dict:
         """Private function to load 3D panoptic segmentation annotations.
 
         Args:
@@ -902,10 +905,18 @@ class LoadAnnotations3D(LoadAnnotations):
             pts_panoptic_mask = np.fromfile(
                 pts_panoptic_mask_path, dtype=np.int64)
 
-        results['pts_panoptic_mask'] = pts_panoptic_mask
+        pts_semantic_mask = pts_panoptic_mask.astype(
+            np.int64) % self.seg_offset
+        results['pts_semantic_mask'] = pts_semantic_mask
+
+        # We can directly take panoptic labels as instance ids.
+        pts_instance_mask = pts_panoptic_mask.astype(np.int64)
+        results['pts_instance_mask'] = pts_instance_mask
+
         # 'eval_ann_info' will be passed to evaluator
         if 'eval_ann_info' in results:
-            results['eval_ann_info']['pts_panoptic_mask'] = pts_panoptic_mask
+            results['eval_ann_info']['pts_semantic_mask'] = pts_semantic_mask
+            results['eval_ann_info']['pts_instance_mask'] = pts_instance_mask
         return results
 
     def _load_bboxes(self, results: dict) -> None:
@@ -953,13 +964,12 @@ class LoadAnnotations3D(LoadAnnotations):
             results = self._load_labels_3d(results)
         if self.with_attr_label:
             results = self._load_attr_labels(results)
-        if self.with_mask_3d:
+        if self.with_seg_3d and self.with_mask_3d:
+            results = self._load_panoptic_3d(results)
+        elif self.with_mask_3d:
             results = self._load_masks_3d(results)
-        if self.with_seg_3d:
+        elif self.with_seg_3d:
             results = self._load_semantic_seg_3d(results)
-        if self.with_panoptic_seg_3d:
-            results = self._load_panoptic_seg_3d(results)
-
         return results
 
     def __repr__(self) -> str:
@@ -977,7 +987,6 @@ class LoadAnnotations3D(LoadAnnotations):
         repr_str += f'{indent_str}with_seg={self.with_seg}, '
         repr_str += f'{indent_str}with_bbox_depth={self.with_bbox_depth}, '
         repr_str += f'{indent_str}poly2mask={self.poly2mask})'
-        repr_str += f'{indent_str}with_panoptic_seg_3d=\
-                        {self.with_panoptic_seg_3d}, '
+        repr_str += f'{indent_str}seg_offset={self.seg_offset})'
 
         return repr_str
