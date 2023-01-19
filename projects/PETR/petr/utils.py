12c3b19d8c788111bf66a375400e768c2790b3a1
SekiroRong <64967662+SekiroRong@users.noreply.github.com>
2023-01-19 17:54:15 +0800

    [fix] fix bug in projects/PETR (#2212)

--- projects/PETR/petr/utils.py
+++ projects/PETR/petr/utils.py
@@ -2,7 +2,6 @@
 import numpy as np
 import torch
 
-import mmdet3d
 from mmdet3d.structures.bbox_3d.utils import limit_period
 
 
@@ -11,19 +10,21 @@ def normalize_bbox(bboxes, pc_range):
     cx = bboxes[..., 0:1]
     cy = bboxes[..., 1:2]
     cz = bboxes[..., 2:3]
-    w = bboxes[..., 3:4].log()
-    length = bboxes[..., 4:5].log()
-    h = bboxes[..., 5:6].log()
+    length = bboxes[..., 3:4].log()
+    width = bboxes[..., 4:5].log()
+    height = bboxes[..., 5:6].log()
 
-    rot = bboxes[..., 6:7]
+    rot = -bboxes[..., 6:7] - np.pi / 2
+    rot = limit_period(rot, period=np.pi * 2)
     if bboxes.size(-1) > 7:
         vx = bboxes[..., 7:8]
         vy = bboxes[..., 8:9]
         normalized_bboxes = torch.cat(
-            (cx, cy, w, length, cz, h, rot.sin(), rot.cos(), vx, vy), dim=-1)
+            (cx, cy, length, width, cz, height, rot.sin(), rot.cos(), vx, vy),
+            dim=-1)
     else:
         normalized_bboxes = torch.cat(
-            (cx, cy, w, length, cz, h, rot.sin(), rot.cos()), dim=-1)
+            (cx, cy, length, width, cz, height, rot.sin(), rot.cos()), dim=-1)
     return normalized_bboxes
 
 
@@ -33,6 +34,8 @@ def denormalize_bbox(normalized_bboxes, pc_range):
 
     rot_cosine = normalized_bboxes[..., 7:8]
     rot = torch.atan2(rot_sine, rot_cosine)
+    rot = -rot - np.pi / 2
+    rot = limit_period(rot, period=np.pi * 2)
 
     # center in the bev
     cx = normalized_bboxes[..., 0:1]
@@ -40,30 +43,21 @@ def denormalize_bbox(normalized_bboxes, pc_range):
     cz = normalized_bboxes[..., 4:5]
 
     # size
-    w = normalized_bboxes[..., 2:3]
-    length = normalized_bboxes[..., 3:4]
-    h = normalized_bboxes[..., 5:6]
+    length = normalized_bboxes[..., 2:3]
+    width = normalized_bboxes[..., 3:4]
+    height = normalized_bboxes[..., 5:6]
 
-    w = w.exp()
+    width = width.exp()
     length = length.exp()
-    h = h.exp()
+    height = height.exp()
     if normalized_bboxes.size(-1) > 8:
         # velocity
         vx = normalized_bboxes[:, 8:9]
         vy = normalized_bboxes[:, 9:10]
         denormalized_bboxes = torch.cat(
-            [cx, cy, cz, w, length, h, rot, vx, vy], dim=-1)
+            [cx, cy, cz, length, width, height, rot, vx, vy], dim=-1)
     else:
-        denormalized_bboxes = torch.cat([cx, cy, cz, w, length, h, rot],
-                                        dim=-1)
+        denormalized_bboxes = torch.cat(
+            [cx, cy, cz, length, width, height, rot], dim=-1)
 
-    if int(mmdet3d.__version__[0]) >= 1:
-        denormalized_bboxes_clone = denormalized_bboxes.clone()
-        denormalized_bboxes[:, 3] = denormalized_bboxes_clone[:, 4]
-        denormalized_bboxes[:, 4] = denormalized_bboxes_clone[:, 3]
-        # change yaw
-        denormalized_bboxes[:,
-                            6] = -denormalized_bboxes_clone[:, 6] - np.pi / 2
-        denormalized_bboxes[:, 6] = limit_period(
-            denormalized_bboxes[:, 6], period=np.pi * 2)
     return denormalized_bboxes
