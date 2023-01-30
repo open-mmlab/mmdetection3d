2c97d9ae6d5d65f300bce4500c569c38ec3d6ce5
huangqipeng <huangqipeng@cambricon.com>
2023-01-30 17:04:36 +0800

    [Fix]: Fix version check of mmcv (#2243)

--- mmdet3d/__init__.py
+++ mmdet3d/__init__.py
@@ -19,7 +19,7 @@ def digit_version(version_str):
 
 
 mmcv_minimum_version = '1.5.2'
-mmcv_maximum_version = '1.7.0'
+mmcv_maximum_version = '1.8.0'
 mmcv_version = digit_version(mmcv.__version__)
 
 
