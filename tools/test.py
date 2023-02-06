598d58c08e4837994894161cd3a430a44cc255c0
sunyinlei <ginray0215@gmail.com>
2023-02-06 19:33:22 +0800

    [Feature] Support training and testing on Ascend NPU

--- tools/test.py
+++ tools/test.py
@@ -207,6 +207,8 @@ def main():
     cfg.model.train_cfg = None
     model = build_model(cfg.model, test_cfg=cfg.get('test_cfg'))
     fp16_cfg = cfg.get('fp16', None)
+    if fp16_cfg is None and cfg.get('device', None) == 'npu':
+        fp16_cfg = dict(loss_scale='dynamic')
     if fp16_cfg is not None:
         wrap_fp16_model(model)
     checkpoint = load_checkpoint(model, args.checkpoint, map_location='cpu')
