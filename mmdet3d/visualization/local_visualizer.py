647a1429287073575057c1c4932a3a7899184aae
Xiangxu-0103 <xuxiang0103@gmail.com>
2023-02-14 05:26:49 +0000

    update

--- mmdet3d/visualization/local_visualizer.py
+++ mmdet3d/visualization/local_visualizer.py
@@ -735,10 +735,9 @@ class Det3DLocalVisualizer(DetLocalVisualizer):
 
         if draw_gt and data_sample is not None:
             if 'gt_instances_3d' in data_sample:
-                if len(data_sample.gt_instances_3d) > 0:
-                    gt_data_3d = self._draw_instances_3d(
-                        data_input, data_sample.gt_instances_3d,
-                        data_sample.metainfo, vis_task, palette)
+                gt_data_3d = self._draw_instances_3d(
+                    data_input, data_sample.gt_instances_3d,
+                    data_sample.metainfo, vis_task, palette)
             if 'gt_instances' in data_sample:
                 if len(data_sample.gt_instances) > 0:
                     assert 'img' in data_input
@@ -760,15 +759,15 @@ class Det3DLocalVisualizer(DetLocalVisualizer):
 
         if draw_pred and data_sample is not None:
             if 'pred_instances_3d' in data_sample:
-                if len(data_sample.pred_instances_3d) > 0:
-                    pred_instances_3d = data_sample.pred_instances_3d
-                    # .cpu can not be used for BaseInstance3DBoxes
-                    # so we need to use .to('cpu')
-                    pred_instances_3d = pred_instances_3d[
-                        pred_instances_3d.scores_3d > pred_score_thr].to('cpu')
-                    pred_data_3d = self._draw_instances_3d(
-                        data_input, pred_instances_3d, data_sample.metainfo,
-                        vis_task, palette)
+                pred_instances_3d = data_sample.pred_instances_3d
+                # .cpu can not be used for BaseInstance3DBoxes
+                # so we need to use .to('cpu')
+                pred_instances_3d = pred_instances_3d[
+                    pred_instances_3d.scores_3d > pred_score_thr].to('cpu')
+                pred_data_3d = self._draw_instances_3d(data_input,
+                                                       pred_instances_3d,
+                                                       data_sample.metainfo,
+                                                       vis_task, palette)
             if 'pred_instances' in data_sample:
                 if 'img' in data_input and len(data_sample.pred_instances) > 0:
                     pred_instances = data_sample.pred_instances
