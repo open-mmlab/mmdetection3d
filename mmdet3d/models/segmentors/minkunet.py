d5e835e69c2c01a65f6f7fbecf041f7a7939edf4
sunjiahao1999 <578431509@qq.com>
2023-02-14 22:01:33 +0800

    add if tp

--- mmdet3d/models/segmentors/minkunet.py
+++ mmdet3d/models/segmentors/minkunet.py
@@ -1,12 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Tuple
+from typing import List, Tuple
 
 from torch import Tensor
 
+from mmdet3d.models.layers.torchsparse import IS_TORCHSPARSE_AVAILABLE
 from mmdet3d.registry import MODELS
 from mmdet3d.structures.det3d_data_sample import OptSampleList, SampleList
 from mmdet3d.utils import ConfigType
 from .encoder_decoder import EncoderDecoder3D
+
+if IS_TORCHSPARSE_AVAILABLE:
+    from torchsparse import SparseTensor
 
 
 @MODELS.register_module()
@@ -26,41 +30,74 @@ class MinkUNet(EncoderDecoder3D):
         super().__init__(**kwargs)
         self.voxel_encoder = MODELS.build(voxel_encoder)
 
-    def predict(self, batch_inputs_dict: dict, batch_data_samples: SampleList,
-                **kwargs) -> SampleList:
-        """Predict results from a batch of inputs and data samples with post-
-        processing.
+    def predict(self,
+                batch_inputs_dict: dict,
+                batch_data_samples: SampleList,
+                rescale: bool = True) -> SampleList:
+        """Simple test with single scene.
 
         Args:
-            batch_inputs_dict (dict): The model input dict which include
-                'points', 'img' keys.
+            batch_inputs_dict (dict): Input sample dict which
+                includes 'points' and 'imgs' keys.
 
-                    - points (list[torch.Tensor]): Point cloud of each sample.
-                    - imgs (torch.Tensor, optional): Image of each sample.
-
-            batch_data_samples (List[:obj:`Det3DDataSample`]): The Data
-                Samples. It usually includes information such as
-                `gt_instance_3d`, `gt_panoptic_seg_3d` and `gt_sem_seg_3d`.
+                - points (list[torch.Tensor]): Point cloud of each sample.
+                - imgs (torch.Tensor, optional): Image tensor has shape
+                    (B, C, H, W).
+            batch_data_samples (list[:obj:`Det3DDataSample`]): The det3d
+                data samples. It usually includes information such
+                as `metainfo` and `gt_pts_sem_seg`.
+            rescale (bool): Whether transform to original number of points.
+                Will be used for voxelization based segmentors.
+                Defaults to True.
 
         Returns:
-            list[:obj:`Det3DDataSample`]: Detection results of the
-            input samples. Each Det3DDataSample usually contain
-            'pred_instances_3d'. And the ``pred_instances_3d`` usually
-            contains following keys.
+            list[dict]: The output prediction result with following keys:
 
-                - scores_3d (Tensor): Classification scores, has a shape
-                    (num_instance, )
-                - labels_3d (Tensor): Labels of bboxes, has a shape
-                    (num_instances, ).
-                - masks_3d (Tensor): Contains a tensor with shape
-                    (num_instances, num_points).
+                - semantic_mask (Tensor): Segmentation mask of shape [N].
         """
-        x = self.extract_feat(batch_inputs_dict)
-        results_list = self.decode_head.predict(x, batch_data_samples,
-                                                **kwargs)
-        predictions = self.add_pred_to_datasample(batch_data_samples,
-                                                  results_list)
-        return predictions
+        # 3D segmentation requires per-point prediction, so it's impossible
+        # to use down-sampling to get a batch of scenes with same num_points
+        # therefore, we only support testing one scene every time
+        seg_pred_list = []
+        batch_input_metas = []
+        for data_sample in batch_data_samples:
+            batch_input_metas.append(data_sample.metainfo)
+
+        points = batch_inputs_dict['points']
+        for point, input_meta in zip(points, batch_input_metas):
+            seg_prob = self.inference(batch_inputs_dict, [input_meta],
+                                      rescale)[0]
+            seg_map = seg_prob.argmax(0)  # [N]
+            # to cpu tensor for consistency with det3d
+            seg_map = seg_map.cpu()
+            seg_pred_list.append(seg_map)
+
+        return self.postprocess_result(seg_pred_list, batch_data_samples)
+
+    def inference(self, inputs: SparseTensor,
+                  batch_img_metas: List[dict]) -> Tensor:
+        """Inference with slide/whole style.
+
+        Args:
+            inputs (Tensor): The input image of shape (N, 3, H, W).
+            batch_img_metas (List[dict]): List of image metainfo where each may
+                also contain: 'img_shape', 'scale_factor', 'flip', 'img_path',
+                'ori_shape', 'pad_shape', and 'padding_size'.
+                For details on the values of these keys see
+                `mmseg/datasets/pipelines/formatting.py:PackSegInputs`.
+
+        Returns:
+            Tensor: The segmentation results, seg_logits from model of each
+                input image.
+        """
+
+        assert self.test_cfg.mode in ['slide', 'whole']
+        if self.test_cfg.mode == 'slide':
+            seg_logit = self.slide_inference(inputs, batch_img_metas)
+        else:
+            seg_logit = self.whole_inference(inputs, batch_img_metas)
+
+        return seg_logit
 
     def extract_feat(self, batch_inputs_dict: dict) -> Tuple[Tensor]:
         """Extract features from points."""
