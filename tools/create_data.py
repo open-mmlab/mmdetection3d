b324f22cbef6172891bef7a85b45b27c51515dc2
Xiang Xu <xuxiang0103@gmail.com>
2023-02-16 19:28:51 +0800

    [Fix]: fix issue when converting waymo on tensorflow (#2143)

--- tools/create_data.py
+++ tools/create_data.py
@@ -263,10 +263,6 @@ if __name__ == '__main__':
     from mmdet3d.utils import register_all_modules
     register_all_modules()
 
-    # Set to spawn mode to avoid stuck when process dataset creating
-    import multiprocessing
-    multiprocessing.set_start_method('spawn')
-
     if args.dataset == 'kitti':
         kitti_data_prep(
             root_path=args.root_path,
