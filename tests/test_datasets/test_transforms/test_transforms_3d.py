cadc5644495519c4125d7a20fef828007346f0fc
Xiangxu-0103 <xuxiang0103@gmail.com>
2023-02-17 01:36:42 +0000

    fix UT

--- tests/test_datasets/test_transforms/test_transforms_3d.py
+++ tests/test_datasets/test_transforms/test_transforms_3d.py
@@ -123,6 +123,7 @@ class TestPolarMix(unittest.TestCase):
                 with_mask_3d=False,
                 with_seg_3d=True,
                 seg_3d_dtype=np.int32),
+            dict(type='PointSegClassMapping'),
         ]
         classes = ('unlabeled', 'car', 'bicycle', 'motorcycle', 'truck', 'bus',
                    'person', 'bicyclist', 'motorcyclist', 'road', 'parking',
@@ -150,10 +151,51 @@ class TestPolarMix(unittest.TestCase):
             [227, 119, 194],
             [82, 84, 163],
         ]
+        seg_label_mapping = {
+            0: 0,  # "unlabeled"
+            1: 0,  # "outlier" mapped to "unlabeled" --------------mapped
+            10: 1,  # "car"
+            11: 2,  # "bicycle"
+            13: 5,  # "bus" mapped to "other-vehicle" --------------mapped
+            15: 3,  # "motorcycle"
+            16: 5,  # "on-rails" mapped to "other-vehicle" ---------mapped
+            18: 4,  # "truck"
+            20: 5,  # "other-vehicle"
+            30: 6,  # "person"
+            31: 7,  # "bicyclist"
+            32: 8,  # "motorcyclist"
+            40: 9,  # "road"
+            44: 10,  # "parking"
+            48: 11,  # "sidewalk"
+            49: 12,  # "other-ground"
+            50: 13,  # "building"
+            51: 14,  # "fence"
+            52: 0,  # "other-structure" mapped to "unlabeled" ------mapped
+            60: 9,  # "lane-marking" to "road" ---------------------mapped
+            70: 15,  # "vegetation"
+            71: 16,  # "trunk"
+            72: 17,  # "terrain"
+            80: 18,  # "pole"
+            81: 19,  # "traffic-sign"
+            99: 0,  # "other-object" to "unlabeled" ----------------mapped
+            252: 1,  # "moving-car" to "car" ------------------------mapped
+            253: 7,  # "moving-bicyclist" to "bicyclist" ------------mapped
+            254: 6,  # "moving-person" to "person" ------------------mapped
+            255: 8,  # "moving-motorcyclist" to "motorcyclist" ------mapped
+            256: 5,  # "moving-on-rails" mapped to "other-vehic------mapped
+            257: 5,  # "moving-bus" mapped to "other-vehicle" -------mapped
+            258: 4,  # "moving-truck" to "truck" --------------------mapped
+            259: 5  # "moving-other"-vehicle to "other-vehicle"-----mapped
+        }
+        max_label = 259
         self.dataset = SemanticKITTIDataset(
             './tests/data/semantickitti/',
             'semantickitti_infos.pkl',
-            metainfo=dict(classes=classes, palette=palette),
+            metainfo=dict(
+                classes=classes,
+                palette=palette,
+                seg_label_mapping=seg_label_mapping,
+                max_label=max_label),
             data_prefix=dict(
                 pts='sequences/00/velodyne',
                 pts_semantic_mask='sequences/00/labels'),
