526b8f2e1c17d8efa13c4bd00ca83ad4832d7769
xzq <zeqixiao1@gmail.com>
2023-02-17 14:33:10 +0800

    add cylinder decode head

---
+++ mmdet3d/models/decode_heads/cylinder3d_head.py
@@ -0,0 +1,121 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import spconv
+import torch
+
+from mmdet3d.registry import MODELS
+from mmdet3d.structures.det3d_data_sample import SampleList
+from mmdet3d.utils.typing_utils import ConfigType
+from .decode_head import Base3DDecodeHead
+
+
+@MODELS.register_module()
+class Cylinder3DHead(Base3DDecodeHead):
+    """Cylinder3D decoder head.
+
+    Decoder head used in `Cylinder3D <https://arxiv.org/abs/2011.10033>`_.
+    Refer to the
+    `official code <https://https://github.com/xinge008/Cylinder3D>`_.
+
+    Args:
+        channels (int): Channels after modules, before conv_seg.
+        num_classes (int): Number of classes.
+        dropout_ratio (float): Ratio of dropout layer. Defaults to 0.5.
+        conv_cfg (dict or :obj:`ConfigDict`): Config of conv layers.
+            Defaults to dict(type='Conv1d').
+        norm_cfg (dict or :obj:`ConfigDict`): Config of norm layers.
+            Defaults to dict(type='BN1d').
+        act_cfg (dict or :obj:`ConfigDict`): Config of activation layers.
+            Defaults to dict(type='ReLU').
+        loss_ce (dict or :obj:`ConfigDict`): Config of CrossEntropy loss.
+            Defaults to dict(
+                     type='mmdet.CrossEntropyLoss',
+                     use_sigmoid=False,
+                     class_weight=None,
+                     loss_weight=1.0).
+        loss_lovasz (dict or :obj:`ConfigDict`): Config of Lovasz loss.
+            Defaults to dict(type='mmseg.LovaszLoss', loss_weight=1.0).
+        conv_seg_kernel_size (int): The kernel size used in conv_seg.
+            Defaults to 1.
+        ignore_index (int): The label index to be ignored. When using masked
+            BCE loss, ignore_index should be set to None. Defaults to 255.
+        init_cfg (dict or :obj:`ConfigDict` or list[dict or :obj:`ConfigDict`],
+            optional): Initialization config dict. Defaults to None.
+    """
+
+    def __init__(self,
+                 channels: int,
+                 num_classes: int,
+                 dropout_ratio: float = 0,
+                 conv_cfg: ConfigType = dict(type='Conv1d'),
+                 norm_cfg: ConfigType = dict(type='BN1d'),
+                 act_cfg: ConfigType = dict(type='ReLU'),
+                 loss_ce: ConfigType = dict(
+                     type='mmdet.CrossEntropyLoss',
+                     use_sigmoid=False,
+                     class_weight=None,
+                     loss_weight=1.0),
+                 loss_lovasz: ConfigType = dict(
+                     type='mmseg.LovaszLoss', loss_weight=1.0),
+                 conv_seg_kernel_size: int = 3,
+                 ignore_index: int = 0,
+                 init_cfg=None) -> None:
+        super(Cylinder3DHead, self).__init__(
+            channels=channels,
+            num_classes=num_classes,
+            dropout_ratio=dropout_ratio,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=act_cfg,
+            conv_seg_kernel_size=conv_seg_kernel_size,
+            init_cfg=init_cfg)
+
+        self.loss_lovasz = MODELS.build(loss_lovasz)
+        self.loss_ce = MODELS.build(loss_ce)
+        self.ignore_index = ignore_index
+
+    def build_conv_seg(self, channels: int, num_classes: int,
+                       kernel_size: int) -> spconv.SubMConv3d:
+        return spconv.SubMConv3d(
+            channels,
+            num_classes,
+            indice_key='logit',
+            kernel_size=kernel_size,
+            stride=1,
+            padding=1,
+            bias=True)
+
+    def forward(
+            self,
+            sparse_voxels: spconv.SparseConvTensor) -> spconv.SparseConvTensor:
+        """Forward function."""
+        sparse_logits = self.cls_seg(sparse_voxels)
+        return sparse_logits
+
+    def loss_by_feat(self, seg_logit: spconv.SparseConvTensor,
+                     batch_data_samples: SampleList) -> dict:
+        """Compute semantic segmentation loss.
+
+        Args:
+            seg_logit (spconv.SparseConvTensor): Predicted per-voxel
+                segmentation logits of shape [num_voxels, num_classes]
+                stored in SparseConvTensor.
+            batch_data_samples (List[:obj:`Det3DDataSample`]): The seg
+                data samples. It usually includes information such
+                as `metainfo` and `gt_pts_seg`.
+        """
+
+        gt_semantic_segs = [
+            data_sample.gt_pts_seg.voxel_semantic_mask
+            for data_sample in batch_data_samples
+        ]
+        seg_label = torch.cat(gt_semantic_segs)
+        seg_logit_feat = seg_logit.features
+        loss = dict()
+        loss['loss_ce'] = self.loss_ce(
+            seg_logit_feat, seg_label, ignore_index=self.ignore_index)
+        seg_logit_feat = seg_logit_feat.permute(1, 0)[None, :, :,
+                                                      None]  # pseudo BCHW
+        loss['loss_lovasz'] = self.loss_lovasz(
+            seg_logit_feat, seg_label, ignore_index=self.ignore_index)
+
+        return loss
---
+++ tests/test_models/test_decode_heads/test_cylinder3d_head.py
@@ -0,0 +1,59 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from unittest import TestCase
+
+import spconv
+import torch
+
+from mmdet3d.models.decode_heads import Cylinder3DHead
+from mmdet3d.structures import Det3DDataSample, PointData
+
+
+class TestCylinder3DHead(TestCase):
+
+    def test_cylinder3d_head_loss(self):
+        """Tests DGCNN head loss."""
+
+        cylinder3d_head = Cylinder3DHead(
+            channels=128,
+            num_classes=20,
+            loss_ce=dict(
+                type='mmdet.CrossEntropyLoss',
+                use_sigmoid=False,
+                class_weight=None,
+                loss_weight=1.0),
+            loss_lovasz=dict(
+                type='mmseg.LovaszLoss', loss_weight=1.0, reduction='none'),
+        ).cuda()
+
+        # DGCNN head expects dict format features
+        voxel_feats = torch.rand(50, 128).cuda()
+        coorx = torch.randint(0, 480, (50, 1)).int().cuda()
+        coory = torch.randint(0, 360, (50, 1)).int().cuda()
+        coorz = torch.randint(0, 32, (50, 1)).int().cuda()
+        coorbatch0 = torch.zeros(50, 1).int().cuda()
+        coors = torch.cat([coorbatch0, coorx, coory, coorz], dim=1)
+        grid_size = [480, 360, 32]
+        batch_size = 1
+
+        sparse_voxels = spconv.SparseConvTensor(voxel_feats, coors, grid_size,
+                                                batch_size)
+        # Test forward
+        seg_logits = cylinder3d_head.forward(sparse_voxels)
+
+        self.assertEqual(seg_logits.features.shape, torch.Size([50, 20]))
+
+        # When truth is non-empty then losses
+        # should be nonzero for random inputs
+        voxel_semantic_mask = torch.randint(0, 20, (50, )).long().cuda()
+        gt_pts_seg = PointData(voxel_semantic_mask=voxel_semantic_mask)
+
+        datasample = Det3DDataSample()
+        datasample.gt_pts_seg = gt_pts_seg
+
+        losses = cylinder3d_head.loss_by_feat(seg_logits, [datasample])
+
+        loss_ce = losses['loss_ce'].item()
+        loss_lovasz = losses['loss_lovasz'].item()
+
+        self.assertGreater(loss_ce, 0, 'ce loss should be positive')
+        self.assertGreater(loss_lovasz, 0, 'lovasz loss should be positive')
