07ef9aba4607f6e80e821ca7b72e70aa2927e47e
daxiongpro <910660298@qq.com>
2023-03-03 10:11:55 +0800

    增加一键安装环境脚本

---
+++ tools/env_install.sh
@@ -0,0 +1,22 @@
+# install pytorch
+pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+# install mim (optional)
+pip install openmim
+# install mmcv, mmdet, mmengine
+pip install -r requirements/mminstall.txt
+# install other library
+pip install numpy==1.23.5 nuscenes-devkit scipy==1.9.1
+# install mmdet3d
+python setup.py develop
+# install ops
+python projects/BEVFusion/setup.py develop
+
+
+
+
+
+
+
+
+
+
