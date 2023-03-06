a1b974a57cf53e416f88b28468749cffbd816406
xizaoqu <45515569+xizaoqu@users.noreply.github.com>
2023-03-06 23:27:19 +0800

    [Feature] Add Cylinder3D head (#2291)
    * add cylinder decode head
    
    * update
    
    * update
    
    * add lovasz loss
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * cylinder3d_head
    
    * update
    
    * update
    
    * update
    
    * update

--- mmdet3d/models/decode_heads/__init__.py
+++ mmdet3d/models/decode_heads/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .cylinder3d_head import Cylinder3DHead
 from .dgcnn_head import DGCNNHead
 from .paconv_head import PAConvHead
 from .pointnet2_head import PointNet2Head
 
-__all__ = ['PointNet2Head', 'DGCNNHead', 'PAConvHead']
+__all__ = ['PointNet2Head', 'DGCNNHead', 'PAConvHead', 'Cylinder3DHead']
