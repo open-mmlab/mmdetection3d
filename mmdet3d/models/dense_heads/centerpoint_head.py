4199bd619281ac37312aeded28c64dc8781ad3ea
yechenzhi <136920488@qq.com>
2023-03-28 16:08:36 +0800

    [Refactor]: rename centerpoint coordinates of gt box length and width (#2385)

--- mmdet3d/models/dense_heads/centerpoint_head.py
+++ mmdet3d/models/dense_heads/centerpoint_head.py
@@ -519,16 +519,16 @@ class CenterHead(BaseModule):
             for k in range(num_objs):
                 cls_id = task_classes[idx][k] - 1
 
-                width = task_boxes[idx][k][3]
-                length = task_boxes[idx][k][4]
-                width = width / voxel_size[0] / self.train_cfg[
+                length = task_boxes[idx][k][3]
+                width = task_boxes[idx][k][4]
+                length = length / voxel_size[0] / self.train_cfg[
                     'out_size_factor']
-                length = length / voxel_size[1] / self.train_cfg[
+                width = width / voxel_size[1] / self.train_cfg[
                     'out_size_factor']
 
                 if width > 0 and length > 0:
                     radius = gaussian_radius(
-                        (length, width),
+                        (width, length),
                         min_overlap=self.train_cfg['gaussian_overlap'])
                     radius = max(self.train_cfg['min_radius'], int(radius))
 
