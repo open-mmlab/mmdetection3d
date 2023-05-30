a44bbfb73e9ac40c0be40c3216280db60f2c094f
wep21 <daisuke.nishimatsu1021@gmail.com>
2023-05-31 07:05:22 +0900

    use code from BEVFusion
    Signed-off-by: wep21 <daisuke.nishimatsu1021@gmail.com>

--- projects/TransFusion/transfusion/__init__.py
+++ projects/TransFusion/transfusion/__init__.py
@@ -1,7 +1,6 @@
-from .hungarian_assigner import HungarianAssigner3D
+from ...BEVFusion.bevfusion.transfusion_head import TransFusionHead
+from ...BEVFusion.bevfusion.utils import HungarianAssigner3D, TransFusionBBoxCoder
 from .transfusion import TransFusionDetector
-from .transfusion_bbox_corder import TransFusionBBoxCoder
-from .transfusion_head import TransFusionHead
 
 __all__ = [
     "HungarianAssigner3D",
