956704ff5bba961cb1ba9a967fc977b9848e0a90
JingweiZhang12 <zjw18@mails.tsinghua.edu.cn>
2023-06-16 19:16:48 +0800

    verify configs

--- mmdet3d/configs/centerpoint/centerpoint_voxel01_second_secfpn_8xb4_cyclic_20e_nus_3d.py
+++ mmdet3d/configs/centerpoint/centerpoint_voxel01_second_secfpn_8xb4_cyclic_20e_nus_3d.py
@@ -1,7 +1,14 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 if '_base_':
-    from .centerpoint_voxel01_second_secfpn_8xb4_cyclic_20e_nus_3d import *
+    from .._base_.datasets.nus_3d import *
+    from .._base_.models.centerpoint_voxel01_second_secfpn_nus import *
+    from .._base_.schedules.cyclic_20e import *
+    from .._base_.default_runtime import *
 
+from mmengine.dataset.sampler import DefaultSampler
+
+from mmdet3d.datasets.dataset_wrappers import CBGSDataset
+from mmdet3d.datasets.nuscenes_dataset import NuScenesDataset
 from mmdet3d.datasets.transforms.formating import Pack3DDetInputs
 from mmdet3d.datasets.transforms.loading import (LoadAnnotations3D,
                                                  LoadPointsFromFile,
@@ -13,11 +20,10 @@ from mmdet3d.datasets.transforms.transforms_3d import (  # noqa
 
 # If point cloud range is changed, the models should also change their point
 # cloud range accordingly
-voxel_size = [0.075, 0.075, 0.2]
-point_cloud_range = [-54, -54, -5.0, 54, 54, 3.0]
+point_cloud_range = [-51.2, -51.2, -5.0, 51.2, 51.2, 3.0]
 # Using calibration info convert the Lidar-coordinate point cloud range to the
 # ego-coordinate point cloud range could bring a little promotion in nuScenes.
-# point_cloud_range = [-54, -54.8, -5.0, 54, 53.2, 3.0]
+# point_cloud_range = [-51.2, -52, -5.0, 51.2, 50.4, 3.0]
 # For nuScenes we usually do 10-class detection
 class_names = [
     'car', 'truck', 'construction_vehicle', 'bus', 'trailer', 'barrier',
@@ -28,62 +34,52 @@ data_prefix.merge(
 model.merge(
     dict(
         data_preprocessor=dict(
-            voxel_layer=dict(
-                voxel_size=voxel_size, point_cloud_range=point_cloud_range)),
-        pts_middle_encoder=dict(sparse_shape=[41, 1440, 1440]),
-        pts_bbox_head=dict(
-            bbox_coder=dict(
-                voxel_size=voxel_size[:2], pc_range=point_cloud_range[:2])),
-        train_cfg=dict(
-            pts=dict(
-                grid_size=[1440, 1440, 40],
-                voxel_size=voxel_size,
-                point_cloud_range=point_cloud_range)),
-        test_cfg=dict(
-            pts=dict(
-                voxel_size=voxel_size[:2], pc_range=point_cloud_range[:2]))))
+            voxel_layer=dict(point_cloud_range=point_cloud_range)),
+        pts_bbox_head=dict(bbox_coder=dict(pc_range=point_cloud_range[:2])),
+        # model training and testing settings
+        train_cfg=dict(pts=dict(point_cloud_range=point_cloud_range)),
+        test_cfg=dict(pts=dict(pc_range=point_cloud_range[:2]))))
 
 dataset_type = 'NuScenesDataset'
 data_root = 'data/nuscenes/'
 backend_args = None
 
-db_sampler.merge(
-    dict(
-        data_root=data_root,
-        info_path=data_root + 'nuscenes_dbinfos_train.pkl',
-        rate=1.0,
-        prepare=dict(
-            filter_by_difficulty=[-1],
-            filter_by_min_points=dict(
-                car=5,
-                truck=5,
-                bus=5,
-                trailer=5,
-                construction_vehicle=5,
-                traffic_cone=5,
-                barrier=5,
-                motorcycle=5,
-                bicycle=5,
-                pedestrian=5)),
-        classes=class_names,
-        sample_groups=dict(
-            car=2,
-            truck=3,
-            construction_vehicle=7,
-            bus=4,
-            trailer=6,
-            barrier=2,
-            motorcycle=6,
-            bicycle=6,
-            pedestrian=2,
-            traffic_cone=2),
-        points_loader=dict(
-            type=LoadPointsFromFile,
-            coord_type='LIDAR',
-            load_dim=5,
-            use_dim=[0, 1, 2, 3, 4],
-            backend_args=backend_args),
-        backend_args=backend_args))
+db_sampler = dict(
+    data_root=data_root,
+    info_path=data_root + 'nuscenes_dbinfos_train.pkl',
+    rate=1.0,
+    prepare=dict(
+        filter_by_difficulty=[-1],
+        filter_by_min_points=dict(
+            car=5,
+            truck=5,
+            bus=5,
+            trailer=5,
+            construction_vehicle=5,
+            traffic_cone=5,
+            barrier=5,
+            motorcycle=5,
+            bicycle=5,
+            pedestrian=5)),
+    classes=class_names,
+    sample_groups=dict(
+        car=2,
+        truck=3,
+        construction_vehicle=7,
+        bus=4,
+        trailer=6,
+        barrier=2,
+        motorcycle=6,
+        bicycle=6,
+        pedestrian=2,
+        traffic_cone=2),
+    points_loader=dict(
+        type=LoadPointsFromFile,
+        coord_type='LIDAR',
+        load_dim=5,
+        use_dim=[0, 1, 2, 3, 4],
+        backend_args=backend_args),
+    backend_args=backend_args)
 
 train_pipeline = [
     dict(
@@ -148,11 +144,29 @@ test_pipeline = [
         ]),
     dict(type=Pack3DDetInputs, keys=['points'])
 ]
+
 train_dataloader.merge(
     dict(
+        _delete_=True,
+        batch_size=4,
+        num_workers=4,
+        persistent_workers=True,
+        sampler=dict(type=DefaultSampler, shuffle=True),
         dataset=dict(
+            type=CBGSDataset,
             dataset=dict(
-                pipeline=train_pipeline, metainfo=dict(classes=class_names)))))
+                type=NuScenesDataset,
+                data_root=data_root,
+                ann_file='nuscenes_infos_train.pkl',
+                pipeline=train_pipeline,
+                metainfo=dict(classes=class_names),
+                test_mode=False,
+                data_prefix=data_prefix,
+                use_valid_flag=True,
+                # we use box_type_3d='LiDAR' in kitti and nuscenes dataset
+                # and box_type_3d='Depth' in sunrgbd and scannet dataset.
+                box_type_3d='LiDAR',
+                backend_args=backend_args))))
 test_dataloader.merge(
     dict(
         dataset=dict(
@@ -161,3 +175,5 @@ val_dataloader.merge(
     dict(
         dataset=dict(
             pipeline=test_pipeline, metainfo=dict(classes=class_names))))
+
+train_cfg.merge(dict(val_interval=20))
