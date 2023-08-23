a0347af14b09983981d42b18b51225bf5752ba39
popcornGit <LBKweiruanzhanghao@outlook.com>
2023-08-23 16:49:05 +0000

    mmdet3D_SECOND_new_cfg

--- configs/_base_/models/second_hv_secfpn_kitti.py
+++ configs/_base_/models/second_hv_secfpn_kitti.py
@@ -1,40 +1,62 @@
+from mmdet3d.models import VoxelNet
+from mmdet3d.models.backbones import SECOND
+from mmdet3d.models.data_preprocessors import Det3DDataPreprocessor
+from mmdet3d.models.necks import SECONDFPN
+from mmdet3d.models.voxel_encoders import HardSimpleVFE
+from mmdet3d.models.middle_encoders import SparseEncoder
+from mmdet3d.models.dense_heads import Anchor3DHead
+from mmdet3d.models.task_modules import Anchor3DRangeGenerator
+from mmdet3d.models.task_modules import DeltaXYZWLHRBBoxCoder
+from mmdet3d.models.task_modules import Max3DIoUAssigner
+from mmdet3d.structures import BboxOverlapsNearest3D
+
+from mmdet.models.losses import FocalLoss, SmoothL1Loss, CrossEntropyLoss
+
+
 voxel_size = [0.05, 0.05, 0.1]
 
 model = dict(
-    type='VoxelNet',
+
+    type=VoxelNet,
+
     data_preprocessor=dict(
-        type='Det3DDataPreprocessor',
+        type=Det3DDataPreprocessor,
         voxel=True,
         voxel_layer=dict(
             max_num_points=5,
             point_cloud_range=[0, -40, -3, 70.4, 40, 1],
             voxel_size=voxel_size,
             max_voxels=(16000, 40000))),
-    voxel_encoder=dict(type='HardSimpleVFE'),
+
+    voxel_encoder=dict(type=HardSimpleVFE),
+
     middle_encoder=dict(
-        type='SparseEncoder',
+        type=SparseEncoder,
         in_channels=4,
         sparse_shape=[41, 1600, 1408],
         order=('conv', 'norm', 'act')),
+
     backbone=dict(
-        type='SECOND',
+        type=SECOND,
         in_channels=256,
         layer_nums=[5, 5],
         layer_strides=[1, 2],
         out_channels=[128, 256]),
+
     neck=dict(
-        type='SECONDFPN',
+        type=SECONDFPN,
         in_channels=[128, 256],
         upsample_strides=[1, 2],
         out_channels=[256, 256]),
+
     bbox_head=dict(
-        type='Anchor3DHead',
+        type=Anchor3DHead,
         num_classes=3,
         in_channels=512,
         feat_channels=512,
         use_direction_classifier=True,
         anchor_generator=dict(
-            type='Anchor3DRangeGenerator',
+            type=Anchor3DRangeGenerator,
             ranges=[
                 [0, -40.0, -0.6, 70.4, 40.0, -0.6],
                 [0, -40.0, -0.6, 70.4, 40.0, -0.6],
@@ -44,38 +66,39 @@ model = dict(
             rotations=[0, 1.57],
             reshape_out=False),
         diff_rad_by_sin=True,
-        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder'),
+        bbox_coder=dict(type=DeltaXYZWLHRBBoxCoder),
         loss_cls=dict(
-            type='mmdet.FocalLoss',
+            type=FocalLoss,
             use_sigmoid=True,
             gamma=2.0,
             alpha=0.25,
             loss_weight=1.0),
         loss_bbox=dict(
-            type='mmdet.SmoothL1Loss', beta=1.0 / 9.0, loss_weight=2.0),
+            type=SmoothL1Loss, beta=1.0 / 9.0, loss_weight=2.0),
         loss_dir=dict(
-            type='mmdet.CrossEntropyLoss', use_sigmoid=False,
+            type=CrossEntropyLoss, use_sigmoid=False,
             loss_weight=0.2)),
+
     # model training and testing settings
     train_cfg=dict(
         assigner=[
             dict(  # for Pedestrian
-                type='Max3DIoUAssigner',
-                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                type=Max3DIoUAssigner,
+                iou_calculator=dict(type=BboxOverlapsNearest3D),
                 pos_iou_thr=0.35,
                 neg_iou_thr=0.2,
                 min_pos_iou=0.2,
                 ignore_iof_thr=-1),
             dict(  # for Cyclist
-                type='Max3DIoUAssigner',
-                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                type=Max3DIoUAssigner,
+                iou_calculator=dict(type=BboxOverlapsNearest3D),
                 pos_iou_thr=0.35,
                 neg_iou_thr=0.2,
                 min_pos_iou=0.2,
                 ignore_iof_thr=-1),
             dict(  # for Car
-                type='Max3DIoUAssigner',
-                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                type=Max3DIoUAssigner,
+                iou_calculator=dict(type=BboxOverlapsNearest3D),
                 pos_iou_thr=0.6,
                 neg_iou_thr=0.45,
                 min_pos_iou=0.45,
@@ -84,6 +107,7 @@ model = dict(
         allowed_border=0,
         pos_weight=-1,
         debug=False),
+
     test_cfg=dict(
         use_rotate_nms=True,
         nms_across_levels=False,
@@ -91,4 +115,5 @@ model = dict(
         score_thr=0.1,
         min_bbox_size=0,
         nms_pre=100,
-        max_num=50))
+        max_num=50)
+        )
---
+++ configs/second/second_hv_secfpn_8xb6_amp_80e_kitti_3d_car.py
@@ -0,0 +1,10 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmengine import read_base
+
+with read_base():
+    from ..second.second_hv_secfpn_8xb6_80e_kitti_3d_car import *
+
+from mmengine.optim.optimizer import AmpOptimWrapper
+
+# schedule settings
+optim_wrapper.update(type=AmpOptimWrapper, loss_scale=4096.)
