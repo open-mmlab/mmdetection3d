94f891fadf8cf56427f261080ed417a8e60d2dbe
Yanyirong <yanyirong@sjtu.edu.cn>
2023-09-14 15:01:32 +0800

    modify the config

--- configs/nerfdet/nerfdet_res50_2x_low_res.py
+++ configs/nerfdet/nerfdet_res50_2x_low_res.py
@@ -1,29 +1,96 @@
-_base_ = [
-    
-]
+# TODO
+_base_ = []
 
+# TODO
+# prior_generator = dict(
+
+# )
+
+# TODO
 model = dict()
 
-dataset_type =  'ScanNetMultiViewDataset'
+dataset_type = 'ScanNetMultiViewDataset'
 data_root = 'data/scannet/'
 class_names = ('cabinet', 'bed', 'chair', 'sofa', 'table', 'door', 'window',
                'bookshelf', 'picture', 'counter', 'desk', 'curtain',
                'refrigerator', 'showercurtrain', 'toilet', 'sink', 'bathtub',
                'garbagebin')
 input_modality = dict(
-    use_image=True,
+    use_camera=True,
     use_depth=False,
     use_lidar=False,
     use_neuralrecon_depth=False,
     use_ray=True)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 metainfo = dict(classes=class_names)
+file_client_args = dict(backend='disk')
+
+backend_args = None
+train_collect_keys = ['img', 'gt_bboxes_3d', 'gt_labels_3d']
+test_collect_keys = ['img']
+if input_modality['use_depth']:
+    train_collect_keys.append('depth')
+    test_collect_keys.append('depth')
+if input_modality['use_lidar']:
+    train_collect_keys.append('lidar')
+    test_collect_keys.append('lidar')
+if input_modality['use_ray']:
+    for key in [
+            # 'c2w',
+            # 'camrotc2w',
+            'lightpos',
+            # 'pixels',
+            'nerf_sizes',
+            'raydirs',
+            'gt_images',
+            'gt_depths',
+            'denorm_images'
+    ]:
+        train_collect_keys.append(key)
+        test_collect_keys.append(key)
+
 train_pipeline = [
-    
+    dict(type='LoadAnnotations3D'),
+    dict(
+        type='MultiViewPipeline',
+        n_images=50,
+        transforms=[
+            dict(type='LoadImageFromFile', file_client_args=file_client_args),
+            dict(type='Resize', scale=(320, 240), keep_ratio=True),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size=(240, 320))
+        ],
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        margin=10,
+        depth_range=[0.5, 5.5],
+        loading='random',
+        nerf_target_views=10),
+    dict(type='RandomShiftOrigin', std=(.7, .7, .0)),
+    dict(type='Pack3DDetInputs', keys=train_collect_keys)
 ]
+
 test_pipeline = [
-    
+    dict(type='LoadAnnotations3D'),
+    dict(
+        type='MultiViewPipeline',
+        n_images=101,
+        transforms=[
+            dict(type='LoadImageFromFile', file_client_args=file_client_args),
+            dict(type='Resize', scale=(320, 240), keep_ratio=True),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size=(240, 320))
+        ],
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        margin=10,
+        depth_range=[0.5, 5.5],
+        loading='random',
+        nerf_target_views=1),
+    dict(type='Pack3DDetInputs', keys=test_collect_keys)
 ]
-train_dataloader=dict(
+train_dataloader = dict(
     batch_size=4,
     num_workers=4,
     persistent_workers=True,
@@ -40,8 +107,9 @@ train_dataloader=dict(
             test_mode=False,
             metainfo=metainfo,
             filter_empty_gt=True,
-            box_type_3d='Depth')))
-val_dataloader=dict(
+            box_type_3d='Depth',
+            backend_args=backend_args)))
+val_dataloader = dict(
     batch_size=1,
     num_workers=1,
     persistent_workers=True,
@@ -55,16 +123,27 @@ val_dataloader=dict(
         modality=input_modality,
         classes=class_names,
         test_mode=True,
-        box_type_3d='Depth'))
+        box_type_3d='Depth',
+        backend_args=backend_args))
 test_dataloader = val_dataloader
 
-val_evaluator = None
-test_evaluator = None
+# TODO
+# val_evaluator = None
+# test_evaluator = None
 
 optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=dict(
         _delete_=True, type='AdamW', lr=0.0002, weight_decay=0.0001),
     paramwise_cfg=dict(
-        custom_keys={'backbone':dict(lr_mult=0.1, decay_mult=1.0)}),
+        custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0)}),
     clip_grad=dict(max_norm=35., norm_type=2))
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=12,
+        by_epoch=True,
+        milestones=[8, 11],
+        gamma=0.1)
+]
