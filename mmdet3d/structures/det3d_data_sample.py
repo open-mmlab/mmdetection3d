d71b32835df6054f64d680608b75143f2f5e5cb7
Yanyirong <yanyirong@sjtu.edu.cn>
2023-10-17 13:46:39 +0800

    update structures

--- mmdet3d/structures/det3d_data_sample.py
+++ mmdet3d/structures/det3d_data_sample.py
@@ -207,16 +207,28 @@ class Det3DDataSample(DetDataSample):
         del self._pred_pts_seg
 
     @property
-    def gt_nerf_3d(self) -> InstanceData:
-        return self._gt_nerf_3d
+    def gt_nerf_images(self) -> InstanceData:
+        return self._gt_nerf_images
 
-    @gt_nerf_3d.setter
-    def gt_nerf_3d(self, value: InstanceData) -> None:
-        self.set_field(value, '_gt_nerf_3d', dtype=InstanceData)
+    @gt_nerf_images.setter
+    def gt_nerf_images(self, value: InstanceData) -> None:
+        self.set_field(value, '_gt_nerf_images', dtype=InstanceData)
 
-    @gt_nerf_3d.deleter
-    def gt_nerf_3d(self) -> None:
-        del self._gt_nerf_3d
+    @gt_nerf_images.deleter
+    def gt_nerf_images(self) -> None:
+        del self._gt_nerf_images
+
+    @property
+    def gt_nerf_depths(self) -> InstanceData:
+        return self._gt_nerf_depths
+
+    @gt_nerf_depths.setter
+    def gt_nerf_depths(self, value: InstanceData) -> None:
+        self.set_field(value, '_gt_nerf_depths', dtype=InstanceData)
+
+    @gt_nerf_depths.deleter
+    def gt_nerf_depths(self) -> None:
+        del self._gt_nerf_depths
 
 
 SampleList = List[Det3DDataSample]
