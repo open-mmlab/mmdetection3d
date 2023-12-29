2066cbf9ed3edb3c99ddfecd92690f03d12f0f30
Yanyirong <yanyirong@sjtu.edu.cn>
2023-12-29 10:28:46 +0800

    modify render_ray.py

--- projects/NeRF-Det/nerfdet/nerf_utils/render_ray.py
+++ projects/NeRF-Det/nerfdet/nerf_utils/render_ray.py
@@ -1,17 +1,7 @@
-# Copyright 2020 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# Copyright (c) OpenMMLab. All rights reserved.
+# Attention: This file is mainly modified based on the file with the same
+# name in the original project. For more details, please refer to the
+# origin project.
 from collections import OrderedDict
 
 import numpy as np
@@ -24,10 +14,6 @@ rng = np.random.RandomState(234)
 # helper functions for nerf ray rendering
 def volume_sampling(sample_pts, features, aabb):
     B, C, D, W, H = features.shape
-    '''
-    Actually here is hard code since per gpu only occupy one scene.
-    hard_code B=1.can directly use point xyz instead of aabb size
-    '''
     assert B == 1
     aabb = torch.Tensor(aabb).to(sample_pts.device)
     N_rays, N_samples, coords = sample_pts.shape
@@ -38,28 +24,20 @@ def volume_sampling(sample_pts, features, aabb):
     norm_pts = (sample_pts - aabb[0]) * invgridSize - 1
     sample_features = F.grid_sample(
         features, norm_pts, align_corners=True, padding_mode='border')
-    # 1, C, 1, 1, N_rays*N_samples
     masks = ((norm_pts < 1) & (norm_pts > -1)).float().sum(dim=-1)
     masks = (masks.view(N_rays, N_samples) == 3)
-    # x,y,z should be all in the volume.
-
     return sample_features.view(C, N_rays,
                                 N_samples).permute(1, 2, 0).contiguous(), masks
 
 
 def _compute_projection(img_meta):
-    # [n_views, 34], 34 = img_size(2) + intrinsics(16) + extrinsics(16)
-    # projection = []
     views = len(img_meta['lidar2img']['extrinsic'])
     intrinsic = torch.tensor(img_meta['lidar2img']['intrinsic'][:4, :4])
     ratio = img_meta['ori_shape'][0] / img_meta['img_shape'][0]
     intrinsic[:2] /= ratio
     intrinsic = intrinsic.unsqueeze(0).view(1, 16).repeat(views, 1)
-
     img_size = torch.Tensor(img_meta['img_shape'][:2]).to(intrinsic.device)
     img_size = img_size.unsqueeze(0).repeat(views, 1)
-    # use predicted pitch and roll for SUNRGBDTotal test
-
     extrinsics = []
     for v in range(views):
         extrinsics.append(
@@ -71,22 +49,16 @@ def _compute_projection(img_meta):
 
 
 def compute_mask_points(feature, mask):
-
     weight = mask / (torch.sum(mask, dim=2, keepdim=True) + 1e-8)
     mean = torch.sum(feature * weight, dim=2, keepdim=True)
-    # TODO: his would be a problem since non-valid point we assign var = 0!!!
     var = torch.sum((feature - mean)**2, dim=2, keepdim=True)
     var = var / (torch.sum(mask, dim=2, keepdim=True) + 1e-8)
     var = torch.exp(-var)
-
-    # mean = torch.mean(feature, dim=2, keepdim=True)
-    # var = torch.mean((feature - mean)**2, dim=2, keepdim=True)
-
     return mean, var
 
 
 def sample_pdf(bins, weights, N_samples, det=False):
-    """
+    """Helper function used for sampling.
 
     Args:
         bins (tensor):Tensor of shape [N_rays, M+1], M is the number of bins
@@ -101,40 +73,33 @@ def sample_pdf(bins, weights, N_samples, det=False):
     M = weights.shape[1]
     weights += 1e-5
     # Get pdf
-    pdf = weights / torch.sum(weights, dim=-1, keepdim=True)  # [N_rays, M]
-    cdf = torch.cumsum(pdf, dim=-1)  # [N_rays, M]
-    cdf = torch.cat([torch.zeros_like(cdf[:, 0:1]), cdf],
-                    dim=-1)  # [N_rays, M+1]
+    pdf = weights / torch.sum(weights, dim=-1, keepdim=True)
+    cdf = torch.cumsum(pdf, dim=-1)
+    cdf = torch.cat([torch.zeros_like(cdf[:, 0:1]), cdf], dim=-1)
 
     # Take uniform samples
     if det:
         u = torch.linspace(0., 1., N_samples, device=bins.device)
-        u = u.unsqueeze(0).repeat(bins.shape[0], 1)  # [N_rays, N_samples]
+        u = u.unsqueeze(0).repeat(bins.shape[0], 1)
     else:
         u = torch.rand(bins.shape[0], N_samples, device=bins.device)
 
     # Invert CDF
-    above_inds = torch.zeros_like(u, dtype=torch.long)  # [N_rays, N_samples]
+    above_inds = torch.zeros_like(u, dtype=torch.long)
     for i in range(M):
         above_inds += (u >= cdf[:, i:i + 1]).long()
 
     # random sample inside each bin
     below_inds = torch.clamp(above_inds - 1, min=0)
-    inds_g = torch.stack((below_inds, above_inds),
-                         dim=2)  # [N_rays, N_samples, 2]
+    inds_g = torch.stack((below_inds, above_inds), dim=2)
 
-    cdf = cdf.unsqueeze(1).repeat(1, N_samples, 1)  # [N_rays, N_samples, M+1]
-    cdf_g = torch.gather(
-        input=cdf, dim=-1, index=inds_g)  # [N_rays, N_samples, 2]
+    cdf = cdf.unsqueeze(1).repeat(1, N_samples, 1)
+    cdf_g = torch.gather(input=cdf, dim=-1, index=inds_g)
 
-    bins = bins.unsqueeze(1).repeat(1, N_samples,
-                                    1)  # [N_rays, N_samples, M+1]
-    bins_g = torch.gather(
-        input=bins, dim=-1, index=inds_g)  # [N_rays, N_samples, 2]
+    bins = bins.unsqueeze(1).repeat(1, N_samples, 1)
+    bins_g = torch.gather(input=bins, dim=-1, index=inds_g)
 
-    # t = (u-cdf_g[:, :, 0]) / (cdf_g[:, :, 1] - cdf_g[:, :, 0] + TINY_NUMBER)
-    # fix numeric issue
-    denom = cdf_g[:, :, 1] - cdf_g[:, :, 0]  # [N_rays, N_samples]
+    denom = cdf_g[:, :, 1] - cdf_g[:, :, 0]
     denom = torch.where(denom < 1e-5, torch.ones_like(denom), denom)
     t = (u - cdf_g[:, :, 0]) / denom
 
@@ -175,16 +140,16 @@ def sample_along_camera_ray(ray_o,
     far_depth = far_depth_value * torch.ones_like(ray_d[..., 0])
 
     if inv_uniform:
-        start = 1. / near_depth  # [N_rays,]
+        start = 1. / near_depth
         step = (1. / far_depth - start) / (N_samples - 1)
         inv_z_vals = torch.stack([start + i * step for i in range(N_samples)],
-                                 dim=1)  # [N_rays, N_samples]
+                                 dim=1)
         z_vals = 1. / inv_z_vals
     else:
         start = near_depth
         step = (far_depth - near_depth) / (N_samples - 1)
         z_vals = torch.stack([start + i * step for i in range(N_samples)],
-                             dim=1)  # [N_rays, N_samples]
+                             dim=1)
 
     if not det:
         # get intervals between samples
@@ -193,10 +158,9 @@ def sample_along_camera_ray(ray_o,
         lower = torch.cat([z_vals[:, 0:1], mids], dim=-1)
         # uniform samples in those intervals
         t_rand = torch.rand_like(z_vals)
-        z_vals = lower + (upper - lower) * t_rand  # [N_rays, N_samples]
+        z_vals = lower + (upper - lower) * t_rand
 
-    ray_d = ray_d.unsqueeze(1).repeat(1, N_samples,
-                                      1)  # [N_rays, N_samples, 3]
+    ray_d = ray_d.unsqueeze(1).repeat(1, N_samples, 1)
     ray_o = ray_o.unsqueeze(1).repeat(1, N_samples, 1)
     pts = z_vals.unsqueeze(2) * ray_d + ray_o  # [N_rays, N_samples, 3]
     return pts, z_vals
@@ -232,32 +196,24 @@ def raw2outputs(raw, z_vals, mask, white_bkgd=False):
     # point samples are ordered with increasing depth
     # interval between samples
     dists = z_vals[:, 1:] - z_vals[:, :-1]
-    dists = torch.cat((dists, dists[:, -1:]), dim=-1)  # [N_rays, N_samples]
+    dists = torch.cat((dists, dists[:, -1:]), dim=-1)
 
-    alpha = sigma2alpha(sigma, dists)  # [N_rays, N_samples]
+    alpha = sigma2alpha(sigma, dists)
 
-    # Eq. (3): T
-    T = torch.cumprod(
-        1. - alpha + 1e-10, dim=-1)[:, :-1]  # [N_rays, N_samples-1]
-    T = torch.cat((torch.ones_like(T[:, 0:1]), T),
-                  dim=-1)  # [N_rays, N_samples]
+    T = torch.cumprod(1. - alpha + 1e-10, dim=-1)[:, :-1]
+    T = torch.cat((torch.ones_like(T[:, 0:1]), T), dim=-1)
 
     # maths show weights, and summation of weights along a ray,
     # are always inside [0, 1]
-    weights = alpha * T  # [N_rays, N_samples]
-    rgb_map = torch.sum(weights.unsqueeze(2) * rgb, dim=1)  # [N_rays, 3]
+    weights = alpha * T
+    rgb_map = torch.sum(weights.unsqueeze(2) * rgb, dim=1)
 
     if white_bkgd:
         rgb_map = rgb_map + (1. - torch.sum(weights, dim=-1, keepdim=True))
 
     if mask is not None:
         mask = mask.float().sum(dim=1) > 8
-        # should at least have 8 valid observation on the ray,
-        # otherwise don't consider its loss
-    # TODO: should be considered in loss, 8 is a tradeoff.
 
-    # TODO: weights may be not sum into 1,
-    # so should be re-normalized, if 0, should add eps.
     depth_map = torch.sum(
         weights * z_vals, dim=-1) / (
             torch.sum(weights, dim=-1) + 1e-8)
@@ -317,15 +273,9 @@ def render_rays_func(
         train_camera = _compute_projection(img_meta).to(img.device)
         rgb_feat, mask = projector.compute(
             pts, img, train_camera, features_2D, grid_sample=True)
-        # RGB_feat: [N_rays, N_samples, N_views, channel]
-        # mask: [n_rays, n_samples, n_views, 1]
-        pixel_mask = mask[..., 0].sum(
-            dim=2
-        ) > 1  # [N_rays, N_samples], should at least have 2 observations
-        mean, var = compute_mask_points(rgb_feat,
-                                        mask)  # [n_rays, n_samples, 1, n_feat]
-        globalfeat = torch.cat([mean, var], dim=-1).squeeze(
-            2)  # [n_rays, n_samples, 1, 2*n_feat]
+        pixel_mask = mask[..., 0].sum(dim=2) > 1
+        mean, var = compute_mask_points(rgb_feat, mask)
+        globalfeat = torch.cat([mean, var], dim=-1).squeeze(2)
         rgb_pts, density_pts = nerf_mlp(pts, ray_d, globalfeat)
         raw_coarse = torch.cat([rgb_pts, density_pts], dim=-1)
         ret['sigma'] = density_pts
@@ -375,56 +325,8 @@ def render_rays(
         is_train=True,
         white_bkgd=False,
         render_testing=False):
-    """The function of the nerf rendering.
-    Note:
-        There is a risk that data augmentation is random origin
-        not influence nerf,but influence using nerf mlp
-        to estimate volme density.
+    """The function of the nerf rendering."""
 
-    Args:
-        ray_batch (dict):
-            - ray_o :The light poses.
-            - ray_d :The ray directions.
-            - gt_rgb :The groundtruth of the rgbs.
-            - gt_depth :The groundtruth of the depths.
-            - nerf_sizes :The shapes of the groundtruth images.
-            - denorm_images :The normalized rgb images.
-        mean_volume (? | None) :The average volume of the scene.
-        cov_volume ():
-        features_2D (): 2D features if needed.
-        img ():The imgs used for nerf.
-        aabb ():
-        near_far_range ():
-        N_samples (int): Samples along each ray
-            (for coarse and fine model).
-        N_rand (int):
-        nerf_mlp (): The mlp of nerf.
-        img_meta ():
-        projector ():
-        mode ():'volume' or 'image'
-        nerf_sample_view ():
-        inv_uniform (bool):If true,
-            uniformly sample inverse depth for coarse model.
-        N_importance (int): Additional samples along each ray
-            produced by importance sampling(for fine model).
-        det (bool) :If true,will deterministicly sample depths.
-        is_train (bool): If True,means in the train mode.
-        white_bkgd(bool):
-        render_testing(bool):
-
-    Returns:
-        ret (dict):
-            - output_coarse (dict):
-                - rgb (Tensor):
-                - depth (Tensor):
-                - weights (Tensor):
-                - mask (Tensor):
-                - alpha (Tensor):
-                - z_vals (Tensor):
-                - transparency (Tensor):
-            - gt_rgb (Tensor):
-            - gt_depth (Tensor):
-    """
     ray_o = ray_batch['ray_o']
     ray_d = ray_batch['ray_d']
     gt_rgb = ray_batch['gt_rgb']
@@ -476,9 +378,6 @@ def render_rays(
             gt_depth)
 
     elif render_testing:
-        # height, width = img_meta['nerf_sizes'].shape[:2]
-        # num_rays = height * width
-        # assert ray_o.shape[0] == num_rays
         nerf_size = nerf_sizes[0]
         view_num = ray_o.shape[1]
         H = nerf_size[0][0]
