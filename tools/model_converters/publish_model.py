ce744642b84be721621643c1acb72eb6c7ffb895
Richard Barnes <rbarnes@umn.edu>
2024-03-27 08:31:50 -0700

    Fix rstrip bug in publish_model.py
    `.rstrip(".pth")` will convert `pthhtp.pth` to the empty string. `removesuffix` does the right thing.

--- tools/model_converters/publish_model.py
+++ tools/model_converters/publish_model.py
@@ -23,7 +23,7 @@ def process_checkpoint(in_file, out_file):
     # add the code here.
     torch.save(checkpoint, out_file)
     sha = subprocess.check_output(['sha256sum', out_file]).decode()
-    final_file = out_file.rstrip('.pth') + '-{}.pth'.format(sha[:8])
+    final_file = out_file.removesuffix('.pth') + '-{}.pth'.format(sha[:8])
     subprocess.Popen(['mv', out_file, final_file])
 
 
